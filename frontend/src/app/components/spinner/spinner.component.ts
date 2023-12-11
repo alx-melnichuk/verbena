c833a6272c1b36cd77795dbe04e884b4ef2ab20e
alx.melnichuk <alx.melnichuk@gmail.com>
2023-12-11 11:47:43 +0200

    FE: Added the 'app-spinner'.

---
+++ frontend/src/app/components/spinner/spinner.component.ts
@@ -0,0 +1,28 @@
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ThemePalette } from '@angular/material/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+/**
+ * <app-spinner *ngIf="isLoad" [isFullOwner]="true"></app-spinner>
+ */
+
+const DEAULT_DIAMETER = 100;
+
+@Component({
+  selector: 'app-spinner',
+  standalone: true,
+  imports: [CommonModule, MatProgressSpinnerModule],
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SpinnerComponent {
+  @Input()
+  public color: ThemePalette = 'primary';
+  @Input()
+  public isFullOwner = false;
+  @Input()
+  public diameter = DEAULT_DIAMETER;
+}
