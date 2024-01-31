6f9bb62eecdae51555d3fe8590dc08aa8baa0a5a
alx.melnichuk <alx.melnichuk@gmail.com>
2024-01-31 22:52:47 +0200

    FE: modify "user-api"

--- frontend/src/app/entities/user/user-api.service.ts
+++ frontend/src/app/entities/user/user-api.service.ts
@@ -14,22 +14,22 @@ export class UserApiService {
   constructor(private http: HttpClient) {}
 
   public login(loginUserDto: LoginUserDto): Promise<LoginUserResponseDto | HttpErrorResponse | undefined> {
-    const url = Uri.appUri('appApi://api/login');
+    const url = Uri.appUri('appApi://login');
     return this.http.post<LoginUserResponseDto | HttpErrorResponse>(url, loginUserDto).toPromise();
   }
 
   public registration(createUserDto: CreateUserDto): Promise<null | HttpErrorResponse | undefined> {
-    const url = Uri.appUri('appApi://api/registration');
+    const url = Uri.appUri('appApi://registration');
     return this.http.post<null | HttpErrorResponse>(url, createUserDto).toPromise();
   }
 
   public recovery(recoveryUserDto: RecoveryUserDto): Promise<null | HttpErrorResponse | undefined> {
-    const url = Uri.appUri('appApi://api/recovery');
+    const url = Uri.appUri('appApi://recovery');
     return this.http.post<null | HttpErrorResponse>(url, recoveryUserDto).toPromise();
   }
 
   public currentUser(): Promise<UserDto | HttpErrorResponse | undefined> {
-    const url = Uri.appUri('appApi://api/users_current');
+    const url = Uri.appUri('appApi://users_current');
     return HttpObservableUtil.toPromise<UserDto>(this.http.get<UserDto | HttpErrorResponse>(url))
     .then((response: UserDto | HttpErrorResponse | undefined) => {
       return UserDtoUtil.new(response as UserDto)
@@ -37,16 +37,16 @@ export class UserApiService {
   }
 
   public isCeckRefreshToken(method: string, url: string): boolean {
-    return method === 'POST' && url === Uri.appUri('appApi://api/token');
+    return method === 'POST' && url === Uri.appUri('appApi://token');
   }
 
   public refreshToken(tokenUserDto: TokenUserDto): Promise<UserTokensDto | HttpErrorResponse | undefined> {
-    const url = Uri.appUri('appApi://api/token');
+    const url = Uri.appUri('appApi://token');
     return this.http.post<UserTokensDto | HttpErrorResponse>(url, tokenUserDto).toPromise();
   }
 
   public logout(): Promise<void | HttpErrorResponse | undefined> {
-    const url = Uri.appUri('appApi://api/logout');
+    const url = Uri.appUri('appApi://logout');
     return this.http.post<void | HttpErrorResponse>(url, null).toPromise();
   }
 
