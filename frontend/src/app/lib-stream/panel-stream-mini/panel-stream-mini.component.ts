a3d14130c150db7da4d14efba2d877ae943507e6
alx.melnichuk <alx.melnichuk@gmail.com>
2024-02-15 21:43:20 +0200

    FE: Added "PanelStreamMini"

---
+++ frontend/src/app/lib-stream/panel-stream-mini/panel-stream-mini.component.ts
@@ -0,0 +1,127 @@
+import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, HostBinding, Input, OnChanges, OnInit, Output, SimpleChanges, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { TranslateModule } from '@ngx-translate/core';
+import { PageData, PageDataUtil } from 'src/app/common/page-data';
+import { PIPE_DATE, PIPE_DATE_TIME } from 'src/app/common/constants';
+import { StreamDto, StreamListDto } from '../stream-api.interface';
+
+// InfiniteScrollModule, LogotypeModule
+@Component({
+  selector: 'app-panel-stream-mini',
+  standalone: true,
+  imports: [CommonModule, TranslateModule, MatButtonModule, MatCardModule,],
+  templateUrl: './panel-stream-mini.component.html',
+  styleUrls: ['./panel-stream-mini.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PanelStreamMiniComponent implements OnChanges, OnInit {
+  @Input()
+  public canDuplicate = false;
+  @Input()
+  public canEdit = false;
+  @Input()
+  public canDelete = false;
+  @Input()
+  public isFuture = false;
+  @Input()
+  public streamList: StreamListDto = this.emptyStreamListDto();
+
+  @Output()
+  readonly requestNextPage: EventEmitter<PageData> = new EventEmitter();
+  @Output()
+  readonly redirectToStream: EventEmitter<number> = new EventEmitter();
+  @Output()
+  readonly actionDuplicate: EventEmitter<number> = new EventEmitter();
+  @Output()
+  readonly actionEdit: EventEmitter<number> = new EventEmitter();
+  @Output()
+  readonly actionDelete: EventEmitter<StreamDto> = new EventEmitter();
+
+  public streamDTOList: StreamDto[] = [];
+  public pageData: PageData = PageDataUtil.create({});
+  readonly formatDate = PIPE_DATE;
+  readonly formatDateTime = PIPE_DATE_TIME;
+
+  @HostBinding('class.global-scroll')
+  public get isGlobalScroll(): boolean { return true; }
+
+  constructor(
+    private element: ElementRef,
+  ) {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!!changes['streamList']) {
+      this.streamList = !this.streamList ? this.emptyStreamListDto() : this.streamList;
+      this.prepareNextStreamsData(this.streamList);
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+  // ** Public API **
+
+  public trackByIdFn(index: number, item: StreamDto): number {
+    return item.id;
+  }
+
+  public onScroll(): void {
+    this.checkAndGetNextPage(this.pageData);
+  }
+
+  public doRedirectToStream(streamId: number): void {
+    if (!!streamId) {
+      this.redirectToStream.emit(streamId);
+    }
+  }
+
+  public doActionDuplicate(streamId: number): void {
+    if (!!streamId) {
+      this.actionDuplicate.emit(streamId);
+    }
+  }
+  public doActionEdit(streamId: number): void {
+    if (!!streamId) {
+      this.actionEdit.emit(streamId);
+    }
+  }
+  public doActionDelete(streamDto: StreamDto): void {
+    if (!!streamDto) {
+      this.actionDelete.emit(streamDto);
+    }
+  }
+
+  // ** Private API **
+
+  private checkAndGetNextPage(pageData: PageData): void {
+    if (pageData.page < pageData.pages) {
+      pageData.page = pageData.page + 1;
+      this.requestNextPage.emit(PageDataUtil.create(pageData));
+    }
+  }
+
+  private prepareNextStreamsData(streamListDto: StreamListDto): void {
+    if (1 === streamListDto.page) {
+      this.streamDTOList.length = 0;
+      this.scrollToTop();
+    }
+    this.streamDTOList.push(...streamListDto.list);
+    this.pageData = PageDataUtil.create(streamListDto);
+  }
+
+  private scrollToTop(): void {
+    if (!!this.element) {
+      try {
+        this.element.nativeElement.scrollTop = 0;
+      } catch (err) { }
+    }
+  }
+
+  private emptyStreamListDto(): StreamListDto {
+    return { list: [], limit: 0, count: 0, page: 0, pages: -1 };
+  }
+}
