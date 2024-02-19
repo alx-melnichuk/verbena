5c7563ee3ae50b07014bea5273104ed57fd53c2e
alx.melnichuk <alx.melnichuk@gmail.com>
2024-02-19 18:08:53 +0200

    FE: Added "Logotype".

---
+++ frontend/src/app/components/logotype/logotype.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-logotype',
+  standalone: true,
+  imports: [CommonModule, TranslateModule],
+  templateUrl: './logotype.component.html',
+  styleUrls: ['./logotype.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LogotypeComponent {
+  @Input()
+  public logo: string | null = null;
+}
