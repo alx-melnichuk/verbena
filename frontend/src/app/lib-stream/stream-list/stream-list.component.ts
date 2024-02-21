6fdb130f4ac6899150d5301cb782d8d00319ff1b
alx.melnichuk <alx.melnichuk@gmail.com>
2024-02-21 22:53:19 +0200

    FE: modify 'StreamList'

--- frontend/src/app/lib-stream/stream-list/stream-list.component.ts
+++ frontend/src/app/lib-stream/stream-list/stream-list.component.ts
@@ -12,13 +12,15 @@ import { HttpErrorUtil } from 'src/app/utils/http-error.util';
 
 import { StringDateTime, StreamDtoUtil, StreamDto } from '../stream-api.interface';
 import { StreamListService } from '../stream-list.service';
+import { PanelStreamEventComponent } from '../panel-stream-event/panel-stream-event.component';
 import { PanelStreamInfoComponent } from '../panel-stream-info/panel-stream-info.component';
 import { StreamService } from '../stream.service';
+import { StreamCalendarService } from '../stream-calendar.service';
 
 @Component({
   selector: 'app-stream-list',
   standalone: true,
-  imports: [CommonModule, TranslateModule, SpinnerComponent, PanelStreamInfoComponent],
+  imports: [CommonModule, TranslateModule, SpinnerComponent, PanelStreamEventComponent, PanelStreamInfoComponent],
   templateUrl: './stream-list.component.html',
   styleUrls: ['./stream-list.component.scss'],
   encapsulation: ViewEncapsulation.None,
@@ -37,6 +39,7 @@ export class StreamListComponent {
     private streamService: StreamService,
     private alertService: AlertService,
     public streamListService: StreamListService,
+    public streamCalendarService: StreamCalendarService,
     // public scheduleService: ScheduleService,
   ) {
     console.log(`StreamListComponent()`); // #-
@@ -75,24 +78,21 @@ export class StreamListComponent {
   }
 
   public doRedirectToStreamView(streamId: number): void {
-    /*if (!!streamId) {
+    if (!!streamId) {
       this.streamService.redirectToStreamViewPage(streamId);
-    }*/
+    }
   }
 
-  // "Panel Calendar"
+  // "Panel Calendar and Events"
 
-  /*public async doChangeSelectedDate(selectedDate: StringDate): Promise<null | HttpErrorResponse> {
-    if (!selectedDate) {
-      return null;
-    }
-    const userId = (this.userId as string);
-    await this.scheduleService.setSelectedDateAndGetMiniStreams(userId, selectedDate)
-      .finally(() => {
+  public async doChangeSelectedDate(selectedDate?: StringDateTime): Promise<void> {
+    const starttime: StringDateTime = selectedDate || this.streamCalendarService.streamsEventStarttime;
+    // Get a list of events (streams) for a specified date.
+    await this.streamCalendarService.setSelectedDateAndGetShortStreams(starttime)
+    .finally(() => {
         this.changeDetector.markForCheck();
-      });
-    return null;
-  }*/
+    });
+  }
 
   /*public async doChangeActiveDate(activeDateStr: StringDate): Promise<null | HttpErrorResponse> {
     if (!activeDateStr) {
@@ -116,6 +116,7 @@ export class StreamListComponent {
 
     // this.doChangeActiveDate(this.scheduleService.activeDate.format(MOMENT_ISO8601_DATE));
     // this.doChangeSelectedDate(this.scheduleService.selectedDate);
+    this.doChangeSelectedDate();
   }
 
   // "Streams"
