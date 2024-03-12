033c36b81eb423e69bf1cb7477d7935d16ca7700
alx.melnichuk <alx.melnichuk@gmail.com>
2024-03-12 17:28:59 +0200

    FE: added 'isShowMyStreams' to 'MainMenu'.

--- frontend/src/app/components/main-menu/main-menu.component.ts
+++ frontend/src/app/components/main-menu/main-menu.component.ts
@@ -5,13 +5,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
+
 import { InitializationService } from 'src/app/common/initialization.service';
+import { ROUTE_STREAM_LIST } from 'src/app/common/routes';
 
 
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatMenuModule, MatButtonModule, MatSlideToggleModule, TranslateModule],
+  imports: [CommonModule, FormsModule, MatMenuModule, MatButtonModule, MatSlideToggleModule, TranslateModule,  RouterLink],
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.scss'],
   encapsulation: ViewEncapsulation.None,
@@ -19,11 +22,13 @@ import { InitializationService } from 'src/app/common/initialization.service';
 })
 export class MainMenuComponent implements OnInit {
   @Input()
-  public isDisabledMenu = false;
+  public isDisabledMenu: boolean = false;
   @Input()
-  public isShowDarkTheme = false;
+  public isShowMyStreams: boolean = false;
   @Input()
-  public isShowLogout = false;
+  public isShowDarkTheme: boolean = false;
+  @Input()
+  public isShowLogout: boolean = false;
 
 
   @Output()
@@ -32,6 +37,7 @@ export class MainMenuComponent implements OnInit {
   readonly logout: EventEmitter<void> = new EventEmitter();
   
   public isDarkTheme = false;
+  public linkMyStreams = ROUTE_STREAM_LIST;
 
   constructor(public initializationService: InitializationService) {
   }
