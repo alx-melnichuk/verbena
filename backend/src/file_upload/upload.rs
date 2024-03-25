dcf4951fa89695894108838cfafc0615f3469068
alx.melnichuk <alx.melnichuk@gmail.com>
2024-03-25 22:12:50 +0200

    BE: Added method for converting and reducing image size.

--- backend/src/file_upload/upload.rs
+++ backend/src/file_upload/upload.rs
@@ -1,6 +1,62 @@
-use std::{self, ffi::OsStr, path::PathBuf};
+use std::{self, ffi::OsStr, fs::File, path::PathBuf};
 
 use image::{imageops::FilterType, DynamicImage, GenericImageView, ImageFormat};
+
+/** Convert the file to another mime type. */
+
+pub fn convert_file2(source: &str, extension: &str, max_width: u32, max_height: u32) -> Result<String, String> {
+    let mut path = PathBuf::from(source);
+
+    let source_ext = path.extension().unwrap_or(OsStr::new("")).to_str().unwrap().to_string();
+    // Check the input file image type is correct.
+    let opt_source_type = ImageFormat::from_extension(&source_ext);
+    if opt_source_type.is_none() {
+        return Err(format!("Invalid source file image type \"{}\".", &source_ext));
+    }
+
+    // Check the output file image type is correct.
+    let opt_receiver_type = ImageFormat::from_extension(extension);
+    if opt_receiver_type.is_none() {
+        return Err(format!("Invalid receiver file image type \"{}\".", extension));
+    }
+
+    // If the image type does not change and the maximum dimensions are not specified, then exit.
+    if source_ext.eq(extension) && max_width == 0 && max_height == 0 {
+        return Ok(source.to_string());
+    }
+
+    path.set_extension(extension);
+    let receiver = path.to_str().unwrap();
+    // Load the source image into memory.
+    let mut image_source: DynamicImage = image::open(source).unwrap();
+
+    if max_width > 0 || max_height > 0 {
+        // Get the width and height of this image.
+        let (curr_width, curr_height) = image_source.dimensions();
+
+        #[rustfmt::skip]
+        let mut nwidth: u32 = curr_width;
+        if max_width > 0 && max_width < curr_width {
+            nwidth = max_width;
+        }
+        let mut nheight: u32 = curr_height;
+        if max_height > 0 && max_height < curr_height {
+            nheight = max_height
+        }
+        // Scale this image down to fit within a specific size. Returns a new image.
+        // The image's aspect ratio is preserved.
+        // The image is scaled to the maximum possible size that fits within the bounds specified by nwidth and nheight.
+        image_source = image_source.thumbnail(nwidth, nheight);
+    }
+
+    // Save the image from memory to the receiver.
+    let result = image_source.save(receiver).map_err(|err| err.to_string());
+
+    match result {
+        Ok(()) => Ok(receiver.to_string()),
+        Err(err) => Err(err),
+    }
+}
 
 /** Convert the file to another mime type. */
 pub fn convert_file(source: &str, receiver: &str, _min_width: u32, _min_height: u32) -> Result<String, String> {
