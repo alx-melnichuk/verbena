64a81473c829d80df8ee7123d75024388831abdb
alx.melnichuk <alx.melnichuk@gmail.com>
2024-04-03 19:10:07 +0300

    BE: Added tests to `env_data`.

--- backend/src/tools/evn_data.rs
+++ backend/src/tools/evn_data.rs
@@ -16,14 +16,14 @@ const MSG_MAX_PRM_IS_NOT_SPECIFIED: &str = "The maximum parameter length is not 
 /// * key_file_path: `&str` - Private key file in PEM encoding format.
 /// * param_len: `usize` - If the parameter length is less than the specified value, then it needs to be encoded.
 ///                        If it is greater, then the parameter value is already encoded.
-/// * Returns: `bool` - If true, the parameters were encoded.
+/// * Returns: `Vec<String>` - List of parameter names that were encrypted.
 ///
 pub fn check_params_env(
     env_file_path: &str,
     param_list: &[&str],
     key_file_path: &str,
     param_len: usize,
-) -> Result<bool, String> {
+) -> Result<Vec<String>, String> {
     if env_file_path.len() == 0 {
         return Err(MSG_ENV_IS_NOT_SPECIFIED.to_string());
     }
@@ -42,7 +42,7 @@ pub fn check_params_env(
     let file = fs::File::open(env_file_path).map_err(|err| err.to_string())?;
     let reader = io::BufReader::new(file);
 
-    let mut amount_crypto = 0;
+    let mut encrypted_params: Vec<String> = Vec::new();
     if param_list.len() > 0 {
         // Read the file line by line using the lines() iterator from std::io::BufRead.
         for line in reader.lines() {
@@ -58,7 +58,7 @@ pub fn check_params_env(
                 continue;
             }
             let value = if param_list.contains(&prm_name) && prm_value.len() < param_len {
-                amount_crypto = amount_crypto + 1;
+                encrypted_params.push(prm_name.to_string());
                 crypto::encrypt_utf8(&pr_key_pem, &prm_value.as_bytes()).map_err(|err| err.to_string())?
             } else {
                 prm_value.to_string()
@@ -69,7 +69,7 @@ pub fn check_params_env(
         }
     }
 
-    if amount_crypto > 0 {
+    if encrypted_params.len() > 0 {
         // Get the name for the old file.
         let mut env_old_path = path::PathBuf::from(&env_file_path);
         env_old_path.set_extension("old");
@@ -86,44 +86,51 @@ pub fn check_params_env(
         }
     }
 
-    Ok(amount_crypto > 0)
+    Ok(encrypted_params)
 }
 /// Update configurations and decryption of specified parameters.
 /// * param_list: `&[&str]` - List of parameters to be encoded.
 /// * key_file_path: `&str` - Private key file in PEM encoding format.
 /// * param_len: `usize` - If the parameter length is less than the specified value, then it needs to be encoded.
 ///                        If it is greater, then the parameter value is already encoded.
-/// * Returns: `()`
+/// * Returns: `Vec<String>` - List of parameter names that were decrypted.
 ///
-pub fn update_params_env(param_list: &[&str], key_file_path: &str, param_len: usize) -> Result<bool, String> {
+pub fn update_params_env(param_list: &[&str], key_file_path: &str, param_len: usize) -> Result<Vec<String>, String> {
     if key_file_path.len() == 0 {
         return Err(MSG_PKEY_IS_NOT_SPECIFIED.to_string());
     }
     if param_len == 0 {
         return Err(MSG_MAX_PRM_IS_NOT_SPECIFIED.to_string());
     }
-
     let file_path = path::PathBuf::from(key_file_path);
     let pr_key_pem = fs::read_to_string(file_path).map_err(|err| err.to_string())?;
-    let mut amount_crypto = 0;
+    let mut decrypted_params: Vec<String> = Vec::new();
     for param in param_list.iter() {
         let prm_name = *param;
         let prm_value = std::env::var(prm_name).unwrap_or("".to_string());
         if prm_name.len() > 0 && prm_value.len() >= param_len {
-            eprintln!("prm_name: {}, prm_value: {}", prm_name, prm_value);
             let value = crypto::decrypt_utf8(&pr_key_pem, &prm_value).map_err(|err| err.to_string())?;
             let value_str = std::str::from_utf8(&value).unwrap();
             std::env::set_var(prm_name, value_str);
-            amount_crypto = amount_crypto + 1;
+            decrypted_params.push(prm_name.to_string());
         }
     }
 
-    Ok(amount_crypto > 0)
+    Ok(decrypted_params)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const DATABASE_NAME: &str = "TEST_DATABASE_URL";
+    const DATABASE_VALUE: &str = "postgresql://database_user:database_password@127.0.0.1:5432/database_name";
+    const HOST_PORT_NAME: &str = "TEST_SMTP_HOST_PORT";
+    const HOST_PORT_VALUE: &str = "smtp.demo.com:9999";
+    const USER_PASS_NAME: &str = "TEST_SMTP_USER_PASS";
+    const USER_PASS_VALUE: &str = "user_demo@demo.com:user_demo";
+    const SECRET_KEY_NAME: &str = "TEST_JWT_SECRET_KEY";
+    const SECRET_KEY_VALUE: &str = "jwt_secret_key";
 
     /// Get the private key in PEM encoded format.
     fn get_pr_key_pem() -> String {
@@ -159,6 +166,7 @@ mod tests {
         result.join("")
     }
 
+    // ** check_params_env **
     #[test]
     fn test_check_params_env_empty_env_file() {
         let list: Vec<&str> = Vec::new();
@@ -186,63 +194,71 @@ mod tests {
     #[test]
     fn test_check_params_env_empty_param_list() {
         // Create private key in PEM encoded format.
-        let pr_key_pem_path = "./demo01.key.pem";
+        let pr_key_pem_path = "./demo11.key.pem";
         let mut file = fs::File::create(&pr_key_pem_path).unwrap();
         let pr_key_pem_str = get_pr_key_pem();
         file.write(&pr_key_pem_str.as_bytes()).unwrap();
 
         // Creating the "env" configuration file.
-        let env_demo_path = "./demo01_env";
+        let env_demo_path = "./demo11_env";
         let mut list: Vec<&str> = Vec::new();
-        list.push("DATABASE_URL=postgresql://database_user:database_password@127.0.0.1:5432/database_name");
-        list.push("SMTP_HOST_PORT=smtp.demo.com:9999");
-        list.push("SMTP_USER_PASS=user_demo@demo.com:user_demo");
-        list.push("JWT_SECRET_KEY=jwt_secret_key");
+        let s1 = format!("{}={}", DATABASE_NAME, DATABASE_VALUE);
+        list.push(&s1);
+        let s2 = format!("{}={}", HOST_PORT_NAME, HOST_PORT_VALUE);
+        list.push(&s2);
+        let s3 = format!("{}={}", USER_PASS_NAME, USER_PASS_VALUE);
+        list.push(&s3);
+        let s4 = format!("{}={}", SECRET_KEY_NAME, SECRET_KEY_VALUE);
+        list.push(&s4);
+
         let mut file = fs::File::create(&env_demo_path).unwrap();
         for line in list.iter() {
             file.write(&format!("{}\n", &line).as_bytes()).unwrap();
         }
 
         // Creating a list of parameters.
-        let list: Vec<&str> = Vec::new();
-        let result = check_params_env(env_demo_path, &list, &pr_key_pem_path, 500);
+        let prms: Vec<&str> = Vec::new();
+        let result = check_params_env(env_demo_path, &prms, &pr_key_pem_path, 500);
         let _ = fs::remove_file(&env_demo_path);
         let _ = fs::remove_file(&pr_key_pem_path);
 
         assert!(result.is_ok());
-        assert_eq!(result.ok(), Some(false));
+        assert_eq!(result.ok(), Some(vec![]));
     }
-
     #[test]
     fn test_check_params_env_valid_data() {
         // Create private key in PEM encoded format.
-        let pr_key_pem_path = "./demo02.key.pem";
+        let pr_key_pem_path = "./demo12.key.pem";
         let mut file = fs::File::create(&pr_key_pem_path).unwrap();
         let pr_key_pem_str = get_pr_key_pem();
         file.write(&pr_key_pem_str.as_bytes()).unwrap();
 
         // Creating the "env" configuration file.
-        let env_demo_path = "./demo02_env";
+        let env_demo_path = "./demo12_env";
         if path::Path::new(&env_demo_path).exists() {
             let _ = fs::remove_file(&env_demo_path);
         }
         let mut list: Vec<&str> = Vec::new();
-        list.push("DATABASE_URL=postgresql://database_user:database_password@127.0.0.1:5432/database_name");
-        list.push("SMTP_HOST_PORT=smtp.demo.com:9999");
-        list.push("SMTP_USER_PASS=user_demo@demo.com:user_demo");
-        list.push("JWT_SECRET_KEY=jwt_secret_key");
+        let s1 = format!("{}={}", DATABASE_NAME, DATABASE_VALUE);
+        list.push(&s1);
+        let s2 = format!("{}={}", HOST_PORT_NAME, HOST_PORT_VALUE);
+        list.push(&s2);
+        let s3 = format!("{}={}", USER_PASS_NAME, USER_PASS_VALUE);
+        list.push(&s3);
+        let s4 = format!("{}={}", SECRET_KEY_NAME, SECRET_KEY_VALUE);
+        list.push(&s4);
         let mut file = fs::File::create(&env_demo_path).unwrap();
         for line in list.iter() {
             file.write(&format!("{}\n", &line).as_bytes()).unwrap();
         }
 
         // Creating a list of parameters.
-        let mut list: Vec<&str> = Vec::new();
-        list.push("DATABASE_URL");
-        list.push("SMTP_HOST_PORT");
-        list.push("SMTP_USER_PASS");
-
-        let result = check_params_env(env_demo_path, &list, &pr_key_pem_path, 500);
+        let mut prms: Vec<&str> = Vec::new();
+        prms.push(DATABASE_NAME);
+        prms.push(HOST_PORT_NAME);
+        prms.push(USER_PASS_NAME);
+        let prms2: Vec<String> = prms.clone().iter().map(|v| v.to_string()).collect();
+        let result = check_params_env(env_demo_path, &prms, &pr_key_pem_path, 500);
         let _ = fs::remove_file(&env_demo_path);
         let _ = fs::remove_file(&pr_key_pem_path);
 
@@ -255,20 +271,136 @@ mod tests {
         }
 
         assert!(result.is_ok());
-        assert_eq!(result.ok(), Some(true));
+        assert_eq!(result.ok(), Some(prms2));
         assert_eq!(is_exist_old_file, true);
     }
 
-    /*#[test]
-    fn test_demo2() {
-        eprintln!("test_demo2()");
-        let mut list: Vec<&str> = Vec::new();
-        list.push("DATABASE_URL");
-        list.push("SMTP_HOST_PORT");
-        list.push("SMTP_USER_PASS");
+    // ** update_params_env **
+    #[test]
+    fn test_update_params_env_empty_key_file() {
+        let list: Vec<&str> = Vec::new();
+        let result = update_params_env(&list, &"", 500);
 
-        let result = update_params_env(&list, "./example.key.pem", 500);
-        eprintln!("result: {:?}", result);
+        assert!(result.is_err());
+        assert_eq!(result.err(), Some(MSG_PKEY_IS_NOT_SPECIFIED.to_string()));
+    }
+    #[test]
+    fn test_update_params_env_param_len_is_0() {
+        let list: Vec<&str> = Vec::new();
+        let result = update_params_env(&list, &"./demo02.key.pem", 0);
+
+        assert!(result.is_err());
+        assert_eq!(result.err(), Some(MSG_MAX_PRM_IS_NOT_SPECIFIED.to_string()));
+    }
+    #[test]
+    fn test_update_params_env_empty_param_list() {
+        // Create private key in PEM encoded format.
+        let pr_key_pem_path = "./demo21.key.pem";
+        let mut file = fs::File::create(&pr_key_pem_path).unwrap();
+        let pr_key_pem_str = get_pr_key_pem();
+        file.write(&pr_key_pem_str.as_bytes()).unwrap();
+
+        // Creating the "env" configuration file.
+        let env_demo_path = "./demo21_env";
+        let mut list: Vec<&str> = Vec::new();
+        let s1 = format!("{}={}", DATABASE_NAME, DATABASE_VALUE);
+        list.push(&s1);
+        let s2 = format!("{}={}", HOST_PORT_NAME, HOST_PORT_VALUE);
+        list.push(&s2);
+        let s3 = format!("{}={}", USER_PASS_NAME, USER_PASS_VALUE);
+        list.push(&s3);
+        let s4 = format!("{}={}", SECRET_KEY_NAME, SECRET_KEY_VALUE);
+        list.push(&s4);
+
+        let mut file = fs::File::create(&env_demo_path).unwrap();
+        for line in list.iter() {
+            file.write(&format!("{}\n", &line).as_bytes()).unwrap();
+        }
+
+        // Creating a list of parameters.
+        let prms: Vec<&str> = Vec::new();
+        let result = update_params_env(&prms, &pr_key_pem_path, 500);
+        let _ = fs::remove_file(&env_demo_path);
+        let _ = fs::remove_file(&pr_key_pem_path);
+
         assert!(result.is_ok());
-    }*/
+        assert_eq!(result.ok(), Some(vec![]));
+    }
+    #[test]
+    fn test_update_params_env_valid_data_no_encoded() {
+        // Create private key in PEM encoded format.
+        let pr_key_pem_path = "./demo22.key.pem";
+        let mut file = fs::File::create(&pr_key_pem_path).unwrap();
+        let pr_key_pem_str = get_pr_key_pem();
+        file.write(&pr_key_pem_str.as_bytes()).unwrap();
+
+        std::env::set_var(DATABASE_NAME, DATABASE_VALUE);
+        std::env::set_var(HOST_PORT_NAME, HOST_PORT_VALUE);
+        std::env::set_var(USER_PASS_NAME, USER_PASS_VALUE);
+
+        // Creating a list of parameters.
+        let mut prms: Vec<&str> = Vec::new();
+        prms.push(DATABASE_NAME);
+        prms.push(HOST_PORT_NAME);
+        prms.push(USER_PASS_NAME);
+
+        let result = update_params_env(&prms, &pr_key_pem_path, 500);
+
+        let _ = fs::remove_file(&pr_key_pem_path);
+
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(vec![]));
+
+        let dbase2 = std::env::var(DATABASE_NAME).unwrap_or("".to_string());
+        assert_eq!(DATABASE_VALUE, dbase2);
+
+        let host_port2 = std::env::var(HOST_PORT_NAME).unwrap_or("".to_string());
+        assert_eq!(HOST_PORT_VALUE, host_port2);
+
+        let user_pass2 = std::env::var(USER_PASS_NAME).unwrap_or("".to_string());
+        assert_eq!(USER_PASS_VALUE, user_pass2);
+    }
+    #[test]
+    fn test_update_params_env_valid_data_encoded() {
+        // Create private key in PEM encoded format.
+        let pr_key_pem_path = "./demo23.key.pem";
+        let mut file = fs::File::create(&pr_key_pem_path).unwrap();
+        let pr_key_pem_str = get_pr_key_pem();
+        file.write(&pr_key_pem_str.as_bytes()).unwrap();
+
+        let file_path = path::PathBuf::from(pr_key_pem_path);
+        let pr_key_pem = fs::read_to_string(file_path).unwrap();
+
+        let dbase = crypto::encrypt_utf8(&pr_key_pem, &DATABASE_VALUE.as_bytes()).unwrap();
+        std::env::set_var(DATABASE_NAME, dbase);
+
+        let host_port = crypto::encrypt_utf8(&pr_key_pem, &HOST_PORT_VALUE.as_bytes()).unwrap();
+        std::env::set_var(HOST_PORT_NAME, host_port);
+
+        let user_pass = crypto::encrypt_utf8(&pr_key_pem, &USER_PASS_VALUE.as_bytes()).unwrap();
+        std::env::set_var(USER_PASS_NAME, user_pass);
+
+        // Creating a list of parameters.
+        let mut prms: Vec<&str> = Vec::new();
+        prms.push(DATABASE_NAME);
+        prms.push(HOST_PORT_NAME);
+        prms.push(USER_PASS_NAME);
+        let prms2: Vec<String> = prms.clone().iter().map(|v| v.to_string()).collect();
+
+        let result = update_params_env(&prms, &pr_key_pem_path, 300);
+
+        let _ = fs::remove_file(&pr_key_pem_path);
+
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(prms2));
+
+        let dbase2 = std::env::var(DATABASE_NAME).unwrap_or("".to_string());
+        assert_eq!(DATABASE_VALUE, dbase2);
+
+        let host_port2 = std::env::var(HOST_PORT_NAME).unwrap_or("".to_string());
+        assert_eq!(HOST_PORT_VALUE, host_port2);
+
+        let user_pass2 = std::env::var(USER_PASS_NAME).unwrap_or("".to_string());
+        assert_eq!(USER_PASS_VALUE, user_pass2);
+    }
 }
