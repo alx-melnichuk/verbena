38b2014417a6b5529691f06a78c71a1570d78730
alx.melnichuk <alx.melnichuk@gmail.com>
2024-04-03 19:09:03 +0300

    BE: Added `parse_bool` to `parser`.

--- backend/src/utils/parser.rs
+++ backend/src/utils/parser.rs
@@ -1,5 +1,6 @@
 pub const CD_PARSE_INT_ERROR: &str = "ParseIntError";
-pub const MSG_PARSE_INT_ERROR: &str = "Failed conversion to i32 from";
+pub const MSG_PARSE_INT_ERROR: &str = "Failed conversion to `i32` from";
+pub const MSG_PARSE_BOOL_ERROR: &str = "Failed conversion to `bool` from";
 
 // let val = parser::parse_i32("1a").map_err(|err| {
 //     AppError::new(CD_PARSE_INT_ERROR, &format!("id: {}", err)).set_status(400)
@@ -7,4 +8,8 @@ pub const MSG_PARSE_INT_ERROR: &str = "Failed conversion to i32 from";
 pub fn parse_i32(val: &str) -> Result<i32, String> {
     val.parse::<i32>()
         .map_err(|e| format!("{} `{}` - {}", MSG_PARSE_INT_ERROR, val, e.to_string()))
+}
+pub fn parse_bool(val: &str) -> Result<bool, String> {
+    val.parse::<bool>()
+        .map_err(|e| format!("{} `{}` - {}", MSG_PARSE_BOOL_ERROR, val, e.to_string()))
 }
