5b742d7a4d29de70f2737204066f2fb86a871ee8
alx.melnichuk <alx.melnichuk@gmail.com>
2024-04-29 14:29:11 +0300

    FE: Added 'ValidatorUtils'.

---
+++ frontend/src/app/utils/validator.utils.ts
@@ -0,0 +1,46 @@
+import { ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+export class ValidatorUtils {
+  // Create an array of check rules based on the specified parameters.
+  public static prepare(params: { [key: string]: any; }): ValidatorFn[] {
+    const resultValidator: ValidatorFn[] = [];
+    if (params["min"] > -1) {
+      resultValidator.push(Validators.min(params["min"]));
+    }
+    if (params["max"] > -1) {
+      resultValidator.push(Validators.max(params["max"]));
+    }
+    if (!!params["required"]) {
+      resultValidator.push(Validators.required);
+    }
+    if (!!params["email"]) {
+      resultValidator.push(Validators.email);
+    }
+    const minLength = 0 + (params["minLength"] || -1);
+    if (minLength > -1) {
+      resultValidator.push(Validators.minLength(minLength));
+    }
+    const maxLength = 0 + (params["maxLength"] || -1);
+    if (maxLength > -1) {
+      resultValidator.push(Validators.maxLength(maxLength));
+    }
+    if (!!params["pattern"]) {
+      resultValidator.push(Validators.pattern(params["pattern"]));
+    }
+    return resultValidator;
+  }
+  // Get an error template based on the specified parameters.
+  public static getErrorMsg(errors: ValidationErrors | null, name: string): string {
+    let result: string = '';
+    const errorsProps: string[] = errors != null ? Object.keys(errors) : [];
+    for (let index = 0; index < errorsProps.length && !result; index++) {
+      const error: string = errorsProps[index];
+      result = !result && 'required' === error ? `Validation.${name}:required` : result;
+      result = !result && 'minlength' === error ? `Validation.${name}:min_length` : result;
+      result = !result && 'maxlength' === error ? `Validation.${name}:max_length` : result;
+      result = !result && 'pattern' === error ? `Validation.${name}:regex` : result;
+      result = !result && 'email' === error ? `Validation.${name}:email_type` : result;
+    }
+    return result;
+  }
+}
