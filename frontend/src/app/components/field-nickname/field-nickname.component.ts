1ca3d40ca538faa6892840737fe020e9216b75c2
alx.melnichuk <alx.melnichuk@gmail.com>
2024-04-29 14:31:15 +0300

    FE : Added use of 'ValidatorUtils'

--- frontend/src/app/components/field-nickname/field-nickname.component.ts
+++ frontend/src/app/components/field-nickname/field-nickname.component.ts
@@ -20,12 +20,13 @@ import {
   ValidationErrors,
   Validator,
   ValidatorFn,
-  Validators,
 } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput, MatInputModule } from '@angular/material/input';
 import { TranslateModule } from '@ngx-translate/core';
+import { ValidatorUtils } from 'src/app/utils/validator.utils';
 
+export const NICKNAME = 'nickname';
 export const NICKNAME_MIN_LENGTH = 3;
 export const NICKNAME_MAX_LENGTH = 64;
 export const NICKNAME_PATTERN = '^[a-zA-Z]+[\\w]+$';
@@ -45,21 +46,25 @@ export const NICKNAME_PATTERN = '^[a-zA-Z]+[\\w]+$';
 })
 export class FieldNicknameComponent implements OnChanges, ControlValueAccessor, Validator {
   @Input()
+  public gist: string = NICKNAME;
+  @Input()
+  public hint: string = '';
+  @Input()
+  public isDisabled: boolean = false;
+  @Input()
   public isReadOnly: boolean = false;
   @Input()
   public isRequired: boolean = false;
   @Input()
   public label: string = 'field-nickname.label';
   @Input()
-  public minLen: number = NICKNAME_MIN_LENGTH;
-  @Input()
   public maxLen: number = NICKNAME_MAX_LENGTH;
+  @Input()
+  public minLen: number = NICKNAME_MIN_LENGTH;
   @Input()
   public pattern: string = NICKNAME_PATTERN;
   @Input()
-  public isDisabled: boolean = false;
-  @Input()
-  public hint: string = '';
+  public type: string = "text";
 
   @ViewChild(MatInput, { static: false })
   public matInput: MatInput | null = null;
@@ -71,7 +76,7 @@ export class FieldNicknameComponent implements OnChanges, ControlValueAccessor, 
   constructor() {}
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (!!changes['isRequired'] || !!changes['minLen'] || !!changes['maxLen']) {
+    if (!!changes['isRequired'] || !!changes['minLen'] || !!changes['maxLen'] || !!changes['pattern'] || !!changes['type']) {
       this.prepareFormGroup();
     }
     if (!!changes['isDisabled']) {
@@ -117,27 +122,22 @@ export class FieldNicknameComponent implements OnChanges, ControlValueAccessor, 
   }
 
   public getErrorMsg(errors: ValidationErrors | null): string {
-    let result: string = '';
-    const errorsProps: string[] = errors != null ? Object.keys(errors) : [];
-    for (let index = 0; index < errorsProps.length && !result; index++) {
-      const error: string = errorsProps[index];
-      result = !result && 'required' === error ? 'Validation.nickname:required' : result;
-      result = !result && 'minlength' === error ? 'Validation.nickname:min_length' : result;
-      result = !result && 'maxlength' === error ? 'Validation.nickname:max_length' : result;
-      result = !result && 'pattern' === error ? 'Validation.nickname:regex' : result;
-    }
-    return result;
+    return ValidatorUtils.getErrorMsg(errors, this.gist || NICKNAME);
   }
 
   // ** Private API **
 
   private prepareFormGroup(): void {
     this.formControl.clearValidators();
-    const newValidator: ValidatorFn[] = [
-      ...(this.isRequired ? [Validators.required] : []),
-      ...(this.minLen > 0 ? [Validators.minLength(this.minLen)] : []),
-      ...(this.maxLen > 0 ? [Validators.maxLength(this.maxLen)] : []),
-    ];
+    const paramsObj = {
+      ...(this.isRequired ? { "required": true } : {}),
+      ...(this.minLen > 0 ? { "minLength": this.minLen } : {}),
+      ...(this.maxLen > 0 ? { "maxLength": this.maxLen } : {}),
+      ...(this.pattern ? { "pattern": this.pattern } : {}),
+      ...(this.type == "email" ? { "email": true } : {})
+    };
+    const newValidator: ValidatorFn[] = ValidatorUtils.prepare(paramsObj);
     this.formControl.setValidators(newValidator);
+    this.formControl.updateValueAndValidity();
   }
 }
