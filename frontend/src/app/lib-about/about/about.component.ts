83419c0d42cb387163e6544f0321cbf8d23a1333
alx.melnichuk <alx.melnichuk@gmail.com>
2024-06-01 17:37:19 +0300

    FE: Modification of the template.

--- frontend/src/app/lib-about/about/about.component.ts
+++ frontend/src/app/lib-about/about/about.component.ts
@@ -17,15 +17,20 @@ export class AboutComponent {
   public title: string = '';
   public label: string = '';
   public frontendItems: string[] = [];
-  public backendItem01 = APP_ABOUT['backend01'] || '';
-  public backendItem02: string[] = APP_ABOUT['backend02'] || [];
-  public backendItem03: string[] = APP_ABOUT['backend03'] || [];
+  public backendItem01 = this.appAbout['backend01'] || '';
+  public backendItem02: string[] = this.appAbout['backend02'] || [];
+  public backendItem03: string[] = this.appAbout['backend03'] || [];
   public linkSwaggerUi = '/swagger-ui/';
   public linkRapidoc = '/rapidoc';
   public linkRedoc = '/redoc';
 
+  public get appAbout(): any {
+    return APP_ABOUT || {};
+  }
+  public set appAbout(value: any) {
+  }
+
   constructor(private translate: TranslateService,) {
-    console.log('PgAboutComponent();');
     const appName = this.translate.instant('app_name') || '';
     this.title = this.translate.instant('about.title', { app_name: appName }) || '';
     this.label = this.translate.instant('about.label', { app_name: appName }) || '';
@@ -35,5 +40,5 @@ export class AboutComponent {
   // ** Public API **
   
   // ** Private API **
-  
+    
 }
