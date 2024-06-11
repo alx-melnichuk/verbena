4dcf37550524ddf622011915bfa76eeef3fca942
alx.melnichuk <alx.melnichuk@gmail.com>
2024-06-11 17:28:26 +0300

    FE: The styles for "panel-profile-info" have been changed.

--- frontend/src/app/lib-profile/panel-profile-info/panel-profile-info.component.ts
+++ frontend/src/app/lib-profile/panel-profile-info/panel-profile-info.component.ts
@@ -3,19 +3,22 @@ import {
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { TranslateModule } from '@ngx-translate/core';
 
-import { FieldNicknameComponent } from 'src/app/components/field-nickname/field-nickname.component';
-import { UserDto, UserDtoUtil } from 'src/app/lib-user/user-api.interface';
+import { FieldDescriptComponent } from 'src/app/components/field-descript/field-descript.component';
 import { FieldEmailComponent } from 'src/app/components/field-email/field-email.component';
+import { FieldNicknameComponent } from 'src/app/components/field-nickname/field-nickname.component';
 import { FieldPasswordComponent } from 'src/app/components/field-password/field-password.component';
-import { MatButtonModule } from '@angular/material/button';
+import { FieldFileUploadComponent } from 'src/app/components/field-file-upload/field-file-upload.component';
+import { UserDto, UserDtoUtil }   from 'src/app/lib-user/user-api.interface';
+import { IMAGE_VALID_FILE_TYPES, MAX_FILE_SIZE } from 'src/app/common/constants';
 
 @Component({
   selector: 'app-panel-profile-info',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, MatButtonModule, TranslateModule, FieldNicknameComponent, FieldEmailComponent,
-    FieldPasswordComponent],
+    FieldPasswordComponent, FieldDescriptComponent, FieldFileUploadComponent],
   templateUrl: './panel-profile-info.component.html',
   styleUrls: ['./panel-profile-info.component.scss'],
   encapsulation: ViewEncapsulation.None,
@@ -30,12 +33,17 @@ export class PanelProfileInfoComponent implements OnChanges {
   public errMsgList: string[] = [];
   
   public controls = {
+    avatar: new FormControl(null, []),
     nickname: new FormControl(null, []),
     email: new FormControl(null, []),
     password: new FormControl(null, []),
+    descript: new FormControl(null, []),
   };
   public formGroup: FormGroup = new FormGroup(this.controls);
 
+  public maxFileSize = MAX_FILE_SIZE;
+  public validFileTypes = IMAGE_VALID_FILE_TYPES;
+  public addedLogoView: string = '';
   private origUserDto: UserDto = UserDtoUtil.create();
 
   constructor() {
@@ -49,6 +57,30 @@ export class PanelProfileInfoComponent implements OnChanges {
  
   // ** Public API **
   
+  public addFile(file: File): void {
+    // this.addedLogoFile = file;
+    // this.controls.logo.setValue(file.name);
+    // this.controls.logo.markAsDirty();
+  }
+
+  public readFile(buffFile: string[]): void {
+    if (buffFile.length > 0) {
+      this.addedLogoView = buffFile[1];
+    //   this.changeDetectorRef.markForCheck();
+    }
+  }
+
+  public deleteFileLogo(): void {
+    // this.addedLogoFile = (!!this.origLogo ? null : undefined);
+    this.addedLogoView = '';
+    // this.controls.logo.setValue(null);
+    // if (!!this.origLogo) {
+    //   this.controls.logo.markAsDirty();
+    // } else {
+    //   this.controls.logo.markAsPristine();
+    // }
+  }
+
   public updateErrMsg(errMsgList: string[] = []): void {
     this.errMsgList = errMsgList;
   }
@@ -71,9 +103,11 @@ export class PanelProfileInfoComponent implements OnChanges {
     Object.freeze(this.origUserDto);
 
     this.formGroup.patchValue({
+      avatar: '',
       nickname: userInfo.nickname,
       email: userInfo.email,
       password: userInfo.password,
+      descript: 'Description of a beautiful trip 2024 to greece 6 - E.Allen',
     });
   }
 
