05e6b6e867a6cc6ba83e79457d256255dd938d04
alx.melnichuk <alx.melnichuk@gmail.com>
2024-06-12 20:06:29 +0300

    FE: Added parameter "errMsg".

--- frontend/src/app/components/field-nickname/field-nickname.component.ts
+++ frontend/src/app/components/field-nickname/field-nickname.component.ts
@@ -1,25 +1,10 @@
 import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  OnChanges,
-  SimpleChanges,
-  ViewChild,
-  ViewEncapsulation,
-  forwardRef,
+  ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges, ViewChild, ViewEncapsulation, forwardRef,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
-  AbstractControl,
-  ControlValueAccessor,
-  FormControl,
-  FormGroup,
-  NG_VALIDATORS,
-  NG_VALUE_ACCESSOR,
-  ReactiveFormsModule,
-  ValidationErrors,
-  Validator,
-  ValidatorFn,
+  AbstractControl, ControlValueAccessor, FormControl, FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR, ReactiveFormsModule,
+  ValidationErrors, Validator, ValidatorFn,
 } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput, MatInputModule } from '@angular/material/input';
@@ -30,6 +15,7 @@ export const NICKNAME = 'nickname';
 export const NICKNAME_MIN_LENGTH = 3;
 export const NICKNAME_MAX_LENGTH = 64;
 export const NICKNAME_PATTERN = '^[a-zA-Z]+[\\w]+$';
+export const NICKNAME_CUSTOM_ERROR = 'customError';
 
 @Component({
   selector: 'app-field-nickname',
@@ -65,13 +51,14 @@ export class FieldNicknameComponent implements OnChanges, ControlValueAccessor, 
   public pattern: string = NICKNAME_PATTERN;
   @Input()
   public type: string = "text";
+  @Input()
+  public errMsg: string | null | undefined;
 
   @ViewChild(MatInput, { static: false })
   public matInput: MatInput | null = null;
 
   public formControl: FormControl = new FormControl({ value: null, disabled: false }, []);
   public formGroup: FormGroup = new FormGroup({ nickname: this.formControl });
-  public errMessage: string = '';
 
   constructor() {}
 
@@ -81,6 +68,9 @@ export class FieldNicknameComponent implements OnChanges, ControlValueAccessor, 
     }
     if (!!changes['isDisabled']) {
       this.setDisabledState(this.isDisabled);
+    }
+    if (!!changes['errMsg']) {
+      this.prepareErrMsg(this.errMsg);
     }
   }
 
@@ -139,5 +129,24 @@ export class FieldNicknameComponent implements OnChanges, ControlValueAccessor, 
     const newValidator: ValidatorFn[] = ValidatorUtils.prepare(paramsObj);
     this.formControl.setValidators(newValidator);
     this.formControl.updateValueAndValidity();
+  }
+
+  private prepareErrMsg(errMsg: string | null | undefined): void {
+    let result: ValidationErrors | null = null;
+    const errorsObj = {...this.formControl.errors};
+    if (!!errMsg) {
+      result = {...errorsObj, ...{ NICKNAME_CUSTOM_ERROR: true } };
+    } else {
+      const list = Object.keys(errorsObj);
+      let res: ValidationErrors = {};
+      for (let index = 0; index < list.length; index++) {
+        const key = list[index];
+        if (key !== NICKNAME_CUSTOM_ERROR) {
+          res[key] = errorsObj[key];
+        }
+      }
+      result = (Object.keys(res).length > 0 ? res : null);
+    }
+    this.formControl.setErrors(result, { emitEvent: true });
   }
 }
