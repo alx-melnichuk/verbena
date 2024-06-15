e987931dbdb444c6874eb9f3a4063b29566ead5e
alx.melnichuk <alx.melnichuk@gmail.com>
2024-06-15 18:42:20 +0300

    FE: Modified "PanelProfileInfo".

--- frontend/src/app/lib-profile/panel-profile-info/panel-profile-info.component.ts
+++ frontend/src/app/lib-profile/panel-profile-info/panel-profile-info.component.ts
@@ -1,5 +1,5 @@
 import {
-  ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild, ViewEncapsulation
+  ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild, ViewEncapsulation
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
@@ -13,8 +13,9 @@ import { FieldFileUploadComponent } from 'src/app/components/field-file-upload/f
 import { FieldNicknameComponent } from 'src/app/components/field-nickname/field-nickname.component';
 import { FieldPasswordComponent } from 'src/app/components/field-password/field-password.component';
 import { UniquenessCheckComponent } from 'src/app/components/uniqueness-check/uniqueness-check.component';
-import { UserDto, UserDtoUtil } from 'src/app/lib-user/user-api.interface';
+import { UserDto, UserDtoUtil, UpdateProfileFileDto } from 'src/app/lib-user/user-api.interface';
 import { UserService } from 'src/app/lib-user/user.service';
+import { FieldImageAndUploadComponent } from 'src/app/components/field-image-and-upload/field-image-and-upload.component';
 
 export const PPI_DEBOUNCE_DELAY = 900;
 
@@ -22,7 +23,7 @@ export const PPI_DEBOUNCE_DELAY = 900;
   selector: 'app-panel-profile-info',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, MatButtonModule, TranslateModule, FieldNicknameComponent, FieldEmailComponent,
-    FieldPasswordComponent, FieldDescriptComponent, FieldFileUploadComponent, UniquenessCheckComponent],
+    FieldPasswordComponent, FieldDescriptComponent, FieldFileUploadComponent, FieldImageAndUploadComponent, UniquenessCheckComponent],
   templateUrl: './panel-profile-info.component.html',
   styleUrls: ['./panel-profile-info.component.scss'],
   encapsulation: ViewEncapsulation.None,
@@ -41,23 +42,34 @@ export class PanelProfileInfoComponent implements OnInit, OnChanges {
   @ViewChild(FieldEmailComponent, { static: true })
   public fieldEmailComp!: FieldEmailComponent;
 
+  @Output()
+  readonly updateProfile: EventEmitter<UpdateProfileFileDto> = new EventEmitter();
+  @Output()
+  readonly cancelProfile: EventEmitter<void> = new EventEmitter();
+
   public controls = {
-    avatar: new FormControl(null, []),
     nickname: new FormControl(null, []),
     email: new FormControl(null, []),
     password: new FormControl(null, []),
+    avatar: new FormControl('', []),
     descript: new FormControl(null, []),
   };
   public formGroup: FormGroup = new FormGroup(this.controls);
 
-  public maxFileSize = MAX_FILE_SIZE;
-  public validFileTypes = IMAGE_VALID_FILE_TYPES;
-  public addedLogoView: string = '';
   public debounceDelay: number = PPI_DEBOUNCE_DELAY;
+  // Avatar Image Options
+  public maxSize = MAX_FILE_SIZE;
+  public validTypes = IMAGE_VALID_FILE_TYPES;
+  public avatarFile: File | null | undefined;
+  public initIsAvatar: boolean = false; // original has an avatar.
+  public  avatarView: string = '';
+  public  isAvatarOrig: boolean = false; // original has an avatar.
   
   private origUserDto: UserDto = UserDtoUtil.create();
 
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService
+  ) {
   }
 
   ngOnInit(): void {
@@ -87,40 +99,41 @@ export class PanelProfileInfoComponent implements OnInit, OnChanges {
     return this.userService.uniqueness('', email).then((response) => response == null);
   }
 
-  public addFile(file: File): void {
-    // this.addedLogoFile = file;
-    // this.controls.logo.setValue(file.name);
-    // this.controls.logo.markAsDirty();
+  public addAvatarFile(file: File): void {
+    this.avatarFile = file;
   }
-
-  public readFile(buffFile: string[]): void {
-    if (buffFile.length > 0) {
-      this.addedLogoView = buffFile[1];
-    //   this.changeDetectorRef.markForCheck();
+  public deleteAvatarFile(): void {
+    this.avatarFile = (!!this.initIsAvatar ? null : undefined);
+    if (!!this.initIsAvatar) {
+      this.controls.avatar.markAsDirty();
+    } else {
+      this.controls.avatar.markAsPristine();
     }
-  }
-
-  public deleteFileLogo(): void {
-    // this.addedLogoFile = (!!this.origLogo ? null : undefined);
-    this.addedLogoView = '';
-    // this.controls.logo.setValue(null);
-    // if (!!this.origLogo) {
-    //   this.controls.logo.markAsDirty();
-    // } else {
-    //   this.controls.logo.markAsPristine();
-    // }
   }
 
   public updateErrMsg(errMsgList: string[] = []): void {
     this.errMsgList = errMsgList;
   }
 
-  public saveProfile(): void {
-    console.log(`saveProfile();`);
+  public saveProfileCard(): void {
+    const nickname: string | undefined = this.controls.nickname.value || undefined;
+    const email: string | undefined = this.controls.email.value || undefined;
+    const password: string | undefined = this.controls.password.value || undefined;
+    const descript: string | undefined = this.controls.descript.value || undefined;
+
+    const updateProfileFileDto: UpdateProfileFileDto = {
+      id: this.origUserDto.id,
+      nickname: (this.controls.nickname.dirty ? nickname : undefined),
+      email: (this.controls.email.dirty ? email : undefined),
+      password: (this.controls.password.dirty ? password : undefined),
+      descript: (this.controls.descript.dirty ? descript : undefined),
+      avatarFile: this.avatarFile,
+    };
+    this.updateProfile.emit(updateProfileFileDto);
   }
 
-  public cancelProfile(): void {
-    console.log(`cancelProfile();`);
+  public cancelProfileCard(): void {
+    this.cancelProfile.emit();
   }
 
   // ** Private API **
@@ -133,11 +146,16 @@ export class PanelProfileInfoComponent implements OnInit, OnChanges {
     Object.freeze(this.origUserDto);
 
     this.formGroup.patchValue({
-      avatar: '',
+      avatar: '/logo/10_02280e22j4di504.png',
       nickname: userInfo.nickname,
       email: userInfo.email,
       password: userInfo.password,
       descript: 'Description of a beautiful trip 2024 to greece 6 - E.Allen',
     });
+    this.avatarFile = undefined;
+    this.initIsAvatar = !!this.controls.avatar.value;
+     this.avatarView = ''; // /*streamDto.logo ||*/ '/logo/10_02280e22j4di504.png';
+     this.isAvatarOrig = !!this.avatarView;
+    // this.controls.avatar.disable();
   }  
 }
