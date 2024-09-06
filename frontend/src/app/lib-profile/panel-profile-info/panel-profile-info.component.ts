adfa65c720e2d725b3f35818ffe81c7132d4fa6f
alx.melnichuk <alx.melnichuk@gmail.com>
2024-09-06 18:10:07 +0300

    FE: Fixed the "PanelProfileInfo" component.

--- frontend/src/app/lib-profile/panel-profile-info/panel-profile-info.component.ts
+++ frontend/src/app/lib-profile/panel-profile-info/panel-profile-info.component.ts
@@ -3,7 +3,7 @@ import {
   SimpleChanges, ViewChild, ViewEncapsulation
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
@@ -63,20 +63,20 @@ export class PanelProfileInfoComponent implements OnInit, OnChanges {
     return true;
   }
 
-  public controls1 = {
+  public cntlsProfile = {
     nickname: new FormControl(null, []),
     email: new FormControl(null, []),
     password: new FormControl(null, []),
     avatar: new FormControl('', []),
     descript: new FormControl(null, []),
   };
-  public formGroup1: FormGroup = new FormGroup(this.controls1);
+  public formGroupProfile: FormGroup = new FormGroup(this.cntlsProfile);
 
-  public controls2 = {
+  public cntlsPassword = {
     password: new FormControl(null, []),
     new_password: new FormControl(null, []),
   };
-  public formGroup2: FormGroup = new FormGroup(this.controls2);
+  public formGroupPassword: FormGroup = new FormGroup(this.cntlsPassword);
   public isRequiredPassword: boolean = false;
 
   public debounceDelay: number = PPI_DEBOUNCE_DELAY;
@@ -96,6 +96,7 @@ export class PanelProfileInfoComponent implements OnInit, OnChanges {
     private dialogService: DialogService,
     private profileService: ProfileService,
   ) {
+    this.formGroupPassword.setValidators(this.validatorsForPassword());
   }
 
   ngOnInit(): void {
@@ -106,17 +107,17 @@ export class PanelProfileInfoComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     if (!!changes['profileDto']) {
       this.prepareForm1GroupByUserDto(this.profileDto);
-      this.controls2.password.setValue(null);
-      this.controls2.new_password.setValue(null);
+      this.cntlsPassword.password.setValue(null);
+      this.cntlsPassword.new_password.setValue(null);
       this.isRequiredPassword = false;
     }
     if (!!changes['isDisabledSubmit']) {
-      if (this.isDisabledSubmit != this.formGroup1.disabled) {
-        this.isDisabledSubmit ? this.formGroup1.disable() : this.formGroup1.enable();
+      if (this.isDisabledSubmit != this.formGroupProfile.disabled) {
+        this.isDisabledSubmit ? this.formGroupProfile.disable() : this.formGroupProfile.enable();
         this.changeDetector.markForCheck();
       }
-      if (this.isDisabledSubmit != this.formGroup2.disabled) {
-        this.isDisabledSubmit ? this.formGroup2.disable() : this.formGroup2.enable();
+      if (this.isDisabledSubmit != this.formGroupPassword.disabled) {
+        this.isDisabledSubmit ? this.formGroupPassword.disable() : this.formGroupPassword.enable();
         this.changeDetector.markForCheck();
       }
     }
@@ -124,7 +125,7 @@ export class PanelProfileInfoComponent implements OnInit, OnChanges {
  
   // ** Public API **
   
-  // ** Section "Udate profile" FormGroup1 **
+  // ** Section: Update profile (formGroupProfile) **
 
   public checkUniquenessNickname = (nickname: string | null | undefined): Promise<boolean> => {
     if (!nickname || this.origProfileDto.nickname.toLowerCase() == nickname.toLowerCase()) {
@@ -146,9 +147,9 @@ export class PanelProfileInfoComponent implements OnInit, OnChanges {
   public deleteAvatarFile(): void {
     this.avatarFile = (!!this.initIsAvatar ? null : undefined);
     if (!!this.initIsAvatar) {
-      this.controls1.avatar.markAsDirty();
+      this.cntlsProfile.avatar.markAsDirty();
     } else {
-      this.controls1.avatar.markAsPristine();
+      this.cntlsProfile.avatar.markAsPristine();
     }
   }
 
@@ -156,18 +157,20 @@ export class PanelProfileInfoComponent implements OnInit, OnChanges {
     this.errMsgsProfile = errMsgList;
   }
 
-  public saveProfile(): void {
-    if (this.formGroup1.pristine || this.formGroup1.invalid) {
+  public saveProfile(formGroup: FormGroup): void {
+    const cntlNickname = formGroup.get('nickname');
+    const cntlEmail = formGroup.get('email');
+    const cntlDescript = formGroup.get('descript');
+    if (formGroup.pristine ||  formGroup.invalid || !cntlNickname || !cntlEmail || !cntlDescript) {
       return;
     }
-    const nickname: string = this.controls1.nickname.value || '';
-    const email: string = this.controls1.email.value || '';
-    const descript: string = this.controls1.descript.value || '';
+    const nickname: string = cntlNickname.value || '';
+    const email: string = cntlEmail.value || '';
+    const descript: string = cntlDescript.value || '';
 
     const updateProfileFileDto: UpdateProfileFile = {
       nickname: (this.origProfileDto.nickname != nickname ? nickname : undefined),
       email: (this.origProfileDto.email != email ? email : undefined),
-      // role?: string; // UserRole ["User","Admin"]
       descript: (this.origProfileDto.descript != descript ? descript : undefined),
       // theme?: string | undefined; // Default color theme. ["light","dark"]
       avatarFile: this.avatarFile,
@@ -178,23 +181,55 @@ export class PanelProfileInfoComponent implements OnInit, OnChanges {
     }
   }
 
-  // ** Section "Set new password" FormGroup2 **
+  // ** Section: Set new password (formPassword) **
 
-  public statePasswordField(passwordValue: string | null) {
+  public validatorsForPassword(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const formGroup = control as FormGroup; 
+      const cntlPassword = formGroup.get('password');
+      const cntlNewPassword = formGroup.get('new_password');
+      if (cntlPassword?.pristine) {
+        return { pristine: 'password' };
+      }
+      if (cntlPassword?.invalid) {
+        return { invalid: 'password' };
+      }
+      if (cntlNewPassword?.pristine) {
+        return { pristine: 'new_password' };
+      }
+      if (cntlNewPassword?.invalid) {
+        return { invalid: 'new_password' };
+      }
+      const passwordValue = cntlPassword?.value || '';
+      const newPasswordValue = cntlNewPassword?.value || '';
+      if (!!passwordValue && !!newPasswordValue && passwordValue == newPasswordValue) {
+        return { new_password_equal_to_old_value: true };
+      }
+      return null;
+    };
+  }
+
+  public statePassword(passwordValue: string | null) {
     if (this.isRequiredPassword !== !!passwordValue) {
       this.isRequiredPassword = !!passwordValue;
     }
   }
 
-  public setNewPassword(): void {
-    if (this.controls2.password.pristine || this.controls2.password.invalid
-     || this.controls2.new_password.pristine || this.controls2.new_password.invalid
-     || !this.controls2.password.value || !this.controls2.new_password.value) {
-       return;
+  public checkPassword(formGroup: FormGroup): void {
+    if (formGroup.errors != null && formGroup.errors['new_password_equal_to_old_value']) {
+      this.errMsgsPassword.push('Validation.new_password:equal_to_old_value');
+    }
+  }
+
+  public setNewPassword(formGroup: FormGroup): void {
+    const cntlPassword = formGroup.get('password');
+    const cntlNewPassword = formGroup.get('new_password');
+    if (formGroup.pristine ||  formGroup.invalid || !cntlPassword || !cntlNewPassword) {
+      return;
     }
     const newPasswordProfileDto: NewPasswordProfileDto = {
-        password: this.controls2.password.value, 
-        newPassword: this.controls2.new_password.value
+        password: cntlPassword.value,
+        newPassword: cntlNewPassword.value
     };
     this.updatePassword.emit(newPasswordProfileDto);
   }
@@ -205,7 +240,7 @@ export class PanelProfileInfoComponent implements OnInit, OnChanges {
 
   // ** Section "Delete Account" **
 
-  public performDeleteAccount(): void {
+  public removeAccount(): void {
     const title = this.translate.instant('profile.dialog_title_question_account');
     const nickname = this.profileDto?.nickname || '';
     const appName = this.translate.instant('app.name');
@@ -226,14 +261,14 @@ export class PanelProfileInfoComponent implements OnInit, OnChanges {
     this.origProfileDto = { ...profileDto };
     Object.freeze(this.origProfileDto);
 
-    this.formGroup1.patchValue({
+    this.formGroupProfile.patchValue({
       nickname: profileDto.nickname,
       email: profileDto.email,
       descript: profileDto.descript,
       avatar: profileDto.avatar,
     });
     this.avatarFile = undefined;
-    this.initIsAvatar = !!this.controls1.avatar.value;
+    this.initIsAvatar = !!this.cntlsProfile.avatar.value;
      this.avatarView = profileDto.avatar || '';
      this.isAvatarOrig = !!this.avatarView;
   }
