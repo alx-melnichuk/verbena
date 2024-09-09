404ee9020eb65b5b3691b564e8f44b0a2aad1550
alx.melnichuk <alx.melnichuk@gmail.com>
2024-09-09 18:50:36 +0300

    FE: Added "pgProfileConfigResolver".

---
+++ frontend/src/app/pg-profile/pg-profile-config.resolver.ts
@@ -0,0 +1,13 @@
+
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+
+import { ProfileConfigDto } from '../lib-profile/profile-config.interface';
+import { ProfileConfigService } from '../lib-profile/profile-config.service';
+
+export const pgProfileConfigResolver: ResolveFn<ProfileConfigDto | HttpErrorResponse | undefined> = 
+(_route: ActivatedRouteSnapshot, _state: RouterStateSnapshot) => {
+  const profileConfigService: ProfileConfigService = inject(ProfileConfigService);
+  return profileConfigService.getConfig();
+};
--- frontend/src/app/pg-profile/pg-profile.routes.ts
+++ frontend/src/app/pg-profile/pg-profile.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 
 import { PgProfileComponent } from './pg-profile.component';
 import { pgProfileResolver } from './pg-profile.resolver';
+import { pgProfileConfigResolver } from './pg-profile-config.resolver';
 
 export const PG_PROFILE_ROUTES: Routes = [
   {
     path: '',
     component: PgProfileComponent,
-    resolve: { profileDto: pgProfileResolver },
+    resolve: {
+      profileDto: pgProfileResolver,
+      profileConfigDto: pgProfileConfigResolver,
+    },
   },
 ];
