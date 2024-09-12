80886ab38572541309ed2b23caf27d41ff8b284e
alx.melnichuk <alx.melnichuk@gmail.com>
2024-09-12 21:49:32 +0300

    FE: Modified "ValidFileTypesUtil".

--- frontend/src/app/utils/valid_file_types.util.ts
+++ frontend/src/app/utils/valid_file_types.util.ts
@@ -1,10 +1,50 @@
 export class ValidFileTypesUtil {
-  public static get(validTypes: string | null | undefined): string {
-    const result: Array<string> = [];
-    const validTypeList = (validTypes || '').split(',');
+  public static sorting(validTypes: string | null | undefined): string[] {
+    const map: { [key: string]: string } = {};
+    const validTypeList: string[] = (validTypes || '').split(',');
     for (let idx = 0; idx < validTypeList.length; idx++) {
-        result.push(validTypeList[idx].replace('image/', ''));
+        let value: string = validTypeList[idx].trim();
+        if (value.length == 0) {
+          continue;
+        }
+        if (map[value] == null) {
+          map[value] = '1';
+        }
     }
-    return result.length > 0 ? result.join(', ') : '';
+    return Object.keys(map);
+  }
+  public static text(validTypes: string | null | undefined): string[] {
+    const map: { [key: string]: string } = {};
+    const maket1 = 'image/';
+    const validTypeList = this.sorting(validTypes);
+    for (let idx = 0; idx < validTypeList.length; idx++) {
+        let value = validTypeList[idx];
+        if (value.startsWith(maket1)) {
+          value = value.slice(maket1.length);
+        }
+        if (value.startsWith('.')) {
+          value = value.slice(1);
+        }
+        if (map[value] == null) {
+          map[value] = '1';
+        }
+    }
+    return Object.keys(map);
+  }
+  public static checkFileByAccept(fileName: string, fileType: string, accept: string): boolean {
+    if (!fileName || !fileType) {
+      return false;
+    }
+    let isExistsInAvailable = false;
+    const acceptList: string[] = ValidFileTypesUtil.sorting(accept);
+    for (let idx = 0; idx < acceptList.length && !isExistsInAvailable; idx++) {
+      const value = acceptList[idx];
+      if (value[0] == '.') {
+        isExistsInAvailable = fileName.endsWith(value);
+      } else {
+        isExistsInAvailable = (new RegExp(value)).test(fileType);
+      }
+    }
+    return isExistsInAvailable;
   }
 }  
