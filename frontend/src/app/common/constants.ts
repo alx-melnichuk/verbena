e9ef1c387caeac2e42edb312ddb6bd238b2ea857
alx.melnichuk <alx.melnichuk@gmail.com>
2024-09-18 21:38:28 +0300

    FE: Added parameters for "profile".

--- frontend/src/app/common/constants.ts
+++ frontend/src/app/common/constants.ts
@@ -12,3 +12,10 @@ export const PIPE_DATE_COMPACT = 'MMM dd yyyy';
 export const PIPE_TIME_SHORT = 'HH:mm'; // 'HH:mm aa';
 export const PIPE_DATE = 'dd MMMM yyyy';
 export const PIPE_DATE_TIME = 'dd MMMM yyyy HH:mm'; // 'dd MMMM yyyy HH:mm aa';
+
+export const THEME_LIGHT = 'light';
+export const THEME_DARK = 'dark';
+
+export const LOCALE_EN_US = 'en-US';  // 'English - United States'
+export const LOCALE_DE_DE = 'de-DE'; // 'German - Germany'
+export const LOCALE_UK = 'uk'; // 'Ukrainian'
