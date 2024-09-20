002a67d4a64805d3dfb525cab9d75202f99dda84
alx.melnichuk <alx.melnichuk@gmail.com>
2024-09-20 19:09:02 +0300

    FE: Added initial "locale" setting.

--- frontend/src/app/common/initialization.service.ts
+++ frontend/src/app/common/initialization.service.ts
@@ -3,17 +3,25 @@ import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { DateAdapter } from '@angular/material/core';
 import { first } from 'rxjs/operators';
-import { ROUTE_LOGIN } from './routes';
 
 import { ProfileService } from '../lib-profile/profile.service';
 import { AuthorizationUtil } from '../utils/authorization.util';
 import { HttpErrorUtil } from '../utils/http-error.util';
+
+import { THEME_DARK, THEME_LIGHT, THEME_SUFFIX } from './constants';
+import { ROUTE_LOGIN } from './routes';
+
+export const LOCALE_EN = 'en';
+export const LOCALE_DE = 'de';
+export const LOCALE_UK = 'uk';
+export const LANGUAGES = [LOCALE_EN, LOCALE_DE, LOCALE_UK];
 
 @Injectable({
   providedIn: 'root',
 })
 export class InitializationService {
   private isDarkTheme: boolean | undefined;
+  private currLocale: string | null = null;
 
   constructor(
     private router: Router,
@@ -25,7 +33,15 @@ export class InitializationService {
   }
 
   public initTranslate(): Promise<void | unknown> {
-    const userLanguage = this.getUserLanguage('en');
+    let language = this.currLocale || this.getBrowserLanguage(LOCALE_EN).slice(0,2);
+    language = (LANGUAGES.indexOf(language) > -1 ? language : LOCALE_EN);
+    // Download translations before starting the application.
+    this.translate.addLangs(LANGUAGES);
+    this.translate.setDefaultLang(LOCALE_EN);
+    
+    this.dateAdapter.setLocale(language);
+    /*
+    const userLanguage = this.getBrowserLanguage('en');
     this.dateAdapter.setLocale(userLanguage);
     // Download translations before starting the application.
     this.translate.addLangs(['en', 'de']);
@@ -33,14 +49,13 @@ export class InitializationService {
     const browserLang = this.translate.getBrowserLang() || '';
     const lang: string = browserLang.match(/en|de/) ? browserLang : 'en';
     // return this.translate.use(lang).toPromise();
+    */
     return new Promise<void>((resolve: () => void, reject: (reason: unknown) => void) => {
-      this.translate
-        .use(lang)
-        .pipe(first())
+      this.translate.use(language).pipe(first())
         .subscribe({
           next: () => {
-            HttpErrorUtil.setTranslate(this.translate)
-            resolve()
+            HttpErrorUtil.setTranslate(this.translate);
+            resolve();
           },
           error: (err) => reject(err) 
         });
@@ -56,6 +71,11 @@ export class InitializationService {
     if (isAuthorizationRequired || (isNotAuthorizationDenied && isExistAccessToken)) {
       try {
         await this.profileService.getCurrentProfile();
+        const language = this.profileService.profileDto?.locale;
+        if (!!language && language != this.currLocale) {
+          this.currLocale = language.slice(0,2);
+          this.setLocale(this.currLocale);
+        }
       } catch {
         this.router.navigateByUrl(ROUTE_LOGIN, { replaceUrl: true });
       }
@@ -70,18 +90,34 @@ export class InitializationService {
   public setDarkTheme(value: boolean, renderer: Renderer2): void {
     if (this.isDarkTheme !== value) {
       const oldClassName = this.getThemeName(!!this.isDarkTheme);
-      this.isDarkTheme = value;
+      this.isDarkTheme = !!value;
       const newClassName = this.getThemeName(this.isDarkTheme);
-      const body: HTMLElement = document.body;
 
-      renderer.removeClass(body, oldClassName);
-      renderer.addClass(body, newClassName);
+      renderer.removeClass(document.body, oldClassName);
+      renderer.addClass(document.body, newClassName);
+    }
+  }
+
+  public getLocale(): string | null {
+    return this.currLocale;
+  }
+
+  public setLocale(language: string | null): void {
+    if (!!language && LANGUAGES.indexOf(language) > -1) {
+      this.currLocale = language;
+      this.dateAdapter.setLocale(language);
+      this.translate.use(language).pipe(first())
+      .subscribe({
+        next: () => {
+          HttpErrorUtil.setTranslate(this.translate);
+        },
+      });
     }
   }
 
   // ** Private Api **
 
-  private getUserLanguage(defaultValue: string): string {
+  private getBrowserLanguage(defaultValue: string): string {
     if (typeof window === 'undefined' || typeof window.navigator === 'undefined') {
       return defaultValue;
     }
@@ -92,7 +128,7 @@ export class InitializationService {
   }
 
   private getThemeName(isDarkTheme: boolean): string {
-    return !!isDarkTheme ? 'dark-theme' : 'light-theme';
+    return !!isDarkTheme ? `${THEME_DARK}-${THEME_SUFFIX}` : `${THEME_LIGHT}-${THEME_SUFFIX}`;
   }
 
 }
