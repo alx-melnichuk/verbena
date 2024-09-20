3a94b62fb75da3e370f350f061cda3fc0a12ccdf
alx.melnichuk <alx.melnichuk@gmail.com>
2024-09-20 19:06:40 +0300

    FE: Added "LocalStorageUtil".

---
+++ frontend/src/app/utils/local-storage.util.ts
@@ -0,0 +1,12 @@
+export class LocalStorageUtil {
+  // Add or remove a value from the store.
+  public static update(name: string, value: string | null): void {
+    if (!!name) {
+      if (!!value) {
+        localStorage.setItem(name, value);
+      } else {
+        localStorage.removeItem(name);
+      }
+    }
+  }
+}
