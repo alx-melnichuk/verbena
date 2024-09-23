454601c832093bd644dcf6d18eaaad582166a289
alx.melnichuk <alx.melnichuk@gmail.com>
2024-09-23 22:45:34 +0300

    FE: Added "ConceptListComponent".

---
+++ frontend/src/app/lib-concept/concept-list/concept-list.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-concept-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './concept-list.component.html',
+  styleUrls: ['./concept-list.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ConceptListComponent {
+
+}
