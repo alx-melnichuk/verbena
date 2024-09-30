9621e7b047d781340ae73e8b12dd8b3d4aae98f2
alx.melnichuk <alx.melnichuk@gmail.com>
2024-09-30 21:02:20 +0300

    FE: Modified "PanelAboutComponent"

--- frontend/src/app/lib-about/panel-about/panel-about.component.ts
+++ frontend/src/app/lib-about/panel-about/panel-about.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 declare var APP_ABOUT: any;
 
@@ -14,15 +14,9 @@ declare var APP_ABOUT: any;
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PanelAboutComponent {
-  public title: string = '';
-  public label: string = '';
-  public frontendItems: string[] = [];
   public backendItem01 = this.appAbout['backend01'] || '';
   public backendItem02: string[] = this.appAbout['backend02'] || [];
   public backendItem03: string[] = this.appAbout['backend03'] || [];
-  public linkSwaggerUi = '/swagger-ui/';
-  public linkRapidoc = '/rapidoc';
-  public linkRedoc = '/redoc';
 
   public get appAbout(): any {
     return APP_ABOUT || {};
@@ -30,11 +24,10 @@ export class PanelAboutComponent {
   public set appAbout(value: any) {
   }
 
-  constructor(private translate: TranslateService,) {
-    const appName = this.translate.instant('app.name') || '';
-    this.title = this.translate.instant('panel-about.title', { appName: appName }) || '';
-    this.label = this.translate.instant('panel-about.label', { appName: appName }) || '';
-    this.frontendItems = this.translate.instant('panel-about.frontend_items') || [];
+  @HostBinding('class.global-scroll')
+  public get isGlobalScroll(): boolean { return true; }
+
+  constructor() {
   }
   
   // ** Public API **
