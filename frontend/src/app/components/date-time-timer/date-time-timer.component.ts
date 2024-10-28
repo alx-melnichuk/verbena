caed8bbea9562c60bc06d5ea2027fec8ed07f9c0
alx.melnichuk <alx.melnichuk@gmail.com>
2024-10-28 21:57:28 +0200

    FE: Modification of the "date-time-timer": added countdown.

--- frontend/src/app/components/date-time-timer/date-time-timer.component.ts
+++ frontend/src/app/components/date-time-timer/date-time-timer.component.ts
@@ -1,15 +1,16 @@
 import {
-  ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Input, OnChanges, OnInit, Renderer2, SimpleChanges, 
+  ChangeDetectionStrategy, Component, ElementRef, HostListener, Input, OnChanges, OnInit, Renderer2, SimpleChanges, 
   ViewEncapsulation
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HtmlElemUtil } from 'src/app/utils/html-elem.util';
 
-const DEF_LEADING_ZEROS = 2;
 const ATTR_IS_ACTIVE = 'is-active';
 const CSS_BOX_JUSTIFY_CONTENT = '--dtt-box-justify-content';
 const CSS_LEADING_ZERO = '--dtt-leading-zero';
 const CSS_LETTER_SPACING = '--dtt-letter-spacing';
+const CSS_DIRECTION = '--dtt-direction';
+const CSS_DAYS = '--dtt-days';
 const CSS_HOURS = '--dtt-hours';
 const CSS_MINUTES = '--dtt-minutes';
 const CSS_SECONDS = '--dtt-seconds';
@@ -25,62 +26,59 @@ const CSS_SECONDS = '--dtt-seconds';
 })
 export class DateTimeTimerComponent implements OnChanges, OnInit {
   @Input()
+  public futureDate: Date | null | undefined;
+  @Input()
   public isActive: boolean | null | undefined;
   @Input()
   public isAlignCenter: boolean | null | undefined; // together with "isHideLeadingZero"
-  @Input()
-  public isCountdown: boolean | null | undefined;
   @Input()
   public isHideLeadingZero: boolean | null | undefined;  // together with "isAlignCenter"
   @Input()
   public letterSpacing: number | null | undefined = 0; // in 'px'
 
-  public currValue: Date | null = null;
-  public hours: number = 0;
-  public minutes: number = 0;
-  public seconds: number = 0;
+  private currValue: Date | null = null;
+  private days: number = 0;
+  private hours: number = 0;
+  private minutes: number = 0;
+  private seconds: number = 0;
   
-  public isEvenIteration: boolean | null = null;
-  public settimeoutId: number | null = null;  
-
-
-  private count = 0; // tmp
-
+  private isEvenIteration: boolean = false;
+  private settimeoutId: number | null = null;
+  private isDocumentVisible: boolean = true;
+  private count = 0;
   constructor(
     private renderer: Renderer2,
     public hostRef: ElementRef<HTMLElement>,
-    private changeDetectorRef: ChangeDetectorRef,
   ) {
   }
 
+  @HostListener('document:visibilitychange', ['$event'])
+  listenerVisibilityChange() {
+    this.isDocumentVisible = !document.hidden;
+    //console.log(`listenerVisibilityChange() isDocumentVisible: ${this.isDocumentVisible}  this.modifyCurrValueAndSetTimeout();`); // #
+    this.modifyCurrValueAndSetTimeout();
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
+    if (!!changes['futureDate']) {
+      HtmlElemUtil.setProperty(this.hostRef, CSS_DIRECTION, this.futureDate != null ? '-1' : null);
+    }
     if (!!changes['isActive']) {
-      console.log(`isActive: ${this.isActive}`); // #
       if (this.isActive) {
-        //this.updateCurrValue(new Date(Date.now())); // #
-        //HtmlElemUtil.setProperty(this.hostRef, CSS_HOURS, this.hours.toString()); // #
-        //HtmlElemUtil.setProperty(this.hostRef, CSS_MINUTES, this.minutes.toString()); // #
-        //this.seconds = 40;
-        //HtmlElemUtil.setProperty(this.hostRef, CSS_SECONDS, this.seconds.toString()); // #
-  
         this.isEvenIteration = false;
         this.modifyCurrValueAndSetTimeout();
       } else {
-        this.isEvenIteration = null;
+        this.clearTimeout();
         this.clearCurrValue();
       }
-        
       HtmlElemUtil.setAttr(this.renderer, this.hostRef, ATTR_IS_ACTIVE, !!this.isActive ? '' : null);
     }
     if (!!changes['isAlignCenter']) {
       HtmlElemUtil.setProperty(this.hostRef, CSS_BOX_JUSTIFY_CONTENT, !!this.isAlignCenter ? 'center' : null);
-      // justify-content: center
     }
     if (!!changes['isHideLeadingZero']) {
-        console.log(`leadingZero: ${!!this.isHideLeadingZero ? JSON.stringify('') : 'null'}`); // #
         HtmlElemUtil.setProperty(this.hostRef, CSS_LEADING_ZERO, !!this.isHideLeadingZero ? '""' : null);
     }
-
     if (!!changes['letterSpacing']) {
       const letterSpacing = (this.letterSpacing || -1) > 0 ? this.letterSpacing?.toString() + 'px' : null;
       HtmlElemUtil.setProperty(this.hostRef, CSS_LETTER_SPACING, letterSpacing);
@@ -91,55 +89,65 @@ export class DateTimeTimerComponent implements OnChanges, OnInit {
   }
   // ** Public API **
 
-  public log(text: string): void {
-    console.log(`log() ${text}`); // #
-  }
   // ** Public API **
 
-  private showValue(value: number, isLeadingZero?: boolean | null): string {
-    const valueStr = value.toString();
-    const len = (!!isLeadingZero ? DEF_LEADING_ZEROS : 0) - valueStr.length;
-    return (len > 0 ? Array(len).fill('0').join('') : '') + valueStr;
-  }
   private clearCurrValue(): void {
+    this.days = 0;
     this.hours = 0;
     this.minutes = 0;
     this.seconds = 0;
     this.currValue = null;
 
   }
-  private updateCurrValue(currValue: Date): number {
-    this.hours = currValue.getHours();
-    this.minutes = currValue.getMinutes();
-    this.seconds = currValue.getSeconds();
-    this.currValue = currValue;
+  private updateValueInForwardDir(currentDate: Date): number {
+    this.days = 0;
+    this.hours = currentDate.getHours();
+    this.minutes = currentDate.getMinutes();
+    this.seconds = currentDate.getSeconds();
+    this.currValue = currentDate;
     return this.seconds;
   }
-  
-  private modifyCurrValueAndSetTimeout = () => {
-    if (this.isActive && this.count < 2) {
-      this.count++
-      
-      let seconds = this.updateCurrValue(new Date(Date.now()));
-      HtmlElemUtil.setProperty(this.hostRef, CSS_HOURS, this.hours.toString());
-      HtmlElemUtil.setProperty(this.hostRef, CSS_MINUTES, this.minutes.toString());
-      if (this.count == 1) {
-        seconds = this.seconds = 0;
-      }
-      HtmlElemUtil.setProperty(this.hostRef, CSS_SECONDS, (0 != seconds ? this.seconds.toString() : null));
-
-      const duration = (!this.isCountdown ? 60 - seconds : seconds);
-
-      this.isEvenIteration = !this.isEvenIteration;
-      
-      console.log(`count: ${this.count}, min: ${this.minutes}, sec: ${this.seconds}, duration: ${duration}`
-        + `, isEven: ${this.isEvenIteration}`); // #
-
-      this.settimeoutId = window.setTimeout(() => { this.modifyCurrValueAndSetTimeout(); }, duration * 1000); 
-    } else if (!this.settimeoutId) {
+  private updateValueInBackwardDir(currentDate: Date, futureDate: Date): number {
+    const currentDateNum = currentDate.getTime();
+    const futureDateNum = futureDate.getTime();
+    // Find the distance between now and the countdown date.
+    const distance = futureDateNum - currentDateNum;
+    // Time calculations for days, hours, minutes and seconds.
+    this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    this.currValue = currentDate;
+    //console.log(`days: ${this.days}, hours: ${this.hours}, minutes: ${this.minutes}, seconds: ${this.seconds}`); // #
+    return this.seconds;
+  }
+  private clearTimeout(): void {
+    if (!this.settimeoutId) {
       window.clearTimeout(this.settimeoutId as number);
       this.settimeoutId = null;
     }
-    this.changeDetectorRef.markForCheck();
+  }
+  private modifyCurrValueAndSetTimeout = () => {
+    HtmlElemUtil.setAttr(this.renderer, this.hostRef, this.isEvenIteration == true ? 'animat2' : 'animat1', null);
+    if (this.isActive && this.isDocumentVisible && this.count < 2) {
+      this.count++;
+      const now = new Date(Date.now());
+      const seconds = this.futureDate != null ?
+        this.updateValueInBackwardDir(now, this.futureDate) : this.updateValueInForwardDir(now);
+
+      HtmlElemUtil.setProperty(this.hostRef, CSS_DAYS, this.days.toString());
+      HtmlElemUtil.setProperty(this.hostRef, CSS_HOURS, this.hours.toString());
+      HtmlElemUtil.setProperty(this.hostRef, CSS_MINUTES, this.minutes.toString());
+      HtmlElemUtil.setProperty(this.hostRef, CSS_SECONDS, this.seconds.toString());
+      const duration = this.futureDate == null ? 60 - seconds : seconds + 1;
+      // const duration = (!this.isCountdown ? 60 - seconds : seconds);
+      this.isEvenIteration = !this.isEvenIteration;
+      HtmlElemUtil.setAttr(this.renderer, this.hostRef, this.isEvenIteration == true ? 'animat2' : 'animat1', '');
+      //console.log(`hours: ${this.hours}, min: ${this.minutes}, sec: ${this.seconds}, duration: ${duration}`
+      //  + `, isEven: ${this.isEvenIteration}`); // #
+      this.settimeoutId = window.setTimeout(() => { this.modifyCurrValueAndSetTimeout(); }, duration * 1000); 
+    } else {
+      this.clearTimeout();
+    }
   }
 }
