7b2694ca2180f60414f91d62c2eb3548ff8be699
alx.melnichuk <alx.melnichuk@gmail.com>
2024-10-30 15:45:56 +0200

    FE: Added "AppErrorHandler".

---
+++ frontend/src/app/app-error-handler.ts
@@ -0,0 +1,14 @@
+import { ErrorHandler, Injectable } from '@angular/core';
+
+@Injectable()
+export class AppErrorHandler implements ErrorHandler {
+  // Handling the error "Loading chunk [\d]+ failed"
+  public handleError(error: any): void {
+    const chunkFailedMessage = /Loading chunk [\d]+ failed/;
+    if (chunkFailedMessage.test(error.message)) {
+      if (confirm('New version available. Load New Version?')) {
+        window.location.reload();
+      }
+    }
+  }
+}
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { HTTP_INTERCEPTORS, HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, ErrorHandler, importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -12,6 +12,7 @@ import { APP_DATE_FORMATS, AppDateAdapter } from './app-date-adapter';
 import { APP_ROUTES } from './app.routes';
 import { AuthorizationInterceptor } from './common/authorization.interceptor';
 import { InitializationService } from './common/initialization.service';
+import { AppErrorHandler } from './app-error-handler';
 
 // AoT requires an exported function for factories
 export const HTTP_LOADER_FACTORY = (httpClient: HttpClient): TranslateHttpLoader => {
@@ -68,6 +69,10 @@ export const appConfig: ApplicationConfig = {
       deps: [InitializationService],
       useFactory: INITIALIZE_TRANSLATE_FACTORY,
       multi: true,
+    },
+    { // Handling the error "Loading chunk [\d]+ failed"
+      provide: ErrorHandler,
+      useClass: AppErrorHandler
     },
   ],
 };
