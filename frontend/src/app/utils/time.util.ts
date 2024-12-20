a5d477eea787c63f7904410f66e05dbbf264f06a
alx.melnichuk <alx.melnichuk@gmail.com>
2024-12-20 19:29:38 +0200

    FE: Added "addTime" method to TimeUtil to add time.

--- frontend/src/app/utils/time.util.ts
+++ frontend/src/app/utils/time.util.ts
@@ -6,10 +6,10 @@ export class TimeUtil {
     let seconds: number = 0;
     if (!!value && value.length > 4 && value.slice(2, 3) == ':') {
       hours = parseInt(value.slice(0, 2), 10);
-      minutes = parseInt(value.slice(3,5), 10);
+      minutes = parseInt(value.slice(3, 5), 10);
     }
     if (!!value && value.length > 7 && value.slice(5, 6) == ':') {
-      seconds = parseInt(value.slice(6,8), 10);
+      seconds = parseInt(value.slice(6, 8), 10);
     }
     return !isNaN(hours) && !isNaN(minutes) && !isNaN(seconds)
       && hours > -1 && minutes > -1 && seconds > -1 ? { hours, minutes, seconds } : null;
@@ -20,9 +20,27 @@ export class TimeUtil {
     if (!!value && value.length > 4 && value.slice(2, 3) == ':') {
         const hoursStr = value.slice(0, 2);
         hours = parseInt(hoursStr, 10);
-        const minutesStr = value.slice(3,6);
+        const minutesStr = value.slice(3, 6);
         minutes = parseInt(minutesStr, 10);
     }
     return { hours, minutes };
+  }
+  // Time addition method.
+  //   value: 'hh:mm' or 'hh:mm:ss'
+  public static addTime(
+    value: string | null | undefined, addHours: number, addMinutes: number, addSeconds: number
+  ): { hours: number, minutes: number, seconds: number } | null {
+    let hours: number = -1;
+    let minutes: number = -1;
+    let seconds: number = -1;
+    const dateValue = TimeUtil.parseTime(value); // { hours: number, minutes: number, seconds: number } | null
+    if (!!dateValue) {
+      const date1 = new Date(Date.now());
+      date1.setHours(dateValue.hours + addHours, dateValue.minutes + addMinutes, dateValue.seconds + addSeconds, 0);
+      hours = date1.getHours();
+      minutes = date1.getMinutes();
+      seconds = date1.getSeconds();
+    }
+    return (!!dateValue ? { hours, minutes, seconds } : null);
   }
 }
