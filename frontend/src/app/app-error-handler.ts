f62bc5b25d7807a0abb622a2f5474245e75db9d6
alx.melnichuk <alx.melnichuk@gmail.com>
2025-01-07 16:15:03 +0200

    FE: minor changes.

--- frontend/src/app/app-error-handler.ts
+++ frontend/src/app/app-error-handler.ts
@@ -5,7 +5,7 @@ export class AppErrorHandler implements ErrorHandler {
   // Handling the error "Loading chunk [\d]+ failed"
   public handleError(error: any): void {
     const chunkFailedMessage = /Loading chunk [\d]+ failed/;
-    if (chunkFailedMessage.test(error.message)) {
+    if (chunkFailedMessage.test(error?.message)) {
       window.location.reload();
     }
   }
