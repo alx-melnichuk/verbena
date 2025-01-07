62ee38f8454d420b565c9893611c8ad27a819c0a
alx.melnichuk <alx.melnichuk@gmail.com>
2025-01-07 19:44:29 +0200

    FE: Removed elements that were not used.

--- frontend/src/app/common/constants.ts
+++ frontend/src/app/common/constants.ts
@@ -7,11 +7,6 @@ export const IMAGE_VALID_FILE_TYPES = 'png,jpg,jpeg,gif';
 // Format date for moment
 export const MOMENT_ISO8601 = 'YYYY-MM-DDTHH:mm:ssZ';
 export const MOMENT_ISO8601_DATE = 'YYYY-MM-DD';
-// Format date for standard pipe
-export const PIPE_DATE_COMPACT = 'MMM dd yyyy';
-export const PIPE_TIME_SHORT = 'HH:mm'; // 'HH:mm aa';
-export const PIPE_DATE = 'dd MMMM yyyy';
-export const PIPE_DATE_TIME = 'dd MMMM yyyy HH:mm'; // 'dd MMMM yyyy HH:mm aa';
 
 // Color scheme
 export const SCHEME_LIGHT = 'light';
