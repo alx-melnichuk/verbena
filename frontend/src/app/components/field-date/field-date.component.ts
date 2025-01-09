77cd23083c46d8cbf567e4d9bbb358e8afef5580
alx.melnichuk <alx.melnichuk@gmail.com>
2025-01-09 18:30:44 +0200

    FE: Fixed a bug: the date format in the error did not change when changing the locale.

--- frontend/src/app/components/field-date/field-date.component.ts
+++ frontend/src/app/components/field-date/field-date.component.ts
@@ -11,7 +11,7 @@ import { DateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput, MatInputModule } from '@angular/material/input';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-field-date',
@@ -61,7 +61,7 @@ export class FieldDateComponent implements OnChanges, ControlValueAccessor, Vali
   private readonly dateAdapter: DateAdapter<Date> = inject(DateAdapter);
   private localeError: string | null = null;
 
-  constructor(private translate: TranslateService) {}
+  constructor() {}
 
   ngOnChanges(changes: SimpleChanges): void {
     if (!!changes['isRequired']) {
@@ -71,6 +71,9 @@ export class FieldDateComponent implements OnChanges, ControlValueAccessor, Vali
       this.setDisabledState(this.isDisabled);
     }
     if (!!changes['locale'] && !!this.formControl.errors) {
+      // In the case where an error is already displayed and the locale changes, it is necessary 
+      // to update the value of the parameter of the "translate" directive (this.formControl.errors).
+      // This needs to be done to display the error in the date format for the new locale.
       this.validate(this.formControl);
     }
   }
@@ -101,23 +104,26 @@ export class FieldDateComponent implements OnChanges, ControlValueAccessor, Vali
   // ** Validator - start ** createFieldTimeMinValidator()
 
   public validate(control: AbstractControl): ValidationErrors | null {
-    const isLocaleShift = this.localeError != this.locale;
-    const errors = this.formControl.errors;
-    if (errors != null) {
-      // Add extended tags. (min_s, max_s, actual_s)
-      const minDate = errors['matDatepickerMin'];
-      this.checkDateAndMapStr(minDate, 'min', isLocaleShift);
-      this.checkDateAndMapStr(minDate, 'actual', isLocaleShift);
-  
-      const maxDate = errors['matDatepickerMax'];
-      this.checkDateAndMapStr(maxDate, 'max', isLocaleShift);
-      this.checkDateAndMapStr(maxDate, 'actual', isLocaleShift);
+    if (this.formControl.errors != null) {
+      const oldErrors = this.formControl.errors;
+      // Add extended tags with date format for the current locale. (min_s, max_s, actual_s)
+      const errors = this.addExtendTags({...this.formControl.errors}, this.locale);
 
-      if (isLocaleShift) {
-        this.localeError = this.locale;
-      }
+      // When called, the "translate" directive compares its parameter with the previous value.
+      // And if the new value of the parameter does not differ from the old one, then the directive is not rendered.
+      // In this case, creating a new object for the new value of the parameter is not enough.
+      // Since the parameter value object is compared with the old value for each property of the object.
+      // For this reason, an additional property is added to the parameter to distinguish the old value.
+      // This is very important in the case where an error is already displayed and the locale changes. 
+      // In this case, it is necessary to update the value of the parameter of the "translate" directive
+      // (this.formControl.errors).
+
+      // Create a new error object.
+      this.formControl.setErrors(errors);
+      // Additional fields to differentiate from the old version.
+      oldErrors[`difference~$#`] = 1;
     }
-    return errors;
+    return this.formControl.errors;
   }
 
   // ** Validator - finish **
@@ -146,7 +152,7 @@ export class FieldDateComponent implements OnChanges, ControlValueAccessor, Vali
   }
 
   public doDateInput(e: any): void {
-    // Add extended tags. (min_s, max_s, actual_s)
+    // Add extended tags with date format for the current locale. (min_s, max_s, actual_s)
     this.validate(this.formControl);
     this.dateInput.emit(this.formControl.value);
   }
@@ -154,10 +160,6 @@ export class FieldDateComponent implements OnChanges, ControlValueAccessor, Vali
   public doDateChange(e: any): void {
     this.dateChange.emit(this.formControl.value);
     this.onChange(this.formControl.value);
-  }
-
-  public doTranslate(value: string, errors: ValidationErrors | null): string {
-    return !!value ? this.translate.instant(value, (errors as Object)) : '';
   }
 
   // ** Private API **
@@ -176,5 +178,24 @@ export class FieldDateComponent implements OnChanges, ControlValueAccessor, Vali
       error_item[key2] = this.dateAdapter.format(error_item[key], null);
     }
     return error_item;
+  }
+  /** Add extended tags with date format for the current locale. (min_s, max_s, actual_s) */
+  private addExtendTags(errors: ValidationErrors | null, locale: string | null): ValidationErrors | null {
+    if (errors != null) {
+      const isLocaleShift = this.localeError != locale;
+
+      const minDate = errors['matDatepickerMin'];
+      this.checkDateAndMapStr(minDate, 'min', isLocaleShift);
+      this.checkDateAndMapStr(minDate, 'actual', isLocaleShift);
+  
+      const maxDate = errors['matDatepickerMax'];
+      this.checkDateAndMapStr(maxDate, 'max', isLocaleShift);
+      this.checkDateAndMapStr(maxDate, 'actual', isLocaleShift);
+
+      if (isLocaleShift) {
+        this.localeError = locale;
+      }
+    }
+    return errors;
   }
 }
