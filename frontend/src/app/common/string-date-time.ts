36a96b76dd06b3e25c9085cf67590e6b684db6ab
alx.melnichuk <alx.melnichuk@gmail.com>
2025-02-08 18:51:29 +0200

    FE: Added comments.

--- frontend/src/app/common/string-date-time.ts
+++ frontend/src/app/common/string-date-time.ts
@@ -1,2 +1,9 @@
-export type StringDate = string;       // ISO 8601 format 'yyyy-MM-dd'
-export type StringDateTime = string;   // ISO 8601 format 'yyyy-MM-ddThh:mm:ss.000Z', 'yyyy-MM-ddThh:mm:ssZ'
+/** Date value in ISO 8601 format 'yyyy-MM-dd' */
+export type StringDate = string;
+
+/** Date and time value in ISO 8601 format
+ * 
+ * 'yyyy-MM-ddThh:mm:ss.000Z', 'yyyy-MM-ddThh:mm:ssZ' */
+export type StringDateTime = string;
+// (new Date()).toISOString() '2020-02-08T10:20:30.000Z'
+// (new Date()).toJSON()      '2020-02-08T10:20:30.000Z'
