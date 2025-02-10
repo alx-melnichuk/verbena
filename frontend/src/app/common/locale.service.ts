d0fcbf46dde56cf4a72b7c0739cc4b74142d8846
alx.melnichuk <alx.melnichuk@gmail.com>
2025-02-10 12:42:22 +0200

    FE: minor changes

--- frontend/src/app/common/locale.service.ts
+++ frontend/src/app/common/locale.service.ts
@@ -8,44 +8,44 @@ import { HttpErrorUtil } from "../utils/http-error.util";
 import { ENV_IS_PROD, LOCALE_EN, LOCALE_LIST } from './constants';
 
 @Injectable({
-  providedIn: "root"
+    providedIn: "root"
 })
 export class LocaleService {
-  private currLocale: string | null = null;
+    private currLocale: string | null = null;
 
-  constructor(
-    private dateAdapter: DateAdapter<Date>,
-    private translate: TranslateService,
-  ) {
-    if (!ENV_IS_PROD) { console.log(`#1-LocaleService();`); }
-  }
+    constructor(
+        private dateAdapter: DateAdapter<Date>,
+        private translate: TranslateService,
+    ) {
+        if (!ENV_IS_PROD) { console.log(`#1-LocaleService();`); }
+    }
 
-  // ** Locale ** 
-  public getLocale(): string | null {
-    return this.currLocale;
-  }
-  
-  public setLocale(value: string | null): Promise<void> {
-    const locale = !!value ? LOCALE_LIST.find((item: string) => item.toLowerCase() == value.toLowerCase()) : LOCALE_EN;
-    if (!locale || LOCALE_LIST.indexOf(locale) == -1) {
-      console.error(`Invalid locale value "${locale}" (available: "${LOCALE_LIST.join('","')}").`);
-      return Promise.reject();
+    // ** Locale **
+    public getLocale(): string | null {
+        return this.currLocale;
     }
-    if (this.currLocale == locale) {
-      Promise.resolve();
-    }
-    return new Promise<void>((resolve: () => void, reject: (reason: unknown) => void) => {
-      this.translate.use(locale).pipe(first())
-        .subscribe({
-          next: () => {
-            this.currLocale = locale;
-            this.dateAdapter.setLocale(locale);
-            HttpErrorUtil.setTranslate(this.translate);
-            resolve();
-          },
-          error: (err) => reject(err) 
+
+    public setLocale(value: string | null): Promise<void> {
+        const locale = !!value ? LOCALE_LIST.find((item: string) => item.toLowerCase() == value.toLowerCase()) : LOCALE_EN;
+        if (!locale || LOCALE_LIST.indexOf(locale) == -1) {
+            console.error(`Invalid locale value "${locale}" (available: "${LOCALE_LIST.join('","')}").`);
+            return Promise.reject();
+        }
+        if (this.currLocale == locale) {
+            Promise.resolve();
+        }
+        return new Promise<void>((resolve: () => void, reject: (reason: unknown) => void) => {
+            this.translate.use(locale).pipe(first())
+                .subscribe({
+                    next: () => {
+                        this.currLocale = locale;
+                        this.dateAdapter.setLocale(locale);
+                        HttpErrorUtil.setTranslate(this.translate);
+                        resolve();
+                    },
+                    error: (err) => reject(err)
+                });
         });
-      });
-  }
+    }
 
 }
