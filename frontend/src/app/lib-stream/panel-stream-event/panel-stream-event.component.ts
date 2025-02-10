cbbc1e00937d6555ec64f4289e79c8eee8c57ce7
alx.melnichuk <alx.melnichuk@gmail.com>
2025-02-10 15:49:36 +0200

    FE: Added parametr "isRefresh".

--- frontend/src/app/lib-stream/panel-stream-event/panel-stream-event.component.ts
+++ frontend/src/app/lib-stream/panel-stream-event/panel-stream-event.component.ts
@@ -1,73 +1,75 @@
 import {
-  ChangeDetectionStrategy, Component, EventEmitter, HostBinding, HostListener, Input, Output, ViewEncapsulation
+    ChangeDetectionStrategy, Component, EventEmitter, HostBinding, HostListener, Input, Output, ViewEncapsulation
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslatePipe } from '@ngx-translate/core';
 
 import { DateTimeFormatPipe } from 'src/app/common/date-time-format.pipe';
-import { LogotypeComponent  } from 'src/app/components/logotype/logotype.component';
-import { ScrollHasMaxUtil   } from 'src/app/utils/scroll-has-max.util';
+import { LogotypeComponent } from 'src/app/components/logotype/logotype.component';
+import { ScrollHasMaxUtil } from 'src/app/utils/scroll-has-max.util';
 
 import { StreamDtoUtil, StreamEventDto } from '../stream-api.interface';
 
 @Component({
-  selector: 'app-panel-stream-event',
-  standalone: true,
-  imports: [CommonModule, TranslatePipe, LogotypeComponent, DateTimeFormatPipe],
-  templateUrl: './panel-stream-event.component.html',
-  styleUrl: './panel-stream-event.component.scss',
-  encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-panel-stream-event',
+    standalone: true,
+    imports: [CommonModule, TranslatePipe, LogotypeComponent, DateTimeFormatPipe],
+    templateUrl: './panel-stream-event.component.html',
+    styleUrl: './panel-stream-event.component.scss',
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PanelStreamEventComponent {
-  @Input()
-  public canEdit = false;
-  @Input()
-  public locale: string | null = null;
-  @Input()
-  public streamShortDtoList: StreamEventDto[] = [];
+    @Input()
+    public canEdit = false;
+    @Input() // Used to refresh the view.
+    public isRefresh: boolean | null | undefined;
+    @Input()
+    public locale: string | null = null;
+    @Input()
+    public streamShortDtoList: StreamEventDto[] = [];
 
-  @Output()
-  readonly requestNextPage: EventEmitter<void> = new EventEmitter();
-  @Output()
-  readonly viewStream: EventEmitter<number> = new EventEmitter();
-  @Output()
-  readonly editStream: EventEmitter<number> = new EventEmitter();
+    @Output()
+    readonly requestNextPage: EventEmitter<void> = new EventEmitter();
+    @Output()
+    readonly viewStream: EventEmitter<number> = new EventEmitter();
+    @Output()
+    readonly editStream: EventEmitter<number> = new EventEmitter();
 
-  readonly formatDate: Intl.DateTimeFormatOptions = { dateStyle: 'short' };
-  readonly formatDateTime: Intl.DateTimeFormatOptions = { dateStyle: 'short', timeStyle: 'short' };
+    readonly formatDate: Intl.DateTimeFormatOptions = { dateStyle: 'short' };
+    readonly formatDateTime: Intl.DateTimeFormatOptions = { dateStyle: 'short', timeStyle: 'short' };
 
-  @HostBinding('class.global-scroll')
-  public get isGlobalScroll(): boolean { return true; }
-  
-  @HostListener('scrollend', ['$event'])
-  public doScrollPanel(event:Event):void {
-    event.preventDefault();
-    event.stopPropagation();
-    const elem: Element | null = event.target as Element;
-    if (ScrollHasMaxUtil.check(elem?.scrollTop, elem?.clientHeight, elem?.scrollHeight)) {
-      this.requestNextPage.emit();  
+    @HostBinding('class.global-scroll')
+    public get isGlobalScroll(): boolean { return true; }
+
+    @HostListener('scrollend', ['$event'])
+    public doScrollPanel(event: Event): void {
+        event.preventDefault();
+        event.stopPropagation();
+        const elem: Element | null = event.target as Element;
+        if (ScrollHasMaxUtil.check(elem?.scrollTop, elem?.clientHeight, elem?.scrollHeight)) {
+            this.requestNextPage.emit();
+        }
     }
-  }
 
-  constructor() {
-  }
-
-  // ** Public API **
-
-  public isFuture(startTime: string | null): boolean | null {
-    return StreamDtoUtil.isFuture(startTime);
-  }
-
-  public doEditStream(streamId: number): void {
-    if (this.canEdit && !!streamId) {
-      this.editStream.emit(streamId);
+    constructor() {
     }
-  }
 
-  public doViewStream(streamId: number): void {
-    if (!!streamId) {
-      this.viewStream.emit(streamId);
+    // ** Public API **
+
+    public isFuture(startTime: string | null): boolean | null {
+        return StreamDtoUtil.isFuture(startTime);
     }
-  }
+
+    public doEditStream(streamId: number): void {
+        if (this.canEdit && !!streamId) {
+            this.editStream.emit(streamId);
+        }
+    }
+
+    public doViewStream(streamId: number): void {
+        if (!!streamId) {
+            this.viewStream.emit(streamId);
+        }
+    }
 }
