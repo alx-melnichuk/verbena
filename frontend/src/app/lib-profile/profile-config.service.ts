01e2bf2ebab475bcbf1f7bac2bd2c5ab56e754d8
alx.melnichuk <alx.melnichuk@gmail.com>
2025-02-13 16:04:03 +0200

    FE: Replacing "HttpObservableUtil.toPromise()" with "lastValueFrom()".

--- frontend/src/app/lib-profile/profile-config.service.ts
+++ frontend/src/app/lib-profile/profile-config.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 import { Uri } from 'src/app/common/uri';
 
-import { HttpObservableUtil } from '../utils/http-observable.util';
 import { ProfileConfigDto } from './profile-config.interface';
 
 @Injectable({
@@ -19,7 +19,7 @@ export class ProfileConfigService {
             return Promise.resolve({ ...this.profileConfigDto });
         }
         const url = Uri.appUri('appApi://profiles_config');
-        return HttpObservableUtil.toPromise<ProfileConfigDto>(this.http.get<ProfileConfigDto | HttpErrorResponse>(url))
+        return lastValueFrom(this.http.get<ProfileConfigDto | HttpErrorResponse>(url))
             .then((response: ProfileConfigDto | HttpErrorResponse | undefined) => {
                 this.profileConfigDto = response as ProfileConfigDto;
                 return { ...this.profileConfigDto };
