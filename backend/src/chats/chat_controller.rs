7b87c12a69b4135de56394707522d993b4eb04bb
alx.melnichuk <alx.melnichuk@gmail.com>
2025-03-18 21:31:04 +0200

    BE: Added "chat_controller".

---
+++ backend/src/chats/chat_controller.rs
@@ -0,0 +1,28 @@
+use actix_files::NamedFile;
+use actix_web::{get, web, HttpResponse, Responder};
+use actix_web_actors::ws;
+
+use crate::chats::session::WsChatSession;
+
+pub fn configure() -> impl FnOnce(&mut web::ServiceConfig) {
+    |config: &mut web::ServiceConfig| {
+        config
+            // GET /ws
+            .service(get_ws_chat)
+            // GET /chat
+            .service(chat);
+    }
+}
+
+#[get("/ws")]
+pub async fn get_ws_chat(
+    request: actix_web::HttpRequest,
+    stream: web::Payload,
+) -> actix_web::Result<HttpResponse<actix_web::body::BoxBody>, actix_web::Error> {
+    ws::start(WsChatSession::default(), &request, stream)
+}
+
+#[get("/chat")]
+async fn chat() -> impl Responder {
+    NamedFile::open_async("./static/chat_index.html").await.unwrap()
+}
