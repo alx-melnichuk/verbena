e237e4db312b25f036eeaba8deee55178cde6fb1
alx.melnichuk <alx.melnichuk@gmail.com>
2025-03-20 21:25:25 +0200

    BE: Implemented processing: Echo, Err, Join, Leave, Name.

--- backend/src/chats/chat_models.rs
+++ backend/src/chats/chat_models.rs
@@ -4,42 +4,37 @@ use std::{collections::HashMap, fmt};
 use serde::{Deserialize, Serialize};
 use serde_json;
 
+// ** Types of events that are transmitted over a websocket. **
+
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "camelCase")]
-pub enum WSEventType {
+pub enum EWSType {
     Block,
-    Count,
     Echo,
     Err,
     Join,
     Leave,
-    Msg,
-    MsgCut,
-    MsgPut,
     Name,
+    Unblock,
 }
 
-impl WSEventType {
-    pub fn iterator() -> Iter<'static, WSEventType> {
-        static LIST: [WSEventType; 10] = [
-            WSEventType::Block,
-            WSEventType::Count,
-            WSEventType::Echo,
-            WSEventType::Err,
-            WSEventType::Join,
-            WSEventType::Leave,
-            WSEventType::Msg,
-            WSEventType::MsgCut,
-            WSEventType::MsgPut,
-            WSEventType::Name,
+impl EWSType {
+    pub fn iterator() -> Iter<'static, EWSType> {
+        static LIST: [EWSType; 7] = [
+            EWSType::Block,
+            EWSType::Echo,
+            EWSType::Err,
+            EWSType::Join,
+            EWSType::Leave,
+            EWSType::Name,
+            EWSType::Unblock,
         ];
         LIST.iter()
     }
-
     pub fn parse(data: &str) -> Option<Self> {
         let mut result: Option<Self> = None;
         let data_str = data.to_lowercase();
-        for et in WSEventType::iterator() {
+        for et in EWSType::iterator() {
             if data_str.eq(&et.to_string().to_lowercase()) {
                 result = Some(et.clone());
                 break;
@@ -49,23 +44,25 @@ impl WSEventType {
     }
 }
 
-impl fmt::Display for WSEventType {
+impl fmt::Display for EWSType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", serde_json::to_string(&self).unwrap().replace("\"", ""))
     }
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
-pub struct WSEvent {
-    pub et: WSEventType,
-    data_map: HashMap<String, String>,
+// ** Parse the type and data for a web socket event. **
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct EventWS {
+    pub et: EWSType,
+    params: HashMap<String, String>,
 }
 
-impl WSEvent {
-    pub fn new(et: WSEventType, data_map_opt: Option<HashMap<String, String>>) -> Self {
-        WSEvent {
+impl EventWS {
+    pub fn new(et: EWSType, params_opt: Option<HashMap<String, String>>) -> Self {
+        EventWS {
             et,
-            data_map: data_map_opt.unwrap_or(HashMap::new()),
+            params: params_opt.unwrap_or(HashMap::new()),
         }
     }
     // Parse input data of ws event.
@@ -81,13 +78,13 @@ impl WSEvent {
         buf.next();
         let first_tag = buf.next().unwrap_or("");
 
-        let res_event_type = WSEventType::parse(first_tag);
-        if let None = res_event_type {
+        let res_ews_type = EWSType::parse(first_tag);
+        if let None = res_ews_type {
             return Err(format!("unknown command: {:?}", event).to_string());
         }
-        let event_type = res_event_type.unwrap();
+        let ews_type = res_ews_type.unwrap();
 
-        let mut data_map: HashMap<String, String> = HashMap::new();
+        let mut params: HashMap<String, String> = HashMap::new();
         // Parse the input data.
         let parsed = serde_json::from_str::<serde_json::Value>(event)
             .map_err(|e| format!("Serialization: {}", e.to_string()))?;
@@ -99,124 +96,68 @@ impl WSEvent {
             } else {
                 val.to_string()
             };
-            data_map.insert(key.clone(), value);
+            params.insert(key.clone(), value);
         }
-        Ok(WSEvent::new(event_type, Some(data_map)))
+        Ok(EventWS::new(ews_type, Some(params)))
     }
 
     pub fn get(&self, name: &str) -> Option<String> {
-        return self.data_map.get(name).map(|s| s.clone());
+        return self.params.get(name).map(|s| s.clone());
     }
 
-    pub fn block(block: String, count: Option<u64>) -> String {
-        serde_json::to_string(&WSEventBlock { block, count }).unwrap()
-    }
-
-    pub fn count(count: usize) -> String {
-        serde_json::to_string(&WSEventCount { count }).unwrap()
-    }
-
-    pub fn echo(echo: String) -> String {
-        serde_json::to_string(&WSEventEcho { echo }).unwrap()
-    }
-
-    pub fn err(err: String) -> String {
-        serde_json::to_string(&WSEventErr { err }).unwrap()
-    }
-
-    pub fn name(name: String) -> String {
-        serde_json::to_string(&WSEventName { name }).unwrap()
-    }
-
-    pub fn join(join: String, member: String, count: usize) -> String {
-        serde_json::to_string(&WSEventJoin { join, member, count }).unwrap()
-    }
-
-    pub fn leave(leave: String, member: String, count: usize) -> String {
-        serde_json::to_string(&WSEventLeave { leave, member, count }).unwrap()
-    }
-
-    pub fn msg(msg: String, member: String, date: String) -> String {
-        serde_json::to_string(&WSEventMsg { msg, member, date }).unwrap()
-    }
-
-    pub fn msg_cut(msg_cut: String, member: String, date: String) -> String {
-        serde_json::to_string(&WSEventMsgCut { msg_cut, member, date }).unwrap()
-    }
-
-    pub fn msg_put(msg_put: String, member: String, date: String) -> String {
-        serde_json::to_string(&WSEventMsgPut { msg_put, member, date }).unwrap()
+    pub fn get_param(&self, name: &str) -> Result<String, String> {
+        let value = self.get(name).unwrap_or("".to_owned());
+        if value.len() == 0 {
+            Err(format!("The \"{}\" field is required.", name))
+        } else {
+            Ok(value)
+        }
     }
 }
+
+// ** **
 
 // ** Block clients in a room by name. **
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
-pub struct WSEventBlock {
+#[derive(Serialize, Deserialize, Clone)]
+pub struct BlockEWS {
     pub block: String,
-    pub count: Option<u64>, // Amount of blocked clients.
 }
 
-// ** Count of clients in the room. **
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
-pub struct WSEventCount {
-    pub count: usize,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
-pub struct WSEventEcho {
+#[derive(Serialize, Deserialize, Clone)]
+pub struct EchoEWS {
     pub echo: String,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
-pub struct WSEventErr {
+#[derive(Serialize, Deserialize, Clone)]
+pub struct ErrEWS {
     pub err: String,
 }
 
 // ** Join the client to the chat room. **
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
-pub struct WSEventJoin {
+#[derive(Serialize, Deserialize, Clone)]
+pub struct JoinEWS {
     pub join: String,
     pub member: String,
     pub count: usize,
 }
 
 // ** Leave the client from the chat room. **
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
-pub struct WSEventLeave {
+#[derive(Serialize, Deserialize, Clone)]
+pub struct LeaveEWS {
     pub leave: String,
     pub member: String,
     pub count: usize,
 }
 
-// ** Send a message to everyone in the chat room. **
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
-pub struct WSEventMsg {
-    pub msg: String,
-    pub member: String,
-    pub date: String,
-}
-
-// ** Send a delete message to all chat members. **
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
-#[serde(rename_all = "camelCase")]
-pub struct WSEventMsgCut {
-    pub msg_cut: String,
-    pub member: String,
-    pub date: String,
-}
-
-// ** Send a correction to the message to everyone in the chat. **
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
-#[serde(rename_all = "camelCase")]
-pub struct WSEventMsgPut {
-    pub msg_put: String,
-    pub member: String,
-    pub date: String,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
-pub struct WSEventName {
+#[derive(Serialize, Deserialize, Clone)]
+pub struct NameEWS {
     pub name: String,
+}
+
+// ** Unblock clients in a room by name. **
+#[derive(Serialize, Deserialize, Clone)]
+pub struct UnblockEWS {
+    pub unblock: String,
 }
 
 // ** **
@@ -226,23 +167,23 @@ mod tests {
 
     use super::*;
 
-    // ** WSEvent **
+    // ** EventWS **
 
     #[test]
-    fn test_wsevent_parse_1() {
-        for et in WSEventType::iterator() {
+    fn test_eventws_parse_1() {
+        for et in EWSType::iterator() {
             let et_s = et.to_string().to_lowercase();
             let str = format!("{}_a", et_s);
-            let res = WSEventType::parse(&str);
-            assert_eq!(res, None, "WSEventType={:?}", et);
+            let res = EWSType::parse(&str);
+            assert_eq!(res, None, "EWSType={:?}", et);
         }
     }
     #[test]
-    fn test_wsevent_parse_2() {
-        for et in WSEventType::iterator() {
+    fn test_eventws_parse_2() {
+        for et in EWSType::iterator() {
             let et_s = et.to_string().to_lowercase();
-            let res = WSEventType::parse(&et_s);
-            assert_eq!(res, Some(et.clone()), "WSEventType={:?}", et);
+            let res = EWSType::parse(&et_s);
+            assert_eq!(res, Some(et.clone()), "EWSType={:?}", et);
         }
     }
 }
