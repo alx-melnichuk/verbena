8a346509a5fd3ac211ca34f8000d9984346056e5
alx.melnichuk <alx.melnichuk@gmail.com>
2025-03-20 21:27:18 +0200

    BE: Implemented processing: JoinRoomSrv, LeaveRoomSrv, CommandSrv.

--- backend/src/chats/message.rs
+++ backend/src/chats/message.rs
@@ -1,51 +1,59 @@
 use actix::prelude::*;
 
-// ** Block clients in a room by name. **
+// ** Blocking clients in a room by name. (Session -> Server) **
 #[derive(Debug, Clone, Message)]
-#[rtype(result = "String")] // amount of blocked members
-pub struct BlockMembers(
+#[rtype(result = "()")] // amount of blocked members // MAX 4_294_967_295u32
+pub struct BlockingClients(
     pub String, // room_name
     pub String, // client_name
+    pub bool,   // is_blocked
 );
 
-// ** Send a chat message to all clients in the room. **
+// ** Send a block to the client in the room. (Server -> Session) **
 #[derive(Debug, Clone, Message)]
 #[rtype(result = "()")]
-pub struct ChatMessage(
+pub struct BlockingSsn(
+    pub bool, // is_blocked
+);
+
+// ** Send a chat message to all clients in the room. (Server -> Session) **
+#[derive(Debug, Clone, Message)]
+#[rtype(result = "()")]
+pub struct ChatMessageSsn(
     pub String, // message
 );
+
 // ** Commands that have one handler. (Session -> Server) **
 #[derive(Debug, Clone, Message)]
 #[rtype(result = "()")]
-pub enum SrvCommand {
-    Block(BlockMembers),
-    Chat(ChatMessage),
+pub enum CommandSrv {
+    Blocking(BlockingSsn),
+    Chat(ChatMessageSsn),
 }
-
+/*
 // ** Count of clients in the room. **
 #[derive(Clone, Message)]
 #[rtype(result = "usize")]
 pub struct CountMembers(pub String);
-
-// ** Join the client to the chat room. **
+*/
+// ** Join the client to the chat room. (Session -> Server) **
 #[derive(Clone, Message)]
 #[rtype(result = "u64")] // id client
-pub struct JoinRoom(
-    pub String,                 // room_name
-    pub Option<String>,         // client_name
-    pub Recipient<ChatMessage>, // client
-    pub Recipient<SrvCommand>,  // client_session: SessionCommand
+pub struct JoinRoomSrv(
+    pub String,                // room_name
+    pub Option<String>,        // client_name
+    pub Recipient<CommandSrv>, // client_session: SessionCommand
 );
 
 // ** Leave the client from the chat room. (Session -> Server) **
 #[derive(Clone, Message)]
 #[rtype(result = "()")]
-pub struct SrvLeaveRoom(
+pub struct LeaveRoomSrv(
     pub String,         // room_name
     pub u64,            // id client
     pub Option<String>, // client_name
 );
-
+/*
 // ** Send a message to everyone in the chat room. (Session -> Server) **
 #[derive(Clone, Message)]
 #[rtype(result = "()")]
@@ -54,3 +62,4 @@ pub struct SrvSendMessage(
     pub u64,    // id client
     pub String, // message
 );
+*/
