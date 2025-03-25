acd844f6d44340e1044530922115870a1c48e3ba
alx.melnichuk <alx.melnichuk@gmail.com>
2025-03-25 21:31:49 +0200

    FE: minor changes.

--- frontend/src/app/components/field-nickname/field-nickname.component.ts
+++ frontend/src/app/components/field-nickname/field-nickname.component.ts
@@ -4,7 +4,7 @@ import {
 import { CommonModule } from '@angular/common';
 import {
     AbstractControl, ControlValueAccessor, FormControl, FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR, ReactiveFormsModule,
-    ValidationErrors, Validator, ValidatorFn,
+    ValidationErrors, Validator,
 } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput, MatInputModule } from '@angular/material/input';
