3a025dba3f5f85f60eed93a20d93b7e3f80fffbb
alx.melnichuk <alx.melnichuk@gmail.com>
2025-03-27 12:20:47 +0200

    FE Added: "pg-signup.routes.ts".

---
+++ frontend/src/app/pg-signup/pg-signup.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+import { PgSignupComponent } from './pg-signup.component';
+
+export const PG_SIGNUP_ROUTES: Routes = [
+    {
+        path: '',
+        component: PgSignupComponent,
+    },
+];
