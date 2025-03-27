eef8efc81f0703831dc67ce2376abf51c9b2a14d
alx.melnichuk <alx.melnichuk@gmail.com>
2025-03-27 12:20:11 +0200

    FE Added: "pg-login.routes.ts".

---
+++ frontend/src/app/pg-login/pg-login.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+import { PgLoginComponent } from './pg-login.component';
+
+export const PG_LOGIN_ROUTES: Routes = [
+    {
+        path: '',
+        component: PgLoginComponent,
+    },
+];
