7d68f7be7c03e60e9beecf6b42c8f73972dd894e
alx.melnichuk <alx.melnichuk@gmail.com>
2025-03-31 17:30:28 +0300

    FE: Correction of the "panel-stream-state".

--- frontend/src/app/lib-concept/panel-stream-state/panel-stream-state.component.ts
+++ frontend/src/app/lib-concept/panel-stream-state/panel-stream-state.component.ts
@@ -2,7 +2,7 @@ import {
     ChangeDetectionStrategy, Component, ElementRef, Input, OnChanges, OnInit, Renderer2, SimpleChanges, ViewEncapsulation
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 import { StreamState } from 'src/app/lib-stream/stream-api.interface';
 import { HtmlElemUtil } from 'src/app/utils/html-elem.util';
@@ -13,7 +13,7 @@ const ATTR_STATE = 'state';
     selector: 'app-panel-stream-state',
     exportAs: 'appPanelStreamState',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, TranslatePipe],
     templateUrl: './panel-stream-state.component.html',
     styleUrl: './panel-stream-state.component.scss',
     encapsulation: ViewEncapsulation.None,
@@ -34,15 +34,14 @@ export class PanelStreamStateComponent implements OnChanges, OnInit {
     constructor(
         private renderer: Renderer2,
         public hostRef: ElementRef<HTMLElement>,
-        private translateService: TranslateService,
     ) {
     }
 
     ngOnChanges(changes: SimpleChanges): void {
         if (!!changes['streamState']) {
-            const streamState = this.streamState || StreamState.waiting;
-            HtmlElemUtil.setAttr(this.renderer, this.hostRef, ATTR_STATE, streamState);
-            this.valueText = this.getValueText(streamState);
+            this.streamState = this.streamState || StreamState.waiting;
+            HtmlElemUtil.setAttr(this.renderer, this.hostRef, ATTR_STATE, this.streamState);
+            this.valueText = this.getValueText(this.streamState);
         }
     }
 
@@ -62,6 +61,6 @@ export class PanelStreamStateComponent implements OnChanges, OnInit {
             case StreamState.paused: res = 'stream_state.has_paused'; break;
             case StreamState.stopped: res = 'stream_state.has_ended'; break;
         }
-        return (!!res ? this.translateService.instant(res) : '');
+        return res;
     }
 }
