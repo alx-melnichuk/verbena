0e330ab4b43ee07e82f226397a6b3a96710484a6
alx.melnichuk <alx.melnichuk@gmail.com>
2025-04-01 21:19:49 +0300

    FE: Correction of the "panel-stream-params".

--- frontend/src/app/lib-concept/panel-stream-params/panel-stream-params.component.ts
+++ frontend/src/app/lib-concept/panel-stream-params/panel-stream-params.component.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatChipsModule } from '@angular/material/chips';
 import { TranslatePipe } from '@ngx-translate/core';
 
 import { DateTimeFormatPipe } from 'src/app/common/date-time-format.pipe';
+import { AvatarComponent } from 'src/app/components/avatar/avatar.component';
 import { DateTimeTimerComponent } from 'src/app/components/date-time-timer/date-time-timer.component';
 
 
@@ -11,7 +11,7 @@ import { DateTimeTimerComponent } from 'src/app/components/date-time-timer/date-
     selector: 'app-panel-stream-params',
     exportAs: 'appPanelStreamParams',
     standalone: true,
-    imports: [CommonModule, DateTimeFormatPipe, MatChipsModule, TranslatePipe, DateTimeTimerComponent],
+    imports: [CommonModule, DateTimeFormatPipe, TranslatePipe, AvatarComponent, DateTimeTimerComponent],
     templateUrl: './panel-stream-params.component.html',
     styleUrl: './panel-stream-params.component.scss',
     encapsulation: ViewEncapsulation.None,
@@ -19,17 +19,19 @@ import { DateTimeTimerComponent } from 'src/app/components/date-time-timer/date-
 })
 export class PanelStreamParamsComponent {
     @Input()
+    public avatar: string | null | undefined;
+    @Input()
+    public countOfViewer: number | null | undefined;
+    @Input()
     public isShowTimer: boolean | null | undefined;
     @Input()
     public locale: string | null = null;
     @Input()
-    public title: string | null | undefined;
+    public nickname: string | null | undefined;
     @Input()
-    public tags: string[] = [];
+    public tags: string[] | null | undefined = [];
     @Input()
     public startDateTime: Date | null | undefined;
-    @Input()
-    public countOfViewer: number | null | undefined;
 
     readonly formatDateTime: Intl.DateTimeFormatOptions = { dateStyle: 'long', timeStyle: 'short' };
 }
