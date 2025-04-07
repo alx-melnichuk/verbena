d0a376f11b6149c096d955004f6aee5443943cd8
alx.melnichuk <alx.melnichuk@gmail.com>
2025-04-07 20:45:41 +0300

    FE: Added: "start", "started", "stopped".

--- frontend/src/app/lib-concept/panel-stream-params/panel-stream-params.component.ts
+++ frontend/src/app/lib-concept/panel-stream-params/panel-stream-params.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { TranslatePipe } from '@ngx-translate/core';
 
 import { DateTimeFormatPipe } from 'src/app/common/date-time-format.pipe';
-import { AvatarComponent } from 'src/app/components/avatar/avatar.component';
 import { DateTimeTimerComponent } from 'src/app/components/date-time-timer/date-time-timer.component';
 
 
@@ -11,7 +10,7 @@ import { DateTimeTimerComponent } from 'src/app/components/date-time-timer/date-
     selector: 'app-panel-stream-params',
     exportAs: 'appPanelStreamParams',
     standalone: true,
-    imports: [CommonModule, DateTimeFormatPipe, TranslatePipe, AvatarComponent, DateTimeTimerComponent],
+    imports: [CommonModule, DateTimeFormatPipe, TranslatePipe, DateTimeTimerComponent],
     templateUrl: './panel-stream-params.component.html',
     styleUrl: './panel-stream-params.component.scss',
     encapsulation: ViewEncapsulation.None,
@@ -19,19 +18,18 @@ import { DateTimeTimerComponent } from 'src/app/components/date-time-timer/date-
 })
 export class PanelStreamParamsComponent {
     @Input()
-    public avatar: string | null | undefined;
-    @Input()
     public countOfViewer: number | null | undefined;
     @Input()
     public isShowTimer: boolean | null | undefined;
     @Input()
     public locale: string | null = null;
-    @Input()
-    public nickname: string | null | undefined;
-    @Input()
-    public tags: string[] | null | undefined = [];
-    @Input()
-    public startDateTime: Date | null | undefined;
+    @Input() // Time when stream should start.
+    public start: Date | null | undefined;
+    @Input() // Time when stream was started
+    public started: Date | null | undefined;
+    @Input() // Time when stream was stopped
+    public stopped: Date | null | undefined;
 
     readonly formatDateTime: Intl.DateTimeFormatOptions = { dateStyle: 'long', timeStyle: 'short' };
+
 }
