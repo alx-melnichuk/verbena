49a6febac106199a82c0b5743c3b523f1d4d513a
alx.melnichuk <alx.melnichuk@gmail.com>
2025-04-07 21:44:50 +0300

    FE: Correction of the "sidebar".

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -12,20 +12,36 @@ import { CommonModule } from '@angular/common';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SidebarComponent {
+    // @Input()
+    // @HostBinding('attr.is-above-parent')
+    // public isAboveParent: boolean = false;
     @Input()
-    @HostBinding('attr.is-above-parent')
-    public isAboveParent: boolean = false;
-    @Input()
-    @HostBinding('attr.is-open')
     public isOpen: boolean = false;
+    @Input()
+    public isOver: boolean = false;
 
-    @Output()
-    readonly clickByVeil: EventEmitter<boolean> = new EventEmitter();
+    // @Output()
+    // readonly clickByVeil: EventEmitter<boolean> = new EventEmitter();
+
+    @HostBinding('attr.is-open')
+    public get attrIsOpen(): string | null {
+        return this.isOpen ? '' : null;
+    }
+
+    @HostBinding('attr.is-over')
+    public get attrIsOver(): string | null {
+        return this.isOver ? '' : null;
+    }
+
+    // @HostBinding('class.sb-relative')
+    // public get classIsRelativeVal(): boolean {
+    //     return this.isVeilByClosing;
+    // }
 
     constructor() {
     }
 
-    public doClickByVeil(): void {
-        this.clickByVeil.emit(true);
-    }
+    // public doClickByVeil(): void {
+    //     this.clickByVeil.emit(true);
+    // }
 }
