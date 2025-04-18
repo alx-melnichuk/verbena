fc651b3f176c236ad6c1ba101198fb4a565512df
alx.melnichuk <alx.melnichuk@gmail.com>
2025-04-18 20:57:19 +0300

    FE: Correction of the "SocketApiService".

--- frontend/src/app/lib-socket/socket-api.service.ts
+++ frontend/src/app/lib-socket/socket-api.service.ts
@@ -1,74 +1,53 @@
 import { Injectable } from '@angular/core';
 
-let idx = 0;
-
 @Injectable({
     providedIn: 'root'
 })
 export class SocketApiService {
-    id = idx++;
-    public socket: WebSocket | null = null
 
-    // public onChange: (val: string) => void = () => { };
-    public handlerByOpening: () => void = () => { };
-    public handlerByClosing: () => void = () => { };
-    public handlerByMessages: (val: string) => void = () => { };
+    public handlOnClose: () => void = () => { };
+    public handlOnError: (ev: Event) => void = this.defOnError;
+    public handlOnOpen: () => void = () => { };
+    public handlOnMessage: (val: string) => void = () => { };
 
     private uriWs: string = '';
-    private isConnected: boolean = false;
+    private socket: WebSocket | null = null
 
     constructor() {
-        console.log(`SocketApiService(id: ${this.id})`); // #
+        console.log(`SocketApiSrv()`); // #
     }
     /** Connect to the server's web socket. */
     public connect(uriWs: string): void {
         this.disconnect();
 
         this.uriWs = uriWs;
-        console.log(`Connecting with "${this.uriWs}" ...`); // #
+        console.log(`SocketApiSrv.connect(${uriWs})`); // #
         // Create a web socket.
         this.socket = new WebSocket(this.uriWs);
 
-        this.socket.onopen = () => {
-            console.log('Connected'); // #
-            this.isConnected = true;
-            if (this.handlerByOpening != null) {
-                this.handlerByOpening();
-            }
-        }
-
-        this.socket.onmessage = (ev: MessageEvent<any>) => {
-            console.log('Received:' + ev.data, 'message'); // #
-            if (this.handlerByMessages != null) {
-                this.handlerByMessages(ev.data);
-            }
-        }
-
-        this.socket.onclose = () => {
-            console.log('Disconnected'); // #
-            this.socket = null;
-            if (this.handlerByClosing != null) {
-                this.handlerByClosing();
-            }
-        }
+        this.socket.onclose = this.handlOnClose;
+        this.socket.onerror = this.handlOnError;
+        this.socket.onopen = this.handlOnOpen;
+        this.socket.onmessage = (ev: MessageEvent<any>) => this.handlOnMessage(ev.data);
     }
     /** Disconnect from the server's web socket. */
     public disconnect(): void {
+        console.log(`SocketApiSrv.disconnect()`); // #
         if (this.socket) {
-            console.log('Disconnecting...'); // #
-            this.isConnected = false;
             this.socket.close();
             this.socket = null;
         }
     }
 
     public hasConnect(): boolean {
-        return !!this.socket && this.isConnected;
+        return !!this.socket;
     }
 
     public send(text: string): void {
-        if (!!this.socket && this.isConnected) {
-            this.socket.send(text);
-        }
+        this.socket?.send(text);
+    }
+
+    private defOnError(ev: Event): void {
+        console.log("WebSocket error: ", ev);
     }
 }
--- frontend/src/app/lib-socket/socket-chat.service.ts
+++ frontend/src/app/lib-socket/socket-chat.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject, Subscription } from 'rxjs';
+
 import { SocketApiService } from './socket-api.service';
 import { EventWS, EWSType } from './socket-chat.interface';
-
-let idx = 0;
 
 export interface ChatConfig {
     nickname: string;
@@ -10,53 +10,140 @@ export interface ChatConfig {
     access?: string | null | undefined; // AccessToken
 }
 
+const TIMEOUT_CONNECT = 60; // sec
+const DEF_RESOLVE = () => { };
+const DEF_REJECT = (reason: unknown) => { };
+
 @Injectable({
     providedIn: 'root'
 })
 export class SocketChatService {
-    id = idx++;
 
-    public isChatReady: boolean = false;
-    public get isConfigurat(): boolean {
-        return !!this.chatConfig;
-    }
-    public countOfMembers: number = 0;
+    public isReady: boolean = false;
 
+    public get isConfigurat(): boolean { return !!this.chatConfig; }
     private chatConfig: ChatConfig | null = null;
 
-    constructor(private socketApiService: SocketApiService) {
-        const srvApiId = this.socketApiService.id;
-        console.log(`SocketChatService(id: ${this.id}) srvApiId: ${srvApiId}`); // #
+    public get countOfMembers(): number { return this.innCountOfMembers; }
+    private innCountOfMembers: number = 0;
+    private innCountOfMembersSub: Subject<number | null> = new Subject();
+    public countOfMembers$ = this.innCountOfMembersSub.asObservable();
 
-        this.socketApiService.handlerByOpening = this.wsHandlerByOpening;
-        this.socketApiService.handlerByMessages = this.wsHandlerByMessages;
-        this.socketApiService.handlerByClosing = this.wsHandlerByClosing;
+    private innConnectResolve: () => void = DEF_RESOLVE;
+    private innConnectReject: (reason: unknown) => void = DEF_REJECT;
+    private innConnectTimer: number | undefined;
+
+    private innSendMsgResolve: () => void = DEF_RESOLVE;
+    private innSendMsgReject: (reason: unknown) => void = DEF_REJECT;
+    private innSendMsgTimer: number | undefined;
+
+    constructor(private socketApiService: SocketApiService) {
+        console.log(`SocketChatSrv()`); // #
+        this.socketApiService.handlOnClose = this.handlOnClose;
+        this.socketApiService.handlOnError = this.handlOnError;
+        this.socketApiService.handlOnOpen = this.handlOnOpen;
+        this.socketApiService.handlOnMessage = this.handlOnMessage;
     }
 
     // ** Public API **
 
     public config(chatConfig: ChatConfig): void {
-        this.chatConfig = { ...chatConfig };
+        if (!!chatConfig.nickname && !!chatConfig.room) {
+            this.chatConfig = { ...chatConfig };
+        }
     }
     /** Connect to the server web socket chat. */
     public connect(pathName: string, host?: string | null): void {
         if (!!this.chatConfig) {
             const { location } = window
             const proto = location.protocol.startsWith('https') ? 'wss' : 'ws';
-            const host2 = !!host ? host : location.host;
+            const host2 = host || location.host;
             const uriWs = `${proto}://${host2}/${pathName}`;
-
+            // Connect to the server's web socket.
             this.socketApiService.connect(uriWs);
         }
     }
+    public connect2(pathName: string, host?: string | null): Promise<void> {
+        if (!this.chatConfig) {
+            return Promise.reject();
+        }
+        this.isReady = false;
+        const { location } = window;
+        const proto = location.protocol.startsWith('https') ? 'wss' : 'ws';
+        const host2 = host || location.host;
+        const uriWs = `${proto}://${host2}/${pathName}`;
+        console.log(`SocketChatSrv_.connect2() socketApiService.connect(uriWs);`); // #
+        // Connect to the server's web socket.
+        this.socketApiService.connect(uriWs);
+
+        return new Promise<void>((resolve: () => void, reject: (reason: unknown) => void) => {
+            console.log(`SocketChatSrv_.connect2() new Promise<void>() !!this.innConnectTimer: ${!!this.innConnectTimer}`); // #
+            this.innConnectResolve = resolve;
+            this.innConnectReject = reject;
+            if (!!this.innConnectTimer) {
+                window.clearTimeout(this.innConnectTimer);
+            }
+            this.innConnectTimer = window.setTimeout(() => {
+                this.innConnectTimer = undefined;
+                console.log(`SocketChatSrv_.connect2() setTimeout() innConnectReject();`)
+                this.innConnectReject(false);
+                this.clearConnectionParams();
+            }, TIMEOUT_CONNECT * 1000);
+        });
+    }
+    public sendMsg(msg: string): Promise<void> {
+        this.socketApiService.send(msg);
+        return new Promise<void>((resolve: () => void, reject: (reason: unknown) => void) => {
+            console.log(`SocketChatSrv_.sendMsg() new Promise<void>() !!this.innSendMsgTimer: ${!!this.innSendMsgTimer}`); // #
+            this.innSendMsgResolve = resolve;
+            this.innSendMsgReject = reject;
+            if (!!this.innSendMsgTimer) {
+                window.clearTimeout(this.innSendMsgTimer);
+            }
+            this.innSendMsgTimer = window.setTimeout(() => {
+                this.innSendMsgTimer = undefined;
+                console.log(`SocketChatSrv_.sendMsg() setTimeout() innSendMsgReject();`)
+                this.innSendMsgReject(false);
+                this.clearSendMsgParams();
+            }, TIMEOUT_CONNECT * 1000);
+        });
+    }
+    /*
+    return new Promise<void>((resolve: () => void, reject: (reason: unknown) => void) => {
+        this.translate.use(locale).pipe(first())
+            .subscribe({
+                next: () => {
+                    this.currLocale = locale;
+                    this.dateAdapter.setLocale(locale);
+                    HttpErrorUtil.setTranslate(this.translate);
+                    resolve();
+                },
+                error: (err) => reject(err)
+            });
+    });
+    */
+
     /** Disconnect from the server web socket chat. */
     public disconnect(): void {
-        this.isChatReady = false;
+        this.isReady = false;
         this.socketApiService.disconnect();
     }
 
-    public wsHandlerByOpening = (): void => {
-        console.log(`PgConceptView.handlerByOpening()`); // #
+    // ** Event handling in SocketApiService **
+
+    /** Processing closing of connection. */
+    public handlOnClose = (): void => {
+        console.log(`SocketChatSrv.handlOnClose()`); // #
+    };
+    /** Processing error data. */
+    public handlOnError = (err: any): void => {
+        console.log(`SocketChatSrv.handlOnError() err:`, err); // #
+        console.log(`SocketChatSrv_.handlOnError() this.innSendMsgReject('error');`); // #
+        this.innSendMsgReject('error');
+    };
+    /** Processing opening of connection. */
+    public handlOnOpen = (): void => {
+        console.log(`SocketChatSrv.handlOnOpen()`); // #
         if (!!this.chatConfig) {
             // Set the chat user name.
             this.socketApiService.send(`{ "name": "${this.chatConfig.nickname}" }`);
@@ -66,19 +153,25 @@ export class SocketChatService {
             this.socketApiService.send(`{ "join": "${this.chatConfig.room}"${accessStr} }`);
         }
     };
-
-    public wsHandlerByClosing = (): void => {
-        console.log(`PgConceptView.handlerByClosing()`); // #
-    };
-
-    public wsHandlerByMessages = (value: string): void => {
-        console.log(`PgConceptView.handlerByMessages(${value})`); // #
-        const eventWS: EventWS | null = EventWS.parse(value);
+    /** Processing receiving of message. */
+    public handlOnMessage = (val: string): void => {
+        console.log(`SocketChatSrv.handlOnMessage() val:`, val); // #
+        console.log(`SocketChatSrv_.handlOnMessage() this.innSendMsgResolve();`); // #
+        this.innSendMsgResolve();
+        const eventWS: EventWS | null = EventWS.parse(val);
         this.eventAnalysis(eventWS, this.chatConfig);
-
     };
+
+    // ** **
 
     // ** Private API **
+
+    private setCountOfMembers(value: number): void {
+        if (value >= 0 && value != this.innCountOfMembers) {
+            this.innCountOfMembers = value;
+            this.innCountOfMembersSub.next(this.countOfMembers);
+        }
+    }
 
     private eventAnalysis(eventWS: EventWS | null, chatConfig: ChatConfig | null) {
         if (!eventWS || !chatConfig) {
@@ -86,16 +179,39 @@ export class SocketChatService {
         }
         if (eventWS.et == EWSType.Join) {
             // Join the client to the chat room. { join: String, member: String, count: usize }
-            if (!this.isChatReady && eventWS.get('member') == chatConfig.nickname) {
-                this.isChatReady = true;
-                console.log(`eventWS.et: Join, isChatReady: ${this.isChatReady}`)
+            if (!this.isReady && eventWS.get('member') == chatConfig.nickname) {
+                this.isReady = true;
+                console.log(`SocketChatSrv.analysis() ev.et: Join, isReady: ${this.isReady}`)
+                console.log(`SocketChatSrv_.analysis() this.innConnectResolve();`)
+                this.innConnectResolve();
+                this.clearConnectionParams();
             }
-            const count = parseInt(eventWS.get('count') || '-1', 10) || -1;
-            if (count > -1) {
-                this.countOfMembers = count;
-            }
-            console.log(`eventWS.et: Join, count: ${count}`)
+            this.setCountOfMembers(parseInt(eventWS.get('count') || '-1', 10) || -1);
+        } else if (eventWS.et == EWSType.Leave) {
+            this.setCountOfMembers(parseInt(eventWS.get('count') || '-1', 10) || -1);
         }
     }
 
+
+    private clearConnectionParams(): void {
+        this.innConnectResolve = DEF_RESOLVE;
+        this.innConnectReject = DEF_REJECT;
+        console.log(`SocketChatSrv_.clearConnectionParams() !!this.innConnectTimer: ${!!this.innConnectTimer}`); // #
+        if (!!this.innConnectTimer) {
+            console.log(`SocketChatSrv_.clearConnectionParams() window.clearTimeout(this.innConnectTimer);`); // #
+            window.clearTimeout(this.innConnectTimer);
+            this.innConnectTimer = undefined;
+        }
+    }
+
+    private clearSendMsgParams(): void {
+        this.innSendMsgResolve = DEF_RESOLVE;
+        this.innSendMsgReject = DEF_REJECT;
+        console.log(`SocketChatSrv_.clearSendMsgParams() !!this.innSendMsgTimer: ${!!this.innSendMsgTimer}`); // #
+        if (!!this.innSendMsgTimer) {
+            console.log(`SocketChatSrv_.clearSendMsgParams() window.clearTimeout(this.innSendMsgTimer);`); // #
+            window.clearTimeout(this.innSendMsgTimer);
+            this.innSendMsgTimer = undefined;
+        }
+    }
 }
