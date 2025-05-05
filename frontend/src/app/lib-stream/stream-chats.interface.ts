333ae67fdbbb23dbe22452f13a7032924d4247f6
alx.melnichuk <alx.melnichuk@gmail.com>
2025-05-05 21:14:31 +0300

    FE: Correction of "ChatMsg".

--- frontend/src/app/lib-stream/stream-chats.interface.ts
+++ frontend/src/app/lib-stream/stream-chats.interface.ts
@@ -1,18 +1,19 @@
 import { StringDateTime } from "../common/string-date-time";
 
 export interface ChatMsg {
+    id: number;
     date: StringDateTime;
     member: string;
     msg: string;
 };
 
 export class ChatMsgUtil {
-    public static create(obj: { date: StringDateTime, member: string, msg: string }): ChatMsg {
-        const { date, member, msg } = obj;
-        return { date, member, msg };
+    public static create(obj: { id: number, date: StringDateTime, member: string, msg: string }): ChatMsg {
+        const { id, date, member, msg } = obj;
+        return { id, date, member, msg };
     }
-    public static getChatMsg(date: StringDateTime, member: string, msg: string): ChatMsg {
-        return { date, member, msg };
+    public static getChatMsg(id: number, date: StringDateTime, member: string, msg: string): ChatMsg {
+        return { id, date, member, msg };
     }
 }
 
