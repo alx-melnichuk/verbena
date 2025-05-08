1efc1b7a9cb74ff94e0c3a44a51b305cb01f62b2
alx.melnichuk <alx.melnichuk@gmail.com>
2025-05-08 12:06:52 +0300

    BE: Replacing "log::error!()" with "error!()".

--- backend/src/utils/token_verification.rs
+++ backend/src/utils/token_verification.rs
@@ -1,3 +1,5 @@
+use log::{debug, error, log_enabled, Level::Debug};
+
 use crate::errors::AppError;
 #[cfg(not(all(test, feature = "mockdata")))]
 use crate::profiles::profile_orm::impls::ProfileOrmApp;
@@ -21,17 +23,20 @@ pub async fn check_token_and_get_profile(
     session_orm: &SessionOrmApp,
     profile_orm: &ProfileOrmApp,
 ) -> Result<Profile, AppError> {
+    #[rustfmt::skip]
+    let opt_timer0 = if log_enabled!(Debug) { Some(std::time::Instant::now()) } else { None };
+
     // let timer1 = std::time::Instant::now();
     // Find a session for a given user.
     let opt_session = session_orm.get_session_by_id(user_id).map_err(|e| {
-        log::error!("{}: {}", err::CD_DATABASE, e.to_string());
+        error!("{}: {}", err::CD_DATABASE, e.to_string());
         return AppError::database507(&e.to_string()); // 507
     })?;
     // let timer1s = format!("{:.2?}", timer1.elapsed());
     let session = opt_session.ok_or_else(|| {
         // There is no session for this user.
         let message = format!("{}; user_id: {}", err::MSG_SESSION_NOT_FOUND, user_id);
-        log::error!("{}: {}", err::CD_NOT_ACCEPTABLE, &message);
+        error!("{}: {}", err::CD_NOT_ACCEPTABLE, &message);
         AppError::not_acceptable406(&message) // 406
     })?;
     // Each session contains an additional numeric value.
@@ -40,12 +45,12 @@ pub async fn check_token_and_get_profile(
     if session_num_token != num_token {
         // If they do not match, then this is an error.
         let message = format!("{}; user_id: {}", err::MSG_UNACCEPTABLE_TOKEN_NUM, user_id);
-        log::error!("{}: {}", err::CD_UNAUTHORIZED, &message);
+        error!("{}: {}", err::CD_UNAUTHORIZED, &message);
         return Err(AppError::unauthorized401(&message)); // 401
     }
     // let timer2 = std::time::Instant::now();
     let result = profile_orm.get_profile_user_by_id(user_id, false).map_err(|e| {
-        log::error!("{}: {}", err::CD_DATABASE, e.to_string());
+        error!("{}: {}", err::CD_DATABASE, e.to_string());
         AppError::database507(&e.to_string()) // 507
     })?;
     // let timer2s = format!("{:.2?}", timer2.elapsed());
@@ -53,9 +58,13 @@ pub async fn check_token_and_get_profile(
 
     let profile = result.ok_or_else(|| {
         let message = format!("{}; user_id: {}", MSG_UNACCEPTABLE_TOKEN_ID, user_id);
-        log::error!("{}: {}", err::CD_UNAUTHORIZED, &message);
+        error!("{}: {}", err::CD_UNAUTHORIZED, &message);
         AppError::unauthorized401(&message) // 401+
     })?;
 
+    if let Some(timer0) = opt_timer0 {
+        #[rustfmt::skip]
+        debug!("timer0: {}, user_id: {}, nickname: {}", format!("{:.2?}", timer0.elapsed()), profile.user_id, &profile.nickname);
+    }
     Ok(profile)
 }
