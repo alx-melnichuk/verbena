bdbcdceb1a9821e6908d77594d0bebb7c4faec02
alx.melnichuk <alx.melnichuk@gmail.com>
2025-07-24 17:28:07 +0300

    BE: Add library "vrb-dbase".

---
+++ backend/vrb_dbase/src/dbase.rs
@@ -0,0 +1,19 @@
+use diesel::{r2d2::{ConnectionManager, Pool, PooledConnection}, PgConnection};
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+
+pub type Connection = PgConnection;
+
+pub type DbPool = Pool<ConnectionManager<Connection>>;
+
+pub type DbPooledConnection = PooledConnection<ConnectionManager<Connection>>;
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+
+pub fn init_db_pool(db_url: &str) -> DbPool {
+    let manager = ConnectionManager::<Connection>::new(db_url);
+    Pool::builder().build(manager).expect("Failed to create pool.")
+}
+
+pub fn run_migration(conn: &mut PgConnection) {
+    conn.run_pending_migrations(MIGRATIONS).unwrap();
+}
