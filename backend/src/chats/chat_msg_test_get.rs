5dc4258a856c55a75dd6fdbc51576781e28a2698
alx.melnichuk <alx.melnichuk@gmail.com>
2025-08-05 21:01:16 +0300

    BE: Modify "chat_msg_test_get".

--- backend/src/chats/chat_msg_test_get.rs
+++ backend/src/chats/chat_msg_test_get.rs
@@ -10,12 +10,13 @@ mod tests {
     use serde_json;
 
     use crate::chats::{
-        chat_message_controller::{
-            get_chat_message,
-            tests::{configure_chat_message, get_cfg_data, header_auth},
-        },
+        chat_message_controller::{get_chat_message, tests as ChtCtTest},
         chat_message_models::ChatMessageDto,
-        chat_message_orm::tests::ChatMsgTest,
+        chat_message_orm::tests::ChatMessageOrmTest as ChMesTest,
+    };
+    use crate::profiles::{
+        config_jwt,
+        profile_orm::tests::{ProfileOrmTest as ProflTest, USER},
     };
 
     const MSG_FAILED_DESER: &str = "Failed to deserialize response from JSON.";
@@ -24,18 +25,24 @@ mod tests {
 
     #[actix_web::test]
     async fn test_get_chat_message_search_by_str_id() {
-        let (cfg_c, data_c, token) = get_cfg_data(4);
-        let stream_id = ChatMsgTest::stream_ids().get(0).unwrap().clone();
+        let token = ProflTest::token1();
+        let data_p = ProflTest::profiles(&[USER]);
+        let data_cm = ChMesTest::chat_messages(6);
+        let stream_id = data_cm.0.get(0).unwrap().stream_id.clone();
         #[rustfmt::skip]
-        let ch_msg1_dto_vec: Vec<ChatMessageDto> = data_c.2.clone()
+        let ch_msg1_dto_vec: Vec<ChatMessageDto> = data_cm.0
             .iter().map(|ch_msg| ChatMessageDto::from(ch_msg.clone())).collect();
         #[rustfmt::skip]
         let app = test::init_service(
-            App::new().service(get_chat_message).configure(configure_chat_message(cfg_c, data_c))).await;
+            App::new().service(get_chat_message)
+                .configure(ProflTest::cfg_config_jwt(config_jwt::get_test_config()))
+                .configure(ProflTest::cfg_profile_orm(data_p))
+                .configure(ChMesTest::cfg_chat_message_orm(data_cm))
+        ).await;
         #[rustfmt::skip]
         let req = test::TestRequest::get()
             .uri(&format!("/api/chat_messages?streamId={}", stream_id))
-            .insert_header(header_auth(&token)).to_request();
+            .insert_header(ChtCtTest::header_auth(&token)).to_request();
         let resp: dev::ServiceResponse = test::call_service(&app, req).await;
         assert_eq!(resp.status(), StatusCode::OK); // 200
 
@@ -43,7 +50,6 @@ mod tests {
         assert_eq!(resp.headers().get(CONTENT_TYPE).unwrap(), HeaderValue::from_static("application/json"));
         let body = body::to_bytes(resp.into_body()).await.unwrap();
         let response: Vec<ChatMessageDto> = serde_json::from_slice(&body).expect(MSG_FAILED_DESER);
-
         let json = serde_json::json!(&ch_msg1_dto_vec).to_string();
         let ch_msg1_vec_ser: Vec<ChatMessageDto> = serde_json::from_slice(json.as_bytes()).expect(MSG_FAILED_DESER);
         assert_eq!(response.len(), ch_msg1_vec_ser.len());
@@ -51,18 +57,24 @@ mod tests {
     }
     #[actix_web::test]
     async fn test_get_chat_message_search_by_str_id_sort_des() {
-        let (cfg_c, data_c, token) = get_cfg_data(4);
-        let stream_id = ChatMsgTest::stream_ids().get(0).unwrap().clone();
+        let token = ProflTest::token1();
+        let data_p = ProflTest::profiles(&[USER]);
+        let data_cm = ChMesTest::chat_messages(6);
+        let stream_id = data_cm.0.get(0).unwrap().stream_id.clone();
         #[rustfmt::skip]
-        let ch_msg1_dto_vec: Vec<ChatMessageDto> = data_c.2.clone()
+        let ch_msg_dto_vec: Vec<ChatMessageDto> = data_cm.0
             .iter().map(|ch_msg| ChatMessageDto::from(ch_msg.clone())).rev().collect();
         #[rustfmt::skip]
         let app = test::init_service(
-            App::new().service(get_chat_message).configure(configure_chat_message(cfg_c, data_c))).await;
+            App::new().service(get_chat_message)
+                .configure(ProflTest::cfg_config_jwt(config_jwt::get_test_config()))
+                .configure(ProflTest::cfg_profile_orm(data_p))
+                .configure(ChMesTest::cfg_chat_message_orm(data_cm))
+        ).await;
         #[rustfmt::skip]
         let req = test::TestRequest::get()
             .uri(&format!("/api/chat_messages?streamId={}&isSortDes=true", stream_id))
-            .insert_header(header_auth(&token)).to_request();
+            .insert_header(ChtCtTest::header_auth(&token)).to_request();
         let resp: dev::ServiceResponse = test::call_service(&app, req).await;
         assert_eq!(resp.status(), StatusCode::OK); // 200
 
@@ -71,28 +83,33 @@ mod tests {
         let body = body::to_bytes(resp.into_body()).await.unwrap();
         let response: Vec<ChatMessageDto> = serde_json::from_slice(&body).expect(MSG_FAILED_DESER);
 
-        let json = serde_json::json!(&ch_msg1_dto_vec).to_string();
-        let ch_msg1_vec_ser: Vec<ChatMessageDto> = serde_json::from_slice(json.as_bytes()).expect(MSG_FAILED_DESER);
-        assert_eq!(response.len(), ch_msg1_vec_ser.len());
-        assert_eq!(response, ch_msg1_vec_ser);
+        let json = serde_json::json!(&ch_msg_dto_vec).to_string();
+        let ch_msg_vec_ser: Vec<ChatMessageDto> = serde_json::from_slice(json.as_bytes()).expect(MSG_FAILED_DESER);
+        assert_eq!(response.len(), ch_msg_vec_ser.len());
+        assert_eq!(response, ch_msg_vec_ser);
     }
     #[actix_web::test]
     async fn test_get_chat_message_search_by_str_id_part1() {
-        let (cfg_c, data_c, token) = get_cfg_data(4);
-        let stream_id = ChatMsgTest::stream_ids().get(0).unwrap().clone();
-        let limit = 3;
+        let token = ProflTest::token1();
+        let data_p = ProflTest::profiles(&[USER]);
+        let data_cm = ChMesTest::chat_messages(6);
+        let stream_id = data_cm.0.get(0).unwrap().stream_id.clone();
         #[rustfmt::skip]
-        let ch_msg_dto_vec: Vec<ChatMessageDto> = data_c.2.clone()
+        let ch_msg_dto_vec: Vec<ChatMessageDto> = data_cm.0
             .iter().map(|ch_msg| ChatMessageDto::from(ch_msg.clone())).collect();
-
-        let ch_msg1_dto_vec = &ch_msg_dto_vec[0..limit];
+        let limit = 3;
+        let ch_msg2_dto_vec = &ch_msg_dto_vec[0..limit];
         #[rustfmt::skip]
         let app = test::init_service(
-            App::new().service(get_chat_message).configure(configure_chat_message(cfg_c, data_c))).await;
+            App::new().service(get_chat_message)
+                .configure(ProflTest::cfg_config_jwt(config_jwt::get_test_config()))
+                .configure(ProflTest::cfg_profile_orm(data_p))
+                .configure(ChMesTest::cfg_chat_message_orm(data_cm))
+        ).await;
         #[rustfmt::skip]
         let req = test::TestRequest::get()
             .uri(&format!("/api/chat_messages?streamId={}&limit={}", stream_id, limit))
-            .insert_header(header_auth(&token)).to_request();
+            .insert_header(ChtCtTest::header_auth(&token)).to_request();
         let resp: dev::ServiceResponse = test::call_service(&app, req).await;
         assert_eq!(resp.status(), StatusCode::OK); // 200
 
@@ -101,30 +118,35 @@ mod tests {
         let body = body::to_bytes(resp.into_body()).await.unwrap();
         let response: Vec<ChatMessageDto> = serde_json::from_slice(&body).expect(MSG_FAILED_DESER);
 
-        let json = serde_json::json!(&ch_msg1_dto_vec).to_string();
-        let ch_msg1_vec_ser: Vec<ChatMessageDto> = serde_json::from_slice(json.as_bytes()).expect(MSG_FAILED_DESER);
-        assert_eq!(response.len(), ch_msg1_vec_ser.len());
-        assert_eq!(response, ch_msg1_vec_ser);
+        let json = serde_json::json!(&ch_msg2_dto_vec).to_string();
+        let ch_msg2_vec_ser: Vec<ChatMessageDto> = serde_json::from_slice(json.as_bytes()).expect(MSG_FAILED_DESER);
+        assert_eq!(response.len(), ch_msg2_vec_ser.len());
+        assert_eq!(response, ch_msg2_vec_ser);
     }
     #[actix_web::test]
     async fn test_get_chat_message_search_by_str_id_part2() {
-        let (cfg_c, data_c, token) = get_cfg_data(4);
-        let stream_id = ChatMsgTest::stream_ids().get(0).unwrap().clone();
-        let limit = 3;
+        let token = ProflTest::token1();
+        let data_p = ProflTest::profiles(&[USER]);
+        let data_cm = ChMesTest::chat_messages(6);
+        let stream_id = data_cm.0.get(0).unwrap().stream_id.clone();
         #[rustfmt::skip]
-        let ch_msg_dto_vec: Vec<ChatMessageDto> = data_c.2.clone()
+        let ch_msg_dto_vec: Vec<ChatMessageDto> = data_cm.0
             .iter().map(|ch_msg| ChatMessageDto::from(ch_msg.clone())).collect();
-
+        let limit = 3;
         let min_date = ch_msg_dto_vec.get(limit - 1).unwrap().date;
         let min_date_str = min_date.to_rfc3339_opts(SecondsFormat::Millis, true);
-        let ch_msg1_dto_vec = &ch_msg_dto_vec[limit..2 * limit];
+        let ch_msg2_dto_vec = &ch_msg_dto_vec[limit..2 * limit];
         #[rustfmt::skip]
         let app = test::init_service(
-            App::new().service(get_chat_message).configure(configure_chat_message(cfg_c, data_c))).await;
+            App::new().service(get_chat_message)
+                .configure(ProflTest::cfg_config_jwt(config_jwt::get_test_config()))
+                .configure(ProflTest::cfg_profile_orm(data_p))
+                .configure(ChMesTest::cfg_chat_message_orm(data_cm))
+        ).await;
         #[rustfmt::skip]
         let req = test::TestRequest::get()
             .uri(&format!("/api/chat_messages?streamId={}&minDate={}&limit={}", stream_id, min_date_str, limit))
-            .insert_header(header_auth(&token)).to_request();
+            .insert_header(ChtCtTest::header_auth(&token)).to_request();
         let resp: dev::ServiceResponse = test::call_service(&app, req).await;
         assert_eq!(resp.status(), StatusCode::OK); // 200
 
@@ -133,28 +155,33 @@ mod tests {
         let body = body::to_bytes(resp.into_body()).await.unwrap();
         let response: Vec<ChatMessageDto> = serde_json::from_slice(&body).expect(MSG_FAILED_DESER);
 
-        let json = serde_json::json!(&ch_msg1_dto_vec).to_string();
-        let ch_msg1_vec_ser: Vec<ChatMessageDto> = serde_json::from_slice(json.as_bytes()).expect(MSG_FAILED_DESER);
-        assert_eq!(response.len(), ch_msg1_vec_ser.len());
-        assert_eq!(response, ch_msg1_vec_ser);
+        let json = serde_json::json!(&ch_msg2_dto_vec).to_string();
+        let ch_msg2_vec_ser: Vec<ChatMessageDto> = serde_json::from_slice(json.as_bytes()).expect(MSG_FAILED_DESER);
+        assert_eq!(response.len(), ch_msg2_vec_ser.len());
+        assert_eq!(response, ch_msg2_vec_ser);
     }
     #[actix_web::test]
     async fn test_get_chat_message_search_by_str_id_sort_des_part1() {
-        let (cfg_c, data_c, token) = get_cfg_data(4);
-        let stream_id = ChatMsgTest::stream_ids().get(0).unwrap().clone();
-        let limit = 3;
+        let token = ProflTest::token1();
+        let data_p = ProflTest::profiles(&[USER]);
+        let data_cm = ChMesTest::chat_messages(6);
+        let stream_id = data_cm.0.get(0).unwrap().stream_id.clone();
         #[rustfmt::skip]
-        let ch_msg_dto_vec: Vec<ChatMessageDto> = data_c.2.clone()
+        let ch_msg_dto_vec: Vec<ChatMessageDto> = data_cm.0
             .iter().map(|ch_msg| ChatMessageDto::from(ch_msg.clone())).rev().collect();
-
-        let ch_msg1_dto_vec = &ch_msg_dto_vec[0..limit];
+        let limit = 3;
+        let ch_msg2_dto_vec = &ch_msg_dto_vec[0..limit];
         #[rustfmt::skip]
         let app = test::init_service(
-            App::new().service(get_chat_message).configure(configure_chat_message(cfg_c, data_c))).await;
+            App::new().service(get_chat_message)
+                .configure(ProflTest::cfg_config_jwt(config_jwt::get_test_config()))
+                .configure(ProflTest::cfg_profile_orm(data_p))
+                .configure(ChMesTest::cfg_chat_message_orm(data_cm))
+        ).await;
         #[rustfmt::skip]
         let req = test::TestRequest::get()
             .uri(&format!("/api/chat_messages?streamId={}&isSortDes=true&limit={}", stream_id, limit))
-            .insert_header(header_auth(&token)).to_request();
+            .insert_header(ChtCtTest::header_auth(&token)).to_request();
         let resp: dev::ServiceResponse = test::call_service(&app, req).await;
         assert_eq!(resp.status(), StatusCode::OK); // 200
 
@@ -163,30 +190,35 @@ mod tests {
         let body = body::to_bytes(resp.into_body()).await.unwrap();
         let response: Vec<ChatMessageDto> = serde_json::from_slice(&body).expect(MSG_FAILED_DESER);
 
-        let json = serde_json::json!(&ch_msg1_dto_vec).to_string();
-        let ch_msg1_vec_ser: Vec<ChatMessageDto> = serde_json::from_slice(json.as_bytes()).expect(MSG_FAILED_DESER);
-        assert_eq!(response.len(), ch_msg1_vec_ser.len());
-        assert_eq!(response, ch_msg1_vec_ser);
+        let json = serde_json::json!(&ch_msg2_dto_vec).to_string();
+        let ch_msg2_vec_ser: Vec<ChatMessageDto> = serde_json::from_slice(json.as_bytes()).expect(MSG_FAILED_DESER);
+        assert_eq!(response.len(), ch_msg2_vec_ser.len());
+        assert_eq!(response, ch_msg2_vec_ser);
     }
     #[actix_web::test]
     async fn test_get_chat_message_search_by_str_id_sort_des_part2() {
-        let (cfg_c, data_c, token) = get_cfg_data(4);
-        let stream_id = ChatMsgTest::stream_ids().get(0).unwrap().clone();
-        let limit = 3;
+        let token = ProflTest::token1();
+        let data_p = ProflTest::profiles(&[USER]);
+        let data_cm = ChMesTest::chat_messages(6);
+        let stream_id = data_cm.0.get(0).unwrap().stream_id.clone();
         #[rustfmt::skip]
-        let ch_msg_dto_vec: Vec<ChatMessageDto> = data_c.2.clone()
+        let ch_msg_dto_vec: Vec<ChatMessageDto> = data_cm.0
             .iter().map(|ch_msg| ChatMessageDto::from(ch_msg.clone())).rev().collect();
-
+        let limit = 3;
         let max_date = ch_msg_dto_vec.get(limit - 1).unwrap().date;
         let max_date_str = max_date.to_rfc3339_opts(SecondsFormat::Millis, true);
-        let ch_msg1_dto_vec = &ch_msg_dto_vec[limit..2 * limit];
+        let ch_msg2_dto_vec = &ch_msg_dto_vec[limit..2 * limit];
         #[rustfmt::skip]
         let app = test::init_service(
-            App::new().service(get_chat_message).configure(configure_chat_message(cfg_c, data_c))).await;
+            App::new().service(get_chat_message)
+                .configure(ProflTest::cfg_config_jwt(config_jwt::get_test_config()))
+                .configure(ProflTest::cfg_profile_orm(data_p))
+                .configure(ChMesTest::cfg_chat_message_orm(data_cm))
+        ).await;
         #[rustfmt::skip]
         let req = test::TestRequest::get()
             .uri(&format!("/api/chat_messages?streamId={}&isSortDes=true&maxDate={}&limit={}", stream_id, max_date_str, limit))
-            .insert_header(header_auth(&token)).to_request();
+            .insert_header(ChtCtTest::header_auth(&token)).to_request();
         let resp: dev::ServiceResponse = test::call_service(&app, req).await;
         assert_eq!(resp.status(), StatusCode::OK); // 200
 
@@ -195,9 +227,9 @@ mod tests {
         let body = body::to_bytes(resp.into_body()).await.unwrap();
         let response: Vec<ChatMessageDto> = serde_json::from_slice(&body).expect(MSG_FAILED_DESER);
 
-        let json = serde_json::json!(&ch_msg1_dto_vec).to_string();
-        let ch_msg1_vec_ser: Vec<ChatMessageDto> = serde_json::from_slice(json.as_bytes()).expect(MSG_FAILED_DESER);
-        assert_eq!(response.len(), ch_msg1_vec_ser.len());
-        assert_eq!(response, ch_msg1_vec_ser);
+        let json = serde_json::json!(&ch_msg2_dto_vec).to_string();
+        let ch_msg2_vec_ser: Vec<ChatMessageDto> = serde_json::from_slice(json.as_bytes()).expect(MSG_FAILED_DESER);
+        assert_eq!(response.len(), ch_msg2_vec_ser.len());
+        assert_eq!(response, ch_msg2_vec_ser);
     }
 }
