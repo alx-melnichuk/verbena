d0073e6fe27403b90af3342bff129179bb9e50a0
alx.melnichuk <alx.melnichuk@gmail.com>
2025-08-10 10:26:54 +0300

    BE: Added display of information during debugging. Tests removed.

--- backend/src/extractors/authentication.rs
+++ backend/src/extractors/authentication.rs
@@ -5,7 +5,7 @@ use futures_util::{
     future::{ready, LocalBoxFuture, Ready},
     FutureExt,
 };
-use log::{debug, error, log_enabled, Level::Debug};
+use log::{debug, error, log_enabled, Level::{Debug, Info}};
 use vrb_common::api_error::{code_to_str, ApiError};
 use vrb_dbase::db_enums::UserRole;
 use vrb_tools::{err, token_coding, token_data};
@@ -112,7 +112,7 @@ where
 
     /// The future type representing the asynchronous response.
     fn call(&self, req: dev::ServiceRequest) -> Self::Future {
-        let opt_timer0 = if log_enabled!(Debug) { Some(std::time::Instant::now()) } else { None };
+        let opt_timer0 = if log_enabled!(Info) { Some(std::time::Instant::now()) } else { None };
 
         // Attempt to extract token from cookie or authorization header
         let token = token_data::get_token_from_cookie_or_header(req.request());
@@ -120,8 +120,11 @@ where
         // If token is missing, return unauthorized error
         if token.is_none() {
             error!("{}: {}", code_to_str(StatusCode::UNAUTHORIZED), err::MSG_MISSING_TOKEN);
+            if log_enabled!(Debug) { 
+                eprintln!("\n401a {}: {}", code_to_str(StatusCode::UNAUTHORIZED), err::MSG_MISSING_TOKEN);
+            }
             let json_error = ApiError::new(401, err::MSG_MISSING_TOKEN);
-            return Box::pin(ready(Err(error::ErrorUnauthorized(json_error)))); // 401
+            return Box::pin(ready(Err(error::ErrorUnauthorized(json_error)))); // 401a
         }
         let token = token.unwrap().clone();
 
@@ -133,8 +136,11 @@ where
         if let Err(e) = token_res {
             let message = format!("{}: {}", err::MSG_INVALID_OR_EXPIRED_TOKEN, &e);
             error!("{}: {}", code_to_str(StatusCode::UNAUTHORIZED), &message);
+            if log_enabled!(Debug) {
+                eprintln!("\n401b {}: {} err: {}", code_to_str(StatusCode::UNAUTHORIZED), &message, &e);
+            }
             let json_error = ApiError::new(401, &message);
-            return Box::pin(ready(Err(error::ErrorUnauthorized(json_error)))); // 401
+            return Box::pin(ready(Err(error::ErrorUnauthorized(json_error)))); // 401b
         }
 
         let (user_id, num_token) = token_res.unwrap();
@@ -148,6 +154,9 @@ where
             // Check the token for correctness and get the user profile.
             let res_profile = check_token_and_get_profile(user_id, num_token, profile_orm).await;
             if let Err(app_error) = res_profile {
+                if log_enabled!(Debug) {
+                    eprintln!("\napp_error: {:?}", &app_error);
+                }
                 return Err(app_error.into());
             }
             let profile = res_profile.unwrap();
@@ -173,265 +182,3 @@ where
     }
 }
 
-#[cfg(all(test, feature = "mockdata"))]
-mod tests {
-    use actix_web::{
-        cookie::Cookie,
-        dev, get,
-        http::{header, StatusCode},
-        test, web, App, HttpResponse,
-    };
-    use vrb_common::api_error::code_to_str;
-    use vrb_dbase::db_enums::UserRole;
-    use vrb_tools::{token_coding, token_data};
-
-    use crate::profiles::{config_jwt, profile_models::Session};
-    use crate::utils::token_verification::MSG_UNACCEPTABLE_TOKEN_ID;
-
-    use super::*;
-
-    const ADMIN: u8 = 0;
-    const USER: u8 = 1;
-    const MSG_ERROR_WAS_EXPECTED: &str = "Service call succeeded, but an error was expected.";
-    const MSG_FAILED_TO_DESER: &str = "Failed to deserialize JSON string";
-
-    #[get("/", wrap = "RequireAuth::allowed_roles(RequireAuth::all_roles())")]
-    async fn handler_with_auth() -> HttpResponse {
-        HttpResponse::Ok().into()
-    }
-
-    #[get("/", wrap = "RequireAuth::allowed_roles(RequireAuth::admin_role())")]
-    async fn handler_with_require_only_admin() -> HttpResponse {
-        HttpResponse::Ok().into()
-    }
-
-    fn create_profile(role: u8) -> Profile {
-        let nickname = "Oliver_Taylor".to_string();
-        let role = if role == ADMIN { UserRole::Admin } else { UserRole::User };
-        let profile = ProfileOrmApp::new_profile(1, &nickname, &format!("{}@gmail.com", &nickname), role);
-        profile
-    }
-    fn profile_with_id(profile: Profile) -> Profile {
-        let profile_orm = ProfileOrmApp::create(&vec![profile], &[]);
-        profile_orm.profile_vec.get(0).unwrap().clone()
-    }
-    fn cfg_jwt() -> config_jwt::ConfigJwt {
-        config_jwt::get_test_config()
-    }
-    fn header_auth(token: &str) -> (header::HeaderName, header::HeaderValue) {
-        let header_value = header::HeaderValue::from_str(&format!("{}{}", token_data::BEARER, token)).unwrap();
-        (header::AUTHORIZATION, header_value)
-    }
-    fn get_cfg_data(role: u8) -> (config_jwt::ConfigJwt, (Vec<Profile>, Vec<Session>), String) {
-        // Create profile values.
-        let profile1: Profile = profile_with_id(create_profile(role));
-        let num_token = 1234;
-        let session1 = Session { user_id: profile1.user_id, num_token: Some(num_token) };
-
-        let config_jwt = config_jwt::get_test_config();
-        let jwt_secret: &[u8] = config_jwt.jwt_secret.as_bytes();
-        // Create token values.
-        let token = token_coding::encode_token(profile1.user_id, num_token, &jwt_secret, config_jwt.jwt_access).unwrap();
-
-        let data_c = (vec![profile1], vec![session1]);
-
-        (config_jwt, data_c, token)
-    }
-    fn configure_auth(
-        config_jwt: config_jwt::ConfigJwt,    // configuration
-        data_c: (Vec<Profile>, Vec<Session>), // cortege of data vectors
-    ) -> impl FnOnce(&mut web::ServiceConfig) {
-        move |config: &mut web::ServiceConfig| {
-            let data_config_jwt = web::Data::new(config_jwt);
-            let data_profile_orm = web::Data::new(ProfileOrmApp::create(&data_c.0, &[]));
-
-            config
-                .app_data(web::Data::clone(&data_config_jwt))
-                .app_data(web::Data::clone(&data_profile_orm));
-        }
-    }
-
-    #[actix_web::test]
-    async fn test_authentication_middelware_valid_token() {
-        let (cfg_c, data_c, token) = get_cfg_data(USER);
-        #[rustfmt::skip]
-        let app = test::init_service(
-            App::new().service(handler_with_auth).configure(configure_auth(cfg_c, data_c))).await;
-        let req = test::TestRequest::get().insert_header(header_auth(&token)).to_request();
-        let resp: dev::ServiceResponse = test::call_service(&app, req).await;
-
-        assert_eq!(resp.status(), StatusCode::OK); // 200
-    }
-
-    #[actix_web::test]
-    async fn test_authentication_middelware_valid_token_with_cookie() {
-        let (cfg_c, data_c, token) = get_cfg_data(USER);
-        #[rustfmt::skip]
-        let app = test::init_service(
-            App::new().service(handler_with_auth).configure(configure_auth(cfg_c, data_c))).await;
-        let req = test::TestRequest::get().cookie(Cookie::new("token", token)).to_request();
-        let resp: dev::ServiceResponse = test::call_service(&app, req).await;
-
-        assert_eq!(resp.status(), StatusCode::OK); // 200
-    }
-
-    #[actix_web::test]
-    async fn test_authentication_middleware_access_admin_only_endpoint_success() {
-        let (cfg_c, data_c, token) = get_cfg_data(ADMIN);
-        #[rustfmt::skip]
-        let app = test::init_service(
-            App::new().service(handler_with_require_only_admin).configure(configure_auth(cfg_c, data_c))).await;
-        let req = test::TestRequest::get().insert_header(header_auth(&token)).to_request();
-        let resp: dev::ServiceResponse = test::call_service(&app, req).await;
-
-        assert_eq!(resp.status(), StatusCode::OK); // 200
-    }
-
-    #[actix_web::test]
-    async fn test_authentication_middleware_missing_token() {
-        let cfg_c = cfg_jwt();
-        let data_c = (vec![], vec![]);
-        #[rustfmt::skip]
-        let app = test::init_service(
-            App::new().service(handler_with_auth).configure(configure_auth(cfg_c, data_c))).await;
-        let req = test::TestRequest::get().to_request();
-        let result = test::try_call_service(&app, req).await.err();
-        let err = result.expect(MSG_ERROR_WAS_EXPECTED);
-
-        let actual_status = err.as_response_error().status_code();
-        assert_eq!(actual_status, StatusCode::UNAUTHORIZED); // 401
-
-        let api_err: ApiError = serde_json::from_str(&err.to_string()).expect(MSG_FAILED_TO_DESER);
-        assert_eq!(api_err.code, code_to_str(StatusCode::UNAUTHORIZED));
-        assert_eq!(api_err.message, err::MSG_MISSING_TOKEN);
-    }
-    #[actix_web::test]
-    async fn test_authentication_middleware_invalid_token() {
-        let cfg_c = cfg_jwt();
-        let data_c = (vec![], vec![]);
-        #[rustfmt::skip]
-        let app = test::init_service(
-            App::new().service(handler_with_auth).configure(configure_auth(cfg_c, data_c))).await;
-        let req = test::TestRequest::get().insert_header(header_auth("invalid_token123")).to_request();
-        let result = test::try_call_service(&app, req).await.err();
-        let err = result.expect(MSG_ERROR_WAS_EXPECTED);
-
-        let actual_status = err.as_response_error().status_code();
-        assert_eq!(actual_status, StatusCode::UNAUTHORIZED); // 401
-
-        let api_err: ApiError = serde_json::from_str(&err.to_string()).expect(MSG_FAILED_TO_DESER);
-        assert_eq!(api_err.code, code_to_str(StatusCode::UNAUTHORIZED));
-        assert!(api_err.message.starts_with(err::MSG_INVALID_OR_EXPIRED_TOKEN));
-    }
-    #[actix_web::test]
-    async fn test_authentication_middelware_expired_token() {
-        let (cfg_c, data_c, _token) = get_cfg_data(USER);
-        let num_token = data_c.1.get(0).unwrap().num_token.unwrap_or(0); // session_vec
-        let user_id = data_c.0.get(0).unwrap().user_id; // profile_vec
-        let jwt_secret: &[u8] = cfg_c.jwt_secret.as_bytes();
-        let token = token_coding::encode_token(user_id, num_token, &jwt_secret, -cfg_c.jwt_access).unwrap();
-        #[rustfmt::skip]
-        let app = test::init_service(
-            App::new().service(handler_with_auth).configure(configure_auth(cfg_c, data_c))).await;
-        let req = test::TestRequest::get().insert_header(header_auth(&token)).to_request();
-        let result = test::try_call_service(&app, req).await.err();
-        let err = result.expect(MSG_ERROR_WAS_EXPECTED);
-
-        let actual_status = err.as_response_error().status_code();
-        assert_eq!(actual_status, StatusCode::UNAUTHORIZED); // 401
-
-        let api_err: ApiError = serde_json::from_str(&err.to_string()).expect(MSG_FAILED_TO_DESER);
-        assert_eq!(api_err.code, code_to_str(StatusCode::UNAUTHORIZED));
-        assert_eq!(api_err.message, format!("{}: ExpiredSignature", err::MSG_INVALID_OR_EXPIRED_TOKEN));
-    }
-    #[actix_web::test]
-    async fn test_authentication_middelware_valid_token_session_non_exist() {
-        let (cfg_c, data_c, token) = get_cfg_data(USER);
-
-        let num_token = data_c.1.get(0).unwrap().num_token.unwrap_or(0); // session_vec
-        let user_id = data_c.0.get(0).unwrap().user_id; // profile_vec
-        let session1 = Session { user_id: user_id + 1, num_token: Some(num_token) };
-
-        let data_c = (data_c.0, vec![session1]);
-        #[rustfmt::skip]
-        let app = test::init_service(
-            App::new().service(handler_with_auth).configure(configure_auth(cfg_c, data_c))).await;
-        let req = test::TestRequest::get().insert_header(header_auth(&token)).to_request();
-        let result = test::try_call_service(&app, req).await.err();
-        let err = result.expect(MSG_ERROR_WAS_EXPECTED);
-
-        let actual_status = err.as_response_error().status_code();
-        assert_eq!(actual_status, StatusCode::NOT_ACCEPTABLE); // 406
-
-        let api_err: ApiError = serde_json::from_str(&err.to_string()).expect(MSG_FAILED_TO_DESER);
-        assert_eq!(api_err.code, code_to_str(StatusCode::NOT_ACCEPTABLE));
-        assert_eq!(api_err.message, format!("{}; user_id: {}", err::MSG_SESSION_NOT_FOUND, user_id));
-    }
-    #[actix_web::test]
-    async fn test_authentication_middelware_valid_token_non_existent_user() {
-        // error1
-        let (cfg_c, data_c, _token) = get_cfg_data(USER);
-
-        let num_token2 = data_c.1.get(0).unwrap().num_token.unwrap_or(0) + 1; // session_vec
-        let user_id_bad = data_c.0.get(0).unwrap().user_id + 1; // profile_vec
-        let session1 = Session { user_id: user_id_bad, num_token: Some(num_token2) };
-
-        let config_jwt = config_jwt::get_test_config();
-        let jwt_secret: &[u8] = config_jwt.jwt_secret.as_bytes();
-        // Create token values.
-        let token = token_coding::encode_token(user_id_bad, num_token2, &jwt_secret, config_jwt.jwt_access).unwrap();
-
-        let data_c = (data_c.0, vec![session1]);
-        #[rustfmt::skip]
-        let app = test::init_service(
-            App::new().service(handler_with_auth).configure(configure_auth(cfg_c, data_c))).await;
-        let req = test::TestRequest::get().insert_header(header_auth(&token)).to_request();
-        let result = test::try_call_service(&app, req).await.err();
-        let err = result.expect(MSG_ERROR_WAS_EXPECTED);
-
-        let actual_status = err.as_response_error().status_code();
-        assert_eq!(actual_status, StatusCode::UNAUTHORIZED); // 401
-
-        let api_err: ApiError = serde_json::from_str(&err.to_string()).expect(MSG_FAILED_TO_DESER);
-        assert_eq!(api_err.code, code_to_str(StatusCode::UNAUTHORIZED));
-        assert_eq!(api_err.message, format!("{}; user_id: {}", MSG_UNACCEPTABLE_TOKEN_ID, user_id_bad));
-    }
-    #[actix_web::test]
-    async fn test_authentication_middelware_valid_token_non_existent_num() {
-        let (cfg_c, data_c, _token) = get_cfg_data(USER);
-        let num_token = data_c.1.get(0).unwrap().num_token.unwrap_or(0); // session_vec
-        let user_id = data_c.0.get(0).unwrap().user_id; // profile_vec
-        let jwt_secret: &[u8] = cfg_c.jwt_secret.as_bytes();
-        let token = token_coding::encode_token(user_id, num_token + 1, &jwt_secret, cfg_c.jwt_access).unwrap();
-        #[rustfmt::skip]
-        let app = test::init_service(
-            App::new().service(handler_with_auth).configure(configure_auth(cfg_c, data_c))).await;
-        let req = test::TestRequest::get().insert_header(header_auth(&token)).to_request();
-        let result = test::try_call_service(&app, req).await.err();
-        let err = result.expect(MSG_ERROR_WAS_EXPECTED);
-
-        let actual_status = err.as_response_error().status_code();
-        assert_eq!(actual_status, StatusCode::UNAUTHORIZED); // 401
-
-        let api_err: ApiError = serde_json::from_str(&err.to_string()).expect(MSG_FAILED_TO_DESER);
-        assert_eq!(api_err.code, code_to_str(StatusCode::UNAUTHORIZED));
-        assert_eq!(api_err.message, format!("{}; user_id: {}", err::MSG_UNACCEPTABLE_TOKEN_NUM, user_id));
-    }
-    #[actix_web::test]
-    async fn test_authentication_middleware_access_admin_only_endpoint_fail() {
-        let (cfg_c, data_c, token) = get_cfg_data(USER);
-        #[rustfmt::skip]
-        let app = test::init_service(
-            App::new().service(handler_with_require_only_admin).configure(configure_auth(cfg_c, data_c))).await;
-        let req = test::TestRequest::get().insert_header(header_auth(&token)).to_request();
-        let result = test::try_call_service(&app, req).await.err();
-        let err = result.expect(MSG_ERROR_WAS_EXPECTED);
-
-        let actual_status = err.as_response_error().status_code();
-        assert_eq!(actual_status, StatusCode::FORBIDDEN); // 403
-
-        let api_err: ApiError = serde_json::from_str(&err.to_string()).expect(MSG_FAILED_TO_DESER);
-        assert_eq!(api_err.code, code_to_str(StatusCode::FORBIDDEN));
-        assert_eq!(api_err.message, err::MSG_ACCESS_DENIED);
-    }
-}
