f593b703682d0add9dfdb89c21f436cee86849d5
alx.melnichuk <alx.melnichuk@gmail.com>
2025-08-19 15:32:18 +0300

    BE: Added "user_auth_orm" to package "vrb_authent".

---
+++ backend/vrb_authent/src/config_jwt.rs
@@ -0,0 +1,36 @@
+use std::env;
+
+pub const ACCESS_TOKEN_DURATION: &str = "900"; // 900 seconds = 15 minutes
+pub const REFRESH_TOKEN_DURATION: &str = "345600"; // 345600 seconds = 4 days  60sec*60min*24hour*4days
+
+#[derive(Debug, Clone)]
+pub struct ConfigJwt {
+    pub jwt_secret: String,
+    // access token duration in seconds
+    pub jwt_access: i64,
+    // refresh token duration in seconds
+    pub jwt_refresh: i64,
+}
+
+impl ConfigJwt {
+    pub fn init_by_env() -> Self {
+        let jwt_secret = env::var("JWT_SECRET_KEY").expect("Env \"JWT_SECRET_KEY\" not found.");
+        let jwt_access = env::var("JWT_ACCESS_TOKEN_DURATION").unwrap_or(ACCESS_TOKEN_DURATION.to_string());
+        let jwt_refresh = env::var("JWT_REFRESH_TOKEN_DURATION").unwrap_or(REFRESH_TOKEN_DURATION.to_string());
+
+        ConfigJwt {
+            jwt_secret,
+            jwt_access: jwt_access.parse::<i64>().unwrap(),
+            jwt_refresh: jwt_refresh.parse::<i64>().unwrap(),
+        }
+    }
+}
+
+#[cfg(any(test, feature = "mockdata"))]
+pub fn get_test_config() -> ConfigJwt {
+    ConfigJwt {
+        jwt_secret: "my-jwt-secret".to_string(),
+        jwt_access: 120,
+        jwt_refresh: 240,
+    }
+}
--- backend/vrb_authent/src/lib.rs
+++ backend/vrb_authent/src/lib.rs
@@ -1,2 +1,5 @@
 pub mod authentication;
 pub mod authentication_test;
+pub mod config_jwt;
+pub mod user_auth_models;
+pub mod user_auth_orm;
