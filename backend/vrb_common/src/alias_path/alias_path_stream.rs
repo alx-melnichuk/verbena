7a92763116b2dfaf9d16879947c9cab48c6d932d
alx.melnichuk <alx.melnichuk@gmail.com>
2025-08-20 21:45:33 +0300

    BE: The AliasStrm::new() method has been modified.

--- backend/vrb_common/src/alias_path/alias_path_stream.rs
+++ backend/vrb_common/src/alias_path/alias_path_stream.rs
@@ -1,5 +1,3 @@
-use std::env;
-
 use crate::alias_path::alias_path::AliasPath;
 use crate::consts;
 
@@ -9,15 +7,15 @@ pub struct AliasStrm {
 }
 
 impl AliasStrm {
-    pub fn new() -> Self {
-        let logo_files_dir = env::var(consts::STRM_LOGO_FILES_DIR).unwrap_or(consts::LOGO_FILES_DIR.to_string());
-
-        let adapter_alias = AliasPath::new(consts::ALIAS_LOGO_FILES_DIR, &logo_files_dir);
-        AliasStrm { adapter_alias }
+    // env::var(consts::STRM_LOGO_FILES_DIR).unwrap_or(consts::LOGO_FILES_DIR.to_string())
+    pub fn new(path: &str) -> Self {
+        AliasStrm { adapter_alias: AliasPath::new(consts::ALIAS_LOGO_FILES_DIR, path) }
     }
+    /// Replace file path prefix with alias.
     pub fn path_to_alias(&self, full_path_file: &str) -> String {
         self.adapter_alias.path_to_alias(full_path_file)
     }
+    /// Return file path prefix instead of alias.
     pub fn alias_to_path(&self, full_path_file: &str) -> String {
         self.adapter_alias.alias_to_path(full_path_file)
     }
