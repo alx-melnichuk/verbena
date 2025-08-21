7305a6c69b7b7f9ffc1a05f6e0f0cccc03b52baa
alx.melnichuk <alx.melnichuk@gmail.com>
2025-08-21 13:17:16 +0300

    BE: Added impl AsRef<AliasPath>.

--- backend/vrb_common/src/alias_path/alias_path_stream.rs
+++ backend/vrb_common/src/alias_path/alias_path_stream.rs
@@ -11,12 +11,10 @@ impl AliasStrm {
     pub fn new(path: &str) -> Self {
         AliasStrm { adapter_alias: AliasPath::new(consts::ALIAS_LOGO_FILES_DIR, path) }
     }
-    /// Replace file path prefix with alias.
-    pub fn path_to_alias(&self, full_path_file: &str) -> String {
-        self.adapter_alias.path_to_alias(full_path_file)
-    }
-    /// Return file path prefix instead of alias.
-    pub fn alias_to_path(&self, full_path_file: &str) -> String {
-        self.adapter_alias.alias_to_path(full_path_file)
+}
+
+impl AsRef<AliasPath> for AliasStrm {
+    fn as_ref(&self) -> &AliasPath {
+        &self.adapter_alias
     }
 }
