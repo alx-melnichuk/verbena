55ee4158c8034b3b8bbe90e6585e9f7834e89478
alx.melnichuk <alx.melnichuk@gmail.com>
2025-08-21 18:11:53 +0300

    BE: Added constants to the "Profiles" section.

--- backend/vrb_common/src/consts.rs
+++ backend/vrb_common/src/consts.rs
@@ -1,4 +1,3 @@
-
 // ** Section: "Streams" **
 
 pub const ALIAS_LOGO_FILES_DIR: &str = "/logo";
@@ -9,10 +8,18 @@ pub const STRM_LOGO_MAX_SIZE: &str = "STRM_LOGO_MAX_SIZE";
 pub const STRM_LOGO_VALID_TYPES: &str = "STRM_LOGO_VALID_TYPES";
 pub const STRM_LOGO_EXT: &str = "STRM_LOGO_EXT";
 pub const STRM_LOGO_MAX_WIDTH: &str = "STRM_LOGO_MAX_WIDTH";
-pub const STRM_LOGO_MAX_HEIGHT: &str ="STRM_LOGO_MAX_HEIGHT"; 
+pub const STRM_LOGO_MAX_HEIGHT: &str = "STRM_LOGO_MAX_HEIGHT";
 
 // ** Section: "Profiles" **
 
 pub const ALIAS_AVATAR_FILES_DIR: &str = "/avatar";
+pub const AVATAR_FILES_DIR: &str = "./imgs/avatar";
+// Config PRFL
+pub const PRFL_AVATAR_FILES_DIR: &str = "PRFL_AVATAR_FILES_DIR";
+pub const PRFL_AVATAR_MAX_SIZE: &str = "PRFL_AVATAR_MAX_SIZE";
+pub const PRFL_AVATAR_VALID_TYPES: &str = "PRFL_AVATAR_VALID_TYPES";
+pub const PRFL_AVATAR_EXT: &str = "PRFL_AVATAR_EXT";
+pub const PRFL_AVATAR_MAX_WIDTH: &str = "PRFL_AVATAR_MAX_WIDTH";
+pub const PRFL_AVATAR_MAX_HEIGHT: &str = "PRFL_AVATAR_MAX_HEIGHT";
 
 // **  **
