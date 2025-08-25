476d8ef40f94b5f938495c9831234a1786f94264
alx.melnichuk <alx.melnichuk@gmail.com>
2025-08-25 18:56:09 +0300

    BE: Added consts: USER1_NAME..USER4_NAME.

--- backend/vrb_common/src/consts_test.rs
+++ backend/vrb_common/src/consts_test.rs
@@ -6,6 +6,11 @@ pub const USER2_ID: i32 = 1101;
 pub const USER3_ID: i32 = 1102;
 pub const USER4_ID: i32 = 1103;
 
+pub const USER1_NAME: &str = "oliver_taylor";
+pub const USER2_NAME: &str = "robert_brown";
+pub const USER3_NAME: &str = "mary_williams";
+pub const USER4_NAME: &str = "ava_wilson";
+
 pub struct UserOrmTest {}
 
 impl UserOrmTest {
