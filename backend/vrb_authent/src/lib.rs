1016f7be083c5c495714fab4679a2219b45b66aa
alx.melnichuk <alx.melnichuk@gmail.com>
2025-08-28 11:44:25 +0300

    BE: Removed obsolete module "user_models".

--- backend/vrb_authent/src/lib.rs
+++ backend/vrb_authent/src/lib.rs
@@ -3,8 +3,6 @@ pub mod authentication_test;
 pub mod config_jwt;
 pub mod user_auth_models;
 pub mod user_orm;
-pub mod user_models;
-pub mod user_models1;
 pub mod user_recovery_models;
 pub mod user_recovery_orm;
 pub mod user_registr_models;
