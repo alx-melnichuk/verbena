2817186eefd6eb61d506580f9f50de1e5832852a
alx.melnichuk <alx.melnichuk@gmail.com>
2025-09-04 21:42:43 +0300

    BE: Added constants from "user_validations".

--- backend/vrb_authent/src/user_models.rs
+++ backend/vrb_authent/src/user_models.rs
@@ -1,7 +1,119 @@
 use chrono::{DateTime, Utc};
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
+use vrb_common::validators::{ValidationChecks, ValidationError};
 use vrb_dbase::{enm_user_role::UserRole, schema};
+
+// ** Section: "nickname" **
+
+pub const NICKNAME_MIN: u8 = 3;
+pub const NICKNAME_MAX: u8 = 64;
+pub const NICKNAME_REGEX: &str = r"^[a-zA-Z]+[\w]+$";
+// \w   Matches any letter, digit or underscore. Equivalent to [a-zA-Z0-9_].
+// \W - Matches anything other than a letter, digit or underscore. Equivalent to [^a-zA-Z0-9_]
+pub const MSG_NICKNAME_REQUIRED: &str = "nickname:required";
+pub const MSG_NICKNAME_MIN_LENGTH: &str = "nickname:min_length";
+pub const MSG_NICKNAME_MAX_LENGTH: &str = "nickname:max_length";
+pub const MSG_NICKNAME_REGEX: &str = "nickname:regex";
+
+// MIN=3, MAX=64, REG="^[a-zA-Z]+[\\w]+$"
+pub fn validate_nickname(value: &str) -> Result<(), ValidationError> {
+    ValidationChecks::required(value, MSG_NICKNAME_REQUIRED)?;
+    ValidationChecks::min_length(value, NICKNAME_MIN.into(), MSG_NICKNAME_MIN_LENGTH)?;
+    ValidationChecks::max_length(value, NICKNAME_MAX.into(), MSG_NICKNAME_MAX_LENGTH)?;
+    ValidationChecks::regexp(value, NICKNAME_REGEX, MSG_NICKNAME_REGEX)?; // /^[a-zA-Z]+[\w]+$/
+    Ok(())
+}
+
+// ** Section: "email" **
+
+pub const EMAIL_MIN: u8 = 5;
+// https://stackoverflow.com/questions/386294/what-is-the-maximum-length-of-a-valid-email-address
+// What is the maximum length of a valid email address?
+// Answer: An email address must not exceed 254 characters.
+pub const EMAIL_MAX: u16 = 254;
+pub const MSG_EMAIL_REQUIRED: &str = "email:required";
+pub const MSG_EMAIL_MIN_LENGTH: &str = "email:min_length";
+pub const MSG_EMAIL_MAX_LENGTH: &str = "email:max_length";
+pub const MSG_EMAIL_EMAIL_TYPE: &str = "email:email_type";
+
+// MIN=5, MAX=254, "email:email_type"
+pub fn validate_email(value: &str) -> Result<(), ValidationError> {
+    ValidationChecks::required(value, MSG_EMAIL_REQUIRED)?;
+    ValidationChecks::min_length(value, EMAIL_MIN.into(), MSG_EMAIL_MIN_LENGTH)?;
+    ValidationChecks::max_length(value, EMAIL_MAX.into(), MSG_EMAIL_MAX_LENGTH)?;
+    ValidationChecks::email(value, MSG_EMAIL_EMAIL_TYPE)?;
+    Ok(())
+}
+
+// ** Section: "password" **
+
+pub const PASSWORD_MIN: u8 = 6;
+pub const PASSWORD_MAX: u8 = 64;
+pub const PASSWORD_LOWERCASE_LETTER_REGEX: &str = r"[a-z]+";
+pub const PASSWORD_CAPITAL_LETTER_REGEX: &str = r"[A-Z]+";
+pub const PASSWORD_NUMBER_REGEX: &str = r"[\d]+";
+// pub const PASSWORD_REGEX: &str = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d\W_]{6,}$";
+pub const MSG_PASSWORD_REQUIRED: &str = "password:required";
+pub const MSG_PASSWORD_MIN_LENGTH: &str = "password:min_length";
+pub const MSG_PASSWORD_MAX_LENGTH: &str = "password:max_length";
+pub const MSG_PASSWORD_REGEX: &str = "password:regex";
+
+// MIN=6, MAX=64, REG="[a-z]+","[A-Z]+","[\\d]+"
+pub fn validate_password(value: &str) -> Result<(), ValidationError> {
+    ValidationChecks::required(value, MSG_PASSWORD_REQUIRED)?;
+    ValidationChecks::min_length(value, PASSWORD_MIN.into(), MSG_PASSWORD_MIN_LENGTH)?;
+    ValidationChecks::max_length(value, PASSWORD_MAX.into(), MSG_PASSWORD_MAX_LENGTH)?;
+    ValidationChecks::regexp(value, PASSWORD_LOWERCASE_LETTER_REGEX, MSG_PASSWORD_REGEX)?;
+    ValidationChecks::regexp(value, PASSWORD_CAPITAL_LETTER_REGEX, MSG_PASSWORD_REGEX)?;
+    ValidationChecks::regexp(value, PASSWORD_NUMBER_REGEX, MSG_PASSWORD_REGEX)?;
+    Ok(())
+}
+
+// ** Section: "new_password" **
+
+pub const MSG_NEW_PASSWORD_REQUIRED: &str = "new_password:required";
+pub const MSG_NEW_PASSWORD_MIN_LENGTH: &str = "new_password:min_length";
+pub const MSG_NEW_PASSWORD_MAX_LENGTH: &str = "new_password:max_length";
+pub const MSG_NEW_PASSWORD_REGEX: &str = "new_password:regex";
+pub const MSG_NEW_PASSWORD_EQUAL_OLD_VALUE: &str = "new_password:equal_to_old_value";
+
+// MIN=6, MAX=64, REG="[a-z]+","[A-Z]+","[\\d]+" OR "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d\\W_]{6,}$"
+pub fn validate_new_password(value: &str) -> Result<(), ValidationError> {
+    ValidationChecks::required(value, MSG_NEW_PASSWORD_REQUIRED)?;
+    ValidationChecks::min_length(value, PASSWORD_MIN.into(), MSG_NEW_PASSWORD_MIN_LENGTH)?;
+    ValidationChecks::max_length(value, PASSWORD_MAX.into(), MSG_NEW_PASSWORD_MAX_LENGTH)?;
+    ValidationChecks::regexp(value, PASSWORD_LOWERCASE_LETTER_REGEX, MSG_NEW_PASSWORD_REGEX)?;
+    ValidationChecks::regexp(value, PASSWORD_CAPITAL_LETTER_REGEX, MSG_NEW_PASSWORD_REGEX)?;
+    ValidationChecks::regexp(value, PASSWORD_NUMBER_REGEX, MSG_NEW_PASSWORD_REGEX)?;
+    Ok(())
+}
+
+pub fn validate_inequality(value1: &str, value2: &str) -> Result<(), ValidationError> {
+    if value1.starts_with(value2) && value1.len() == value2.len() {
+        let err = ValidationError::new(MSG_NEW_PASSWORD_EQUAL_OLD_VALUE);
+        return Err(err);
+    }
+    Ok(())
+}
+
+// ** Section: "descript" **
+
+pub const DESCRIPT_MIN: u8 = 2;
+pub const DESCRIPT_MAX: u16 = 2048; // 2*1024
+
+// ** Section: "theme" **
+
+pub const THEME_MIN: u8 = 2;
+pub const THEME_MAX: u8 = 32;
+
+// ** Section: "locale" **
+
+pub const LOCALE_MIN: u8 = 2;
+pub const LOCALE_MAX: u8 = 32;
+
+
+// * * * * Section: models for "UserOrm". * * * *
 
 // ** Model: "User". **
 
@@ -124,3 +236,112 @@ impl Profile {
 }
 
 // * * * *   * * * *
+
+#[cfg(any(test, feature = "mockdata"))]
+pub const ADMIN: u8 = 0;
+#[cfg(any(test, feature = "mockdata"))]
+pub const USER: u8 = 1;
+
+#[cfg(any(test, feature = "mockdata"))]
+pub const USER1: usize = 0;
+#[cfg(any(test, feature = "mockdata"))]
+pub const USER2: usize = 1;
+#[cfg(any(test, feature = "mockdata"))]
+pub const USER3: usize = 2;
+#[cfg(any(test, feature = "mockdata"))]
+pub const USER4: usize = 3;
+
+#[cfg(any(test, feature = "mockdata"))]
+pub const USER1_ID: i32 = 1100;
+#[cfg(any(test, feature = "mockdata"))]
+pub const USER2_ID: i32 = 1101;
+#[cfg(any(test, feature = "mockdata"))]
+pub const USER3_ID: i32 = 1102;
+#[cfg(any(test, feature = "mockdata"))]
+pub const USER4_ID: i32 = 1103;
+
+#[cfg(any(test, feature = "mockdata"))]
+pub const USER1_NAME: &str = "oliver_taylor";
+#[cfg(any(test, feature = "mockdata"))]
+pub const USER2_NAME: &str = "robert_brown";
+#[cfg(any(test, feature = "mockdata"))]
+pub const USER3_NAME: &str = "mary_williams";
+#[cfg(any(test, feature = "mockdata"))]
+pub const USER4_NAME: &str = "ava_wilson";
+
+#[cfg(any(test, feature = "mockdata"))]
+pub const USER_IDS: [i32; 4] = [USER1_ID, USER2_ID, USER3_ID, USER4_ID];
+#[cfg(any(test, feature = "mockdata"))]
+pub const USER_NAMES: [&str; 4] = [USER1_NAME, USER2_NAME, USER3_NAME, USER4_NAME];
+
+#[cfg(any(test, feature = "mockdata"))]
+pub struct UserMock {}
+
+#[cfg(any(test, feature = "mockdata"))]
+impl UserMock {
+    pub fn nickname_min() -> String {
+        (0..(NICKNAME_MIN - 1)).map(|_| 'a').collect()
+    }
+    pub fn nickname_max() -> String {
+        (0..(NICKNAME_MAX + 1)).map(|_| 'a').collect()
+    }
+    pub fn nickname_wrong() -> String {
+        let nickname: String = (0..(NICKNAME_MIN - 1)).map(|_| 'a').collect();
+        format!("{}#", nickname)
+    }
+    pub fn email_min() -> String {
+        let suffix = "@us".to_owned();
+        let email_min: usize = EMAIL_MIN.into();
+        let email: String = (0..(email_min - 1 - suffix.len())).map(|_| 'a').collect();
+        format!("{}{}", email, suffix)
+    }
+    pub fn email_max() -> String {
+        let email_max: usize = EMAIL_MAX.into();
+        let prefix: String = (0..64).map(|_| 'a').collect();
+        let domain = ".ua";
+        let len = email_max - prefix.len() - domain.len() + 1;
+        let suffix: String = (0..len).map(|_| 'a').collect();
+        format!("{}@{}{}", prefix, suffix, domain)
+    }
+    pub fn email_wrong() -> String {
+        let suffix = "@".to_owned();
+        let email_min: usize = EMAIL_MIN.into();
+        let email: String = (0..(email_min - suffix.len())).map(|_| 'a').collect();
+        format!("{}{}", email, suffix)
+    }
+    pub fn password_min() -> String {
+        (0..(PASSWORD_MIN - 1)).map(|_| 'a').collect()
+    }
+    pub fn password_max() -> String {
+        (0..(PASSWORD_MAX + 1)).map(|_| 'a').collect()
+    }
+    pub fn password_wrong() -> String {
+        (0..(PASSWORD_MIN)).map(|_| 'a').collect()
+    }
+    pub fn role_wrong() -> String {
+        let role = UserRole::all_values().get(0).unwrap().to_string();
+        role[0..(role.len() - 1)].to_string()
+    }
+    pub fn get_num_token(user_id: i32) -> i32 {
+        40000 + user_id
+    }
+    pub fn users(roles: &[u8]) -> (Vec<User>, Vec<Session>) {
+        let mut user_vec: Vec<User> = Vec::new();
+        let mut session_vec: Vec<Session> = Vec::new();
+        let user_ids = USER_IDS.clone();
+
+        let len = if roles.len() > user_ids.len() { user_ids.len() } else { roles.len() };
+        for index in 0..len {
+            let user_id = user_ids.get(index).unwrap().clone();
+            let nickname = USER_NAMES.get(index).unwrap().to_lowercase();
+            #[rustfmt::skip]
+            let role = if *(roles.get(index).unwrap()) == ADMIN { UserRole::Admin } else { UserRole::User };
+
+            let user = User::new(user_id, &nickname, &format!("{}@gmail.com", nickname), "", role);
+            user_vec.push(user);
+            let num_token = if user_id == USER1_ID { Some(Self::get_num_token(user_id)) } else { None };
+            session_vec.push(Session { user_id, num_token });
+        }
+        (user_vec, session_vec)
+    }
+}
