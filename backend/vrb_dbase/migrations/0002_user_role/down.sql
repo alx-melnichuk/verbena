0fc3a87912c88316154858266a917976e174db43
alx.melnichuk <alx.melnichuk@gmail.com>
2025-09-09 17:03:03 +0300

    SQL: Removing "0002_user_role".

--- backend/vrb_dbase/migrations/0002_user_role/down.sql
+++
@@ -1,8 +0,0 @@
-/* Remove the "role" column from the "users" table. */
-ALTER TABLE users DROP COLUMN "role";
-
-/* Remove type "user_role". */
-DROP TYPE IF EXISTS user_role;
-
-/* Drop the session table. */
-DROP TABLE IF EXISTS sessions;
