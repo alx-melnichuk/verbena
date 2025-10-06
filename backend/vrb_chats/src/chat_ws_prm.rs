58c16f9abdd1ad94be3bf22ff56d2dec267b8673
alx.melnichuk <alx.melnichuk@gmail.com>
2025-10-06 11:29:30 +0300

    BE: Modification "handle_event_ews_prm".

--- backend/vrb_chats/src/chat_ws_prm.rs
+++ backend/vrb_chats/src/chat_ws_prm.rs
@@ -25,31 +25,31 @@ pub trait ChatWsPrm {
 
     fn prm_issue_system_async<M: BrokerMsg>(&self, msg: M);
 
-    fn handle_event_ews_prm(&self, event: EventWS) -> Result<(), ErrEWS> {
+    fn handle_event_ews_prm(&self, event: EventWS) -> Result<bool, ErrEWS> {
         match event.ews_type() {
             EWSType::PrmBool => {
                 // {"prmBool": "paramB", "valBool": true }
                 let prm_bool = event.get_string("prmBool").unwrap_or("".to_owned());
                 let opt_val_bool = event.get_bool("valBool");
                 self.handle_ews_prm_bool(&prm_bool, opt_val_bool)?;
-                Ok(())
+                Ok(true)
             }
             EWSType::PrmInt => {
                 // {"prmInt": "paramI", "valInt": 10 }
                 let prm_int = event.get_string("prmInt").unwrap_or("".to_owned());
                 let opt_val_int = event.get_i32("valInt");
                 self.handle_ews_prm_int(&prm_int, opt_val_int)?;
-                Ok(())
+                Ok(true)
             }
             EWSType::PrmStr => {
                 // {"prmStr": "paramS", "valStr": "text" }
                 let prm_str = event.get_string("prmStr").unwrap_or("".to_owned());
                 let opt_val_str = event.get_string("valStr");
                 self.handle_ews_prm_str(&prm_str, opt_val_str)?;
-                Ok(())
+                Ok(true)
             }
             _ => {
-                Ok(())
+                Ok(false)
             }
         }
     }
