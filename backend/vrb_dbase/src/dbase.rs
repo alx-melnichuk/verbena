1b6060d4901a5c0ab0ba54ddfa261919c6aae216
alx.melnichuk <alx.melnichuk@gmail.com>
2025-10-06 21:19:58 +0300

    BE: Added "max_size" value setting for connection pool.

--- backend/vrb_dbase/src/dbase.rs
+++ backend/vrb_dbase/src/dbase.rs
@@ -14,7 +14,15 @@ pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 pub fn init_db_pool(db_url: &str) -> DbPool {
     let manager = ConnectionManager::<Connection>::new(db_url);
-    Pool::builder().build(manager).expect("Failed to create pool.")
+    let max_size = 15;
+    let db_pool = Pool::builder()
+        .max_size(max_size)
+        .build(manager)
+        .expect("Failed to create pool.");
+     
+    // let db_pool = Pool::builder().build(manager).expect("Failed to create pool.");
+    eprintln!("db_pool.max_size(): {}", db_pool.max_size());
+    db_pool
 }
 
 pub fn run_migration(conn: &mut PgConnection) {
