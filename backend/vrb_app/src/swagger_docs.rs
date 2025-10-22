8973a72b17e0ed8c43b0fde750bdf49987697ba0
alx.melnichuk <alx.melnichuk@gmail.com>
2025-10-22 15:22:28 +0300

    BE: minor changes.

--- backend/vrb_app/src/swagger_docs.rs
+++ backend/vrb_app/src/swagger_docs.rs
@@ -63,45 +63,45 @@ use vrb_streams::{stream_controller, stream_models};
             // vrb_dbase
             enm_user_role::UserRole,
             // user_authent_controller
-            user_authent_models::UserUniquenessDto, // ::users_uniqueness
+            user_authent_models::UserUniquenessDto,         // ::users_uniqueness
             user_authent_models::UserUniquenessResponseDto, // ::users_uniqueness
-            user_authent_models::LoginDto, // ::login
-            user_authent_models::LoginResponseDto, // ::login
-            user_authent_models::UserTokenDto, // ::update_token
-            user_authent_models::UserTokenResponseDto, // ::update_token
+            user_authent_models::LoginDto,                  // ::login
+            user_authent_models::LoginResponseDto,          // ::login
+            user_authent_models::UserTokenDto,              // ::update_token
+            user_authent_models::UserTokenResponseDto,      // ::update_token
             // user_registr_controller
-            user_registr_models::RegistrUserDto, // ::registration
-            user_registr_models::RegistrUserResponseDto, // ::registration
-            user_registr_models::ConfirmRegistrUserResponseDto, // ::confirm_registration
+            user_registr_models::RegistrUserDto,                         // ::registration
+            user_registr_models::RegistrUserResponseDto,                 // ::registration
+            user_registr_models::ConfirmRegistrUserResponseDto,          // ::confirm_registration
             user_registr_models::RegistrationClearForExpiredResponseDto, // ::registration_clear_for_expired
             // user_recovery_controller
-            user_recovery_models::RecoveryUserDto, // ::recovery
-            user_recovery_models::RecoveryUserResponseDto, // ::recovery
-            user_recovery_models::ConfirmRecoveryUserResponseDto, // ::confirm_recovery
+            user_recovery_models::RecoveryUserDto,                    // ::recovery
+            user_recovery_models::RecoveryUserResponseDto,            // ::recovery
+            user_recovery_models::ConfirmRecoveryUserResponseDto,     // ::confirm_recovery
             user_recovery_models::RecoveryClearForExpiredResponseDto, // ::recovery_clear_for_expired
 
             // profile_controller
             // ::get_profile_by_id, ::get_profile_current, ::put_profile, ::put_profile_new_password,
             // ::delete_profile, ::delete_profile_current
-            profile_models::UserProfileDto, 
-            profile_models::UserProfileMiniDto, // ::get_profile_mini_by_id
-            profile_models::ProfileConfigDto, // ::get_profile_config
-            profile_models::ModifyUserProfileDto, // ::put_profile,
+            profile_models::UserProfileDto,
+            profile_models::UserProfileMiniDto,        // ::get_profile_mini_by_id
+            profile_models::ProfileConfigDto,          // ::get_profile_config
+            profile_models::ModifyUserProfileDto,      // ::put_profile,
             profile_models::NewPasswordUserProfileDto, // ::put_profile_new_password,
 
             // stream_controller
             enm_stream_state::StreamState,
             // ::get_stream_by_id, ::post_stream, ::put_stream, ::put_toggle_state
             stream_models::StreamInfoDto,
-            stream_models::SearchStreamInfoDto, // ::get_streams
-            stream_models::StreamInfoPageDto, // ::get_streams
-            stream_models::StreamConfigDto, // ::get_stream_config
-            stream_models::SearchStreamEventDto, // ::get_streams_events
-            stream_models::StreamEventPageDto, // ::get_streams_events
+            stream_models::SearchStreamInfoDto,   // ::get_streams
+            stream_models::StreamInfoPageDto,     // ::get_streams
+            stream_models::StreamConfigDto,       // ::get_stream_config
+            stream_models::SearchStreamEventDto,  // ::get_streams_events
+            stream_models::StreamEventPageDto,    // ::get_streams_events
             stream_models::SearchStreamPeriodDto, // ::get_streams_period
-            stream_models::CreateStreamInfoDto, // ::post_stream
-            stream_models::ModifyStreamInfoDto, // ::put_stream
-            stream_models::ToggleStreamStateDto, // ::put_toggle_state
+            stream_models::CreateStreamInfoDto,   // ::post_stream
+            stream_models::ModifyStreamInfoDto,   // ::put_stream
+            stream_models::ToggleStreamStateDto,  // ::put_toggle_state
 
             // chat_message_controller
             // ::get_chat_message, ::post_chat_message, ::put_chat_message, ::delete_chat_message
@@ -109,25 +109,25 @@ use vrb_streams::{stream_controller, stream_models};
             chat_message_models::SearchChatMessageDto, // ::get_chat_message
             chat_message_models::CreateChatMessageDto, // ::post_chat_message
             chat_message_models::ModifyChatMessageDto, // ::put_chat_message
-            chat_message_models::BlockedUserDto, // ::get_blocked_users, ::post_blocked_user, ::delete_blocked_user
+            chat_message_models::BlockedUserDto,       // ::get_blocked_users, ::post_blocked_user, ::delete_blocked_user
             chat_message_models::CreateBlockedUserDto, // ::post_blocked_user
             chat_message_models::DeleteBlockedUserDto, // ::delete_blocked_user
 
             // chat_ws_controller
-            chat_event_ws::BlockEWS, // ::get_ws_chat
-            chat_event_ws::CountEWS, // ::get_ws_chat
-            chat_event_ws::EchoEWS, // ::get_ws_chat
-            chat_event_ws::ErrEWS, // ::get_ws_chat
-            chat_event_ws::JoinEWS, // ::get_ws_chat
-            chat_event_ws::LeaveEWS, // ::get_ws_chat
-            chat_event_ws::MsgEWS, // ::get_ws_chat
-            chat_event_ws::MsgCutEWS, // ::get_ws_chat
-            chat_event_ws::MsgPutEWS, // ::get_ws_chat
-            chat_event_ws::MsgRmvEWS, // ::get_ws_chat
-            chat_event_ws::NameEWS, // ::get_ws_chat
+            chat_event_ws::BlockEWS,   // ::get_ws_chat
+            chat_event_ws::CountEWS,   // ::get_ws_chat
+            chat_event_ws::EchoEWS,    // ::get_ws_chat
+            chat_event_ws::ErrEWS,     // ::get_ws_chat
+            chat_event_ws::JoinEWS,    // ::get_ws_chat
+            chat_event_ws::LeaveEWS,   // ::get_ws_chat
+            chat_event_ws::MsgEWS,     // ::get_ws_chat
+            chat_event_ws::MsgCutEWS,  // ::get_ws_chat
+            chat_event_ws::MsgPutEWS,  // ::get_ws_chat
+            chat_event_ws::MsgRmvEWS,  // ::get_ws_chat
+            chat_event_ws::NameEWS,    // ::get_ws_chat
             chat_event_ws::PrmBoolEWS, // ::get_ws_chat
-            chat_event_ws::PrmIntEWS, // ::get_ws_chat
-            chat_event_ws::PrmStrEWS, // ::get_ws_chat
+            chat_event_ws::PrmIntEWS,  // ::get_ws_chat
+            chat_event_ws::PrmStrEWS,  // ::get_ws_chat
             chat_event_ws::UnblockEWS, // ::get_ws_chat
         )
     ),
