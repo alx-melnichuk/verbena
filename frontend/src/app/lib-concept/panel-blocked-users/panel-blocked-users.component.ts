1693fa2c3ec0fdecfb81868946afab8ea81be5e2
alx.melnichuk <alx.melnichuk@gmail.com>
2025-10-28 18:52:53 +0200

    FE: Added matMenu "unblock_user".

--- frontend/src/app/lib-concept/panel-blocked-users/panel-blocked-users.component.ts
+++ frontend/src/app/lib-concept/panel-blocked-users/panel-blocked-users.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
 import { TranslatePipe } from '@ngx-translate/core';
 import { DateTimeFormatPipe } from 'src/app/common/date-time-format.pipe';
 import { SpinnerComponent } from 'src/app/components/spinner/spinner.component';
@@ -8,7 +9,7 @@ import { BlockedUserDto } from 'src/app/lib-chat/chat-message-api.interface';
 @Component({
     selector: 'app-panel-blocked-users',
     standalone: true,
-    imports: [CommonModule, DateTimeFormatPipe, SpinnerComponent, TranslatePipe],
+    imports: [CommonModule, MatMenuModule, DateTimeFormatPipe, SpinnerComponent, TranslatePipe],
     templateUrl: './panel-blocked-users.component.html',
     styleUrl: './panel-blocked-users.component.scss',
     encapsulation: ViewEncapsulation.None,
@@ -24,8 +25,22 @@ export class PanelBlockedUsersComponent {
     @Input()
     public title: string | null = null;
 
-    readonly formatDateTime: Intl.DateTimeFormatOptions = { dateStyle: 'medium', timeStyle: 'short' };
+    @Output()
+    readonly unblockUser: EventEmitter<string> = new EventEmitter();
+
     readonly formatDate: Intl.DateTimeFormatOptions = { dateStyle: 'medium' };
     readonly formatTime: Intl.DateTimeFormatOptions = { timeStyle: 'short' };
+
+
+    constructor() {
+    }
+
+    // ** Public API **
+
+    public doUnblockUser(member: string | null | undefined, blockedUsers: BlockedUserDto[]): void {
+        if (!!member && !!blockedUsers && blockedUsers.findIndex((v) => member == v.blockedNickname) > -1) {
+            this.unblockUser.emit(member);
+        }
+    }
 
 }
