fbb6bb2484c8fd0e0350ed45b5e864041029c281
alx.melnichuk <alx.melnichuk@gmail.com>
2025-11-05 18:50:23 +0200

    FE: The "PanelBannedUsers" has been changed.

--- frontend/src/app/lib-banned/panel-banned-users/panel-banned-users.component.ts
+++ frontend/src/app/lib-banned/panel-banned-users/panel-banned-users.component.ts
@@ -1,14 +1,28 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { DateTimeFormatPipe } from 'src/app/common/date-time-format.pipe';
+import { BlockedUserDto } from 'src/app/lib-chat/chat-message-api.interface';
 
 @Component({
-  selector: 'app-panel-banned-users',
-  standalone: true,
-  imports: [],
-  templateUrl: './panel-banned-users.component.html',
-  styleUrl: './panel-banned-users.component.scss',
-  encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-panel-banned-users',
+    standalone: true,
+    imports: [CommonModule, DateTimeFormatPipe, TranslatePipe],
+    templateUrl: './panel-banned-users.component.html',
+    styleUrl: './panel-banned-users.component.scss',
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PanelBannedUsersComponent {
+    @Input()
+    public blockedUsers: BlockedUserDto[] = [];
+    @Input()
+    public locale: string | null | undefined;
+
+    readonly formatDate: Intl.DateTimeFormatOptions = { dateStyle: 'medium' };
+    readonly formatTime: Intl.DateTimeFormatOptions = { timeStyle: 'short' };
+
+    constructor() {
+    }
 
 }
