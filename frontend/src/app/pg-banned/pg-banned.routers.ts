7ecee2c6e69b441c6cd2f8ee8657056fc985d15a
alx.melnichuk <alx.melnichuk@gmail.com>
2025-11-06 16:40:46 +0200

    FE: The "PgBanned" has been changed.

--- frontend/src/app/pg-banned/pg-banned.routers.ts
+++ frontend/src/app/pg-banned/pg-banned.routers.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
 import { PgBannedComponent } from './pg-banned.component';
 import { pgBannedResolver } from './pg-banned.resolver';
+import { BannedUsersComponent } from '../lib-banned/banned-users/banned-users.component';
 
 export const PG_BANNED_ROUTES: Routes = [
     {
         path: '', // 'ind/banned'
         component: PgBannedComponent,
-        resolve: { blockedUsers: pgBannedResolver },
+        children: [
+            {
+                path: '', // 'ind/banned'
+                component: BannedUsersComponent,
+                resolve: { blockedUsers: pgBannedResolver },
+            }
+        ]
     },
 ];
