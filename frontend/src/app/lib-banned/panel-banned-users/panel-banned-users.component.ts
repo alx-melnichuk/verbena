e1cc76c0049c21cf0e9755578ae7dad2fe3e9337
alx.melnichuk <alx.melnichuk@gmail.com>
2025-11-06 16:39:11 +0200

    FE: The "PanelBannedUsers" has been changed.

--- frontend/src/app/lib-banned/panel-banned-users/panel-banned-users.component.ts
+++ frontend/src/app/lib-banned/panel-banned-users/panel-banned-users.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { TranslatePipe } from '@ngx-translate/core';
 import { DateTimeFormatPipe } from 'src/app/common/date-time-format.pipe';
+import { AvatarComponent } from 'src/app/components/avatar/avatar.component';
 import { BlockedUserDto } from 'src/app/lib-chat/chat-message-api.interface';
 
 @Component({
     selector: 'app-panel-banned-users',
     standalone: true,
-    imports: [CommonModule, DateTimeFormatPipe, TranslatePipe],
+    imports: [CommonModule, MatButtonModule, AvatarComponent, DateTimeFormatPipe, TranslatePipe],
     templateUrl: './panel-banned-users.component.html',
     styleUrl: './panel-banned-users.component.scss',
     encapsulation: ViewEncapsulation.None,
@@ -18,6 +20,8 @@ export class PanelBannedUsersComponent {
     public blockedUsers: BlockedUserDto[] = [];
     @Input()
     public locale: string | null | undefined;
+    @Input()
+    public title: string | null = 'panel-banned-users.title';
 
     readonly formatDate: Intl.DateTimeFormatOptions = { dateStyle: 'medium' };
     readonly formatTime: Intl.DateTimeFormatOptions = { timeStyle: 'short' };
