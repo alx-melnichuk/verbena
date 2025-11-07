492c66513f76eb29e56c8138f0901acbe931e9a7
alx.melnichuk <alx.melnichuk@gmail.com>
2025-11-07 18:57:37 +0200

    FE: The "PgBanned" has been changed.

--- frontend/src/app/pg-banned/pg-banned.component.ts
+++ frontend/src/app/pg-banned/pg-banned.component.ts
@@ -1,9 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LocaleService } from '../common/locale.service';
 import { PanelBannedUsersComponent } from '../lib-banned/panel-banned-users/panel-banned-users.component';
-import { BlockedUserDto } from '../lib-chat/chat-message-api.interface';
+import { BlockedUserDto, BlockedUserMiniDto } from '../lib-chat/chat-message-api.interface';
+import { ChatMessageService } from '../lib-chat/chat-message.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { HttpErrorUtil } from '../utils/http-error.util';
+import { AlertService } from '../lib-dialog/alert.service';
+
+export const SORT_COL_INIT: string = 'nickname';
+export const SORT_DESC_INIT: boolean = false;
 
 @Component({
     selector: 'app-pg-banned',
@@ -16,12 +23,68 @@ import { BlockedUserDto } from '../lib-chat/chat-message-api.interface';
 })
 export class PgBannedComponent {
     public blockedUsers: BlockedUserDto[] = [];
+    public isLoading: boolean = false;
+    public sortColumn: string = SORT_COL_INIT;
+    public sortDesc: boolean = SORT_DESC_INIT;
 
     public localeService: LocaleService = inject(LocaleService);
 
+    private alertService: AlertService = inject(AlertService);
+    private changeDetector: ChangeDetectorRef = inject(ChangeDetectorRef);
     private route: ActivatedRoute = inject(ActivatedRoute);
+    private chatMessageService: ChatMessageService = inject(ChatMessageService);
 
     constructor() {
         this.blockedUsers = this.route.snapshot.data['blockedUsers'];
+    }
+
+    // ** Public API **
+
+    public doSort(event: Record<string, boolean>): void {
+        const keys = Object.keys(event);
+        const sortColumn = keys.length > 0 ? keys[0] : '';
+        const sortDesc: boolean | undefined = !!sortColumn ? event[sortColumn] : false;
+        if (!sortColumn) {
+            return;
+        }
+        this.isLoading = true;
+        this.chatMessageService.getBlockedUsers(sortColumn, sortDesc)
+            .then((response: BlockedUserDto[] | HttpErrorResponse | undefined) => {
+                this.blockedUsers = (response as BlockedUserDto[]); // List of past chat messages.
+                this.sortColumn = sortColumn;
+                this.sortDesc = sortDesc;
+            })
+            .catch((errHttp: HttpErrorResponse) => {
+                console.error(`GetBlockedUsersError:`, errHttp);
+                const errMsg = HttpErrorUtil.getMsgs(errHttp)[0];
+                this.alertService.showError(errMsg, `pg-banned.error_get_blocked_users`);
+
+            })
+            .finally(() => {
+                this.isLoading = false;
+                this.changeDetector.markForCheck();
+            });
+    }
+
+    public doUnblockUser(nickname: string, blockedUsers: BlockedUserDto[]): void {
+        const idx = !!nickname ? blockedUsers.findIndex((v) => v.nickname == nickname) : -1;
+        if (!nickname || idx == -1) {
+            return;
+        }
+        this.isLoading = true;
+        this.chatMessageService.deleteBlockedUser(nickname)
+            .then(() => {
+                Promise.resolve().then(() =>
+                    this.doSort({ [this.sortColumn]: this.sortDesc }));
+            })
+            .catch((errHttp: HttpErrorResponse) => {
+                console.error(`GetBlockedUsersError:`, errHttp);
+                const errMsg = HttpErrorUtil.getMsgs(errHttp)[0];
+                this.alertService.showError(errMsg, `pg-banned.error_get_blocked_users`);
+            })
+            .finally(() => {
+                this.isLoading = false;
+                this.changeDetector.markForCheck();
+            });
     }
 }
--- frontend/src/app/pg-banned/pg-banned.resolver.ts
+++ frontend/src/app/pg-banned/pg-banned.resolver.ts
@@ -4,6 +4,7 @@ import { inject } from '@angular/core';
 
 import { ChatMessageService } from '../lib-chat/chat-message.service';
 import { BlockedUserDto } from '../lib-chat/chat-message-api.interface';
+import { SORT_COL_INIT, SORT_DESC_INIT } from './pg-banned.component';
 
 function goToPageNotFound(router: Router): Promise<undefined> {
     return router.navigateByUrl('/technical/not-found').then(() => Promise.resolve(undefined));
@@ -14,8 +15,8 @@ export const pgBannedResolver: ResolveFn<BlockedUserDto[] | HttpErrorResponse | 
         const router = inject(Router);
         const chatMessageService: ChatMessageService = inject(ChatMessageService);
 
-        let sortColumn: string | undefined;
-        let sortDesc: boolean | undefined;
+        let sortColumn: string = SORT_COL_INIT;
+        let sortDesc: boolean = SORT_DESC_INIT;
         return chatMessageService.getBlockedUsers(sortColumn, sortDesc)
             .catch((err) => {
                 console.error(err);
