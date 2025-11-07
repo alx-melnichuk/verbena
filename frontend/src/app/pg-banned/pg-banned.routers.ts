4f50ce39d4a9b53576c5d9dd3bfe9e381a303e3c
alx.melnichuk <alx.melnichuk@gmail.com>
2025-11-07 16:57:25 +0200

    FE: Removed "BannedUsers".

--- frontend/src/app/pg-banned/pg-banned.routers.ts
+++ frontend/src/app/pg-banned/pg-banned.routers.ts
@@ -1,18 +1,11 @@
 import { Routes } from '@angular/router';
 import { PgBannedComponent } from './pg-banned.component';
 import { pgBannedResolver } from './pg-banned.resolver';
-import { BannedUsersComponent } from '../lib-banned/banned-users/banned-users.component';
 
 export const PG_BANNED_ROUTES: Routes = [
     {
         path: '', // 'ind/banned'
         component: PgBannedComponent,
-        children: [
-            {
-                path: '', // 'ind/banned'
-                component: BannedUsersComponent,
-                resolve: { blockedUsers: pgBannedResolver },
-            }
-        ]
+        resolve: { blockedUsers: pgBannedResolver },
     },
 ];
