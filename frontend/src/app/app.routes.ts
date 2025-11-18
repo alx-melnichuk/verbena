946f1b9443ccdd4d53b48a5a4c8ab9e9c01282ac
alx.melnichuk <alx.melnichuk@gmail.com>
2025-11-18 12:09:47 +0200

    FE: minor changes.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,28 +7,32 @@ export const APP_ROUTES: Routes = [
     {
         path: R_ABOUT, // 'ind/about'
         loadChildren: () => import('./pg-about/pg-about.routes').then((c) => c.PG_ABOUT_ROUTES),
+        // Authorization is not required.
     },
     {
         path: R_LOGIN, // 'ind/login'
         loadChildren: () => import('./pg-login/pg-login.routes').then((c) => c.PG_LOGIN_ROUTES),
+        // Authorization is not required.
     },
     {
         path: R_SIGNUP, // 'ind/signup'
         loadChildren: () => import('./pg-signup/pg-signup.routes').then((c) => c.PG_SIGNUP_ROUTES),
+        // Authorization is not required.
     },
     {
         path: R_FORGOT_PASSWORD, // 'ind/forgot-password'
         loadChildren: () => import('./pg-forgot-password/pg-forgot-password.routes').then((c) => c.PG_FORGOT_PASSWORD_ROUTES),
+        // Authorization is not required.
     },
     {
         path: R_PROFILE, // 'ind/profile'
         loadChildren: () => import('./pg-profile/pg-profile.routes').then((c) => c.PG_PROFILE_ROUTES),
-        canActivate: [authenticationGuard],
+        canActivate: [authenticationGuard], // Authorization is required.
     },
     {
         path: R_STREAM, // 'ind/stream'
         loadChildren: () => import('./pg-stream/pg-stream.routes').then(c => c.PG_STREAM_ROUTES),
-        canActivate: [authenticationGuard],
+        canActivate: [authenticationGuard], // Authorization is required.
     },
     {
         path: R_CONCEPT, // 'ind/concept'
@@ -38,7 +42,7 @@ export const APP_ROUTES: Routes = [
     {
         path: R_BANNED, // 'ind/banned'
         loadChildren: () => import('./pg-banned/pg-banned.routers').then(c => c.PG_BANNED_ROUTES),
-        canActivate: [authenticationGuard],
+        canActivate: [authenticationGuard], // Authorization is required.
     },
     { path: '**', redirectTo: R_ABOUT },
 ];
