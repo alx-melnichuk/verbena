632dc23ac6c77bb7040b9300d38357234f27d96b
alx.melnichuk <alx.melnichuk@gmail.com>
2025-11-18 12:08:21 +0200

    FE: minor changes.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,38 +5,38 @@ import { authenticationGuard } from './common/authentication.guard';
 
 export const APP_ROUTES: Routes = [
     {
-        path: R_ABOUT,
+        path: R_ABOUT, // 'ind/about'
         loadChildren: () => import('./pg-about/pg-about.routes').then((c) => c.PG_ABOUT_ROUTES),
     },
     {
-        path: R_LOGIN,
+        path: R_LOGIN, // 'ind/login'
         loadChildren: () => import('./pg-login/pg-login.routes').then((c) => c.PG_LOGIN_ROUTES),
     },
     {
-        path: R_SIGNUP,
+        path: R_SIGNUP, // 'ind/signup'
         loadChildren: () => import('./pg-signup/pg-signup.routes').then((c) => c.PG_SIGNUP_ROUTES),
     },
     {
-        path: R_FORGOT_PASSWORD,
+        path: R_FORGOT_PASSWORD, // 'ind/forgot-password'
         loadChildren: () => import('./pg-forgot-password/pg-forgot-password.routes').then((c) => c.PG_FORGOT_PASSWORD_ROUTES),
     },
     {
-        path: R_PROFILE,
+        path: R_PROFILE, // 'ind/profile'
         loadChildren: () => import('./pg-profile/pg-profile.routes').then((c) => c.PG_PROFILE_ROUTES),
         canActivate: [authenticationGuard],
     },
     {
-        path: R_STREAM,
+        path: R_STREAM, // 'ind/stream'
         loadChildren: () => import('./pg-stream/pg-stream.routes').then(c => c.PG_STREAM_ROUTES),
         canActivate: [authenticationGuard],
     },
     {
-        path: R_CONCEPT,
+        path: R_CONCEPT, // 'ind/concept'
         loadChildren: () => import('./pg-concept/pg-concept.routers').then(c => c.PG_CONCEPT_ROUTES),
         // Authorization is not required.
     },
     {
-        path: R_BANNED,
+        path: R_BANNED, // 'ind/banned'
         loadChildren: () => import('./pg-banned/pg-banned.routers').then(c => c.PG_BANNED_ROUTES),
         canActivate: [authenticationGuard],
     },
