0297acd989725b7472c9c00240fbd1bc123fe826
alx.melnichuk <alx.melnichuk@gmail.com>
2026-05-05 18:25:38 +0300

    FE: Minor changes.

--- frontend/src/app/lib-pg-browse/pg-browse/pg-user.resolver.ts
+++ frontend/src/app/lib-pg-browse/pg-browse/pg-user.resolver.ts
@@ -1,6 +1,6 @@
-import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
-import { SessionSrv, User } from '../../common/session-srv';
+import { inject } from "@angular/core";
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from "@angular/router";
+import { SessionSrv, User } from "../../common/session-srv";
 
 export const pgUserResolver: ResolveFn<User | null>
     = (_route: ActivatedRouteSnapshot, _state: RouterStateSnapshot) => {
