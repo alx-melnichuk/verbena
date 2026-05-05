df5c6207cea568f937907985a7a0b0bd42b7eec3
alx.melnichuk <alx.melnichuk@gmail.com>
2026-05-05 18:26:31 +0300

    FE: Minor changes.

--- frontend/src/app/lib-pg-browse/pg-browse/pg-access-token.resolver.ts
+++ frontend/src/app/lib-pg-browse/pg-browse/pg-access-token.resolver.ts
@@ -1,6 +1,6 @@
-import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
-import { SessionSrv } from '../../common/session-srv';
+import { inject } from "@angular/core";
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from "@angular/router";
+import { SessionSrv } from "../../common/session-srv";
 
 export const pgAccessTokenResolver: ResolveFn<string | null>
     = (_route: ActivatedRouteSnapshot, _state: RouterStateSnapshot) => {
