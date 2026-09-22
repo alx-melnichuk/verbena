49eec8597e77716999359ccd515db2a6b15e09b1
alx.melnichuk <alx.melnichuk@gmail.com>
2026-09-22 22:06:22 +0300

    FE: Minor changes.

--- frontend/src/app/directives/field-drag-and-drop.ts
+++ frontend/src/app/directives/field-drag-and-drop.ts
@@ -2,6 +2,7 @@ import { Directive, EventEmitter, HostBinding, HostListener, Output } from "@ang
 
 @Directive({
     selector: "[appFieldDragAndDrop]",
+    exportAs: "appFieldDragAndDrop",
     standalone: true
 })
 export class FieldDragAndDrop {
