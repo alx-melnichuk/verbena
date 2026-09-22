f4323e664d95223f5aaa95087afa753464e2f00f
alx.melnichuk <alx.melnichuk@gmail.com>
2026-09-22 21:58:09 +0300

    FE: Added the "IconBox".

---
+++ frontend/src/app/directives/icon-box.spec.ts
@@ -0,0 +1,8 @@
+import { IconBox } from './icon-box';
+
+describe('IconBox', () => {
+  it('should create an instance', () => {
+    const directive = new IconBox();
+    expect(directive).toBeTruthy();
+  });
+});
